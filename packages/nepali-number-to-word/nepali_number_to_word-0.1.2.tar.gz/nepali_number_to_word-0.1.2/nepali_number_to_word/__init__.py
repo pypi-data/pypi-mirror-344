313ec5921a8489daca214b7345f9fe822bd20b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 15:04:02 +0000

    Add package nepali-number-to-word/0.1.2/nepali_number_to_word-0.1.2.tar.gz

---
+++ packages/nepali-number-to-word/nepali_number_to_word-0.1.2.tar.gz/nepali_number_to_word-0.1.2/nepali_number_to_word/__init__.py
@@ -0,0 +1,9 @@
+"""
+Nepali Number to Word Converter
+A package to convert numbers to Nepali words
+"""
+
+from .converter import NepaliNumberConverter
+
+__version__ = "0.1.2"
+__all__ = ["NepaliNumberConverter"]
