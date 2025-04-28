bec9581af4aa488730573917bb8ad53bd2e665a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:18 +0000

    Add package AOT-biomaps/1.3.2/aot_biomaps-1.3.2.tar.gz

---
+++ packages/AOT-biomaps/aot_biomaps-1.3.2.tar.gz/aot_biomaps-1.3.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AOT_biomaps',
+    version='1.3.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'k-wave-python',
+        'GPUtil',
+        'cupy',
+    ],
+    author='Lucas Duclos',
+    author_email='lucas.duclos@universite-paris-saclay.fr',
+    description='Acousto-Optic Tomography',
+    url='https://github.com/LucasDuclos/AcoustoOpticTomography',
+)
