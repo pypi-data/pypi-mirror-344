70e8595b0bd5b37aa7ee3009e9d769f3b421af45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:04:35 +0000

    Add package google-flights/0.0.2/google_flights-0.0.2.tar.gz

---
+++ packages/google-flights/google_flights-0.0.2.tar.gz/google_flights-0.0.2/src/google_flights/__init__.py
@@ -0,0 +1,15 @@
+from google_flights.filter import create_filter
+from google_flights.flights_pb_implem import FlightData, Passengers, TFSData
+from google_flights.main import get_flights_from_filter
+from google_flights.search import search_airline, search_airport
+
+
+__all__ = [
+    "TFSData",
+    "create_filter",
+    "FlightData",
+    "Passengers",
+    "get_flights_from_filter",
+    "search_airline",
+    "search_airport",
+]
