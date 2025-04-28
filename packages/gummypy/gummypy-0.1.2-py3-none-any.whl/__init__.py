9eb92df2bc3506ecb9127428db62782375a294d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 16:04:45 +0000

    Add package gummypy/0.1.2/gummypy-0.1.2-py3-none-any.whl

---
+++ packages/gummypy/gummypy-0.1.2-py3-none-any.whl/__init__.py
@@ -0,0 +1,4 @@
+from .gummypy import choose, confirm, file, format, input, join, pager, spin, table, write, log
+
+__version__ = "0.1.2"
+__all__ = [ "choose", "confirm", "file", "format", "input", "join", "pager", "spin", "table", "write", "log" ]
