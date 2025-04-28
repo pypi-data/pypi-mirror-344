d6630edb2e64509f60b200974bec609763bc4206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 15:04:15 +0000

    Add package tpf/1.0.36/tpf-1.0.36-py3-none-any.whl

---
+++ packages/tpf/tpf-1.0.36-py3-none-any.whl/tpf/llm/__init__.py
@@ -0,0 +1,7 @@
+
+from tpf.llm.ollama import chat_ollama
+from tpf.llm.ollama import client_ollama
+from tpf.llm.ollama import chat 
+from tpf.llm.openai import chat_stream
+from tpf.llm.tools import Tools
+from tpf.llm.funcall import FuncCall
