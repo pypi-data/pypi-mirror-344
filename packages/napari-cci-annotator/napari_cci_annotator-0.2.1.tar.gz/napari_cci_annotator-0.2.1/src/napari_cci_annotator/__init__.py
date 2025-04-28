61a243b6b05d085ed1e8373eff72d8e1023e9f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 16:04:58 +0000

    Add package napari-cci-annotator/0.2.1/napari_cci_annotator-0.2.1.tar.gz

---
+++ packages/napari-cci-annotator/napari_cci_annotator-0.2.1.tar.gz/napari_cci_annotator-0.2.1/src/napari_cci_annotator/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.2.1"
+
+from ._reader import napari_get_reader
+from ._sample_data import make_sample_data
+from ._widget import CciAnnotatorQWidget
+#from ._widget import CciAnnotatorQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from ._writer import write_multiple, write_single_image
+
+__all__ = (
+#    "napari_get_reader",
+#    "write_single_image",
+#    "write_multiple",
+#    "make_sample_data",
+    "CciAnnotatorQWidget",
+#    "ImageThreshold",
+#    "threshold_autogenerate_widget",
+#    "threshold_magic_widget",
+)
