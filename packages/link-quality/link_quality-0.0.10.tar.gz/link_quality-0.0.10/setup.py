5ee975ceb924952bf173d5f507f69490eea5b112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 14:05:06 +0000

    Add package link-quality/0.0.10/link_quality-0.0.10.tar.gz

---
+++ packages/link-quality/link_quality-0.0.10.tar.gz/link_quality-0.0.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="link-quality",
+    version="0.0.10",
+    author="daoya",
+    license="MIT",
+    python_requires=">=3.6",
+    description="link-quality",
+    install_requires=[
+        "tldextract",
+        "urllib3",
+        "aioredis",
+        "aiocache",
+        "loguru",
+        "multipledispatch"
+    ],
+    py_modules=[
+        "spiders.realtime.link_filter",
+        "spiders.realtime.normalize",
+        "spiders.realtime.rt_logger",
+    ],
+    classifiers=["Programming Language :: Python :: 3"],
+)
