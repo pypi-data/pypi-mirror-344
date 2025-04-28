b49c98936c581ff8eb2bf1532093bd35c76935b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:04:36 +0000

    Add package whitespacesv/0.1.0/whitespacesv-0.1.0-py3-none-any.whl

---
+++ packages/whitespacesv/whitespacesv-0.1.0-py3-none-any.whl/whitespacesv/__init__.py
@@ -0,0 +1,9 @@
+"""The main module of the whitespacesv package."""
+
+from __future__ import annotations
+
+from whitespacesv.document import WsvDocument
+from whitespacesv.utils import reinfer_types
+
+__version__ = "0.1.0"
+__all__ = ["WsvDocument", "reinfer_types"]
