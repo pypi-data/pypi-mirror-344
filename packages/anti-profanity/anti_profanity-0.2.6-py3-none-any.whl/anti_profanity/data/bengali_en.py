704e843c599be4852c071ad588ccf22488a2e702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 15:04:13 +0000

    Add package anti-profanity/0.2.6/anti_profanity-0.2.6-py3-none-any.whl

---
+++ packages/anti-profanity/anti_profanity-0.2.6-py3-none-any.whl/anti_profanity/data/bengali_en.py
@@ -0,0 +1,6 @@
+# ////////// Contribution needed ///////////////////
+
+BengaliProfanity = [
+    "bokachoda", "abal", "gand", "gudmarani", "khankirchele", "machuda", "bonchod",
+    "harami", "pagolchoda", "hol", "besya", "bal", "mang", "chudirbhai"
+]
