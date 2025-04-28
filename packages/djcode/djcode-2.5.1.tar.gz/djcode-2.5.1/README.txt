f868d8ac6d965048940c6590d94b1558523b0dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 14:04:50 +0000

    Add package djcode/2.5.1/djcode-2.5.1.tar.gz

---
+++ packages/djcode/djcode-2.5.1.tar.gz/djcode-2.5.1/README.txt
@@ -0,0 +1,6 @@
+# change the version in setup file 
+# delete egg-info folder 
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/* 
+# Generate new token for project and paste (pypi-AgEIcHlwaS5vcmcCJGQ1NjBhN2FhLTBkNTMtNDEyNC05ODA1LWJkYzFkZjY5MGE0MwACDlsxLFsiZGpjb2RlIl1dAAIsWzIsWyIwYmNlZjQxZC0zZGEzLTQ1MjEtODFiNi05NWRhOWM0MWYyMGQiXV0AAAYgG2tBVDyr0OtQ4CAH7Ad0ZmEhbSRLsWu8QNcf6hEKdCA)
+# done
---
+++ packages/djcode/djcode-2.5.1.tar.gz/djcode-2.5.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='djcode',
+  version='2.5.1',
+  description='DJCODE',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Daksh Rathod',
+  author_email='dakshbrathod@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['djcode'], 
+  packages=find_packages(),
+)
