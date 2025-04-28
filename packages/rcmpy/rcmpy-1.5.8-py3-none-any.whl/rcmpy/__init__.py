a5a86adc206e822ff39813ae4edf3509d82d8633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 14:05:06 +0000

    Add package rcmpy/1.5.8/rcmpy-1.5.8-py3-none-any.whl

---
+++ packages/rcmpy/rcmpy-1.5.8-py3-none-any.whl/rcmpy/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.2.1
+# hash=669148a99b09e995f293d2564a0f35f8
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A configuration-file management system."
+PKG_NAME = "rcmpy"
+VERSION = "1.5.8"
---
+++ packages/rcmpy/rcmpy-1.5.8-py3-none-any.whl/rcmpy/commands/all.py
@@ -0,0 +1,56 @@
+# =====================================
+# generator=datazen
+# version=3.2.1
+# hash=2b9927a9d538a17ed80c0411b8d02364
+# =====================================
+
+"""
+A module aggregating package commands.
+"""
+
+# built-in
+from typing import List as _List
+from typing import Tuple as _Tuple
+
+# third-party
+from vcorelib.args import CommandRegister as _CommandRegister
+
+# internal
+from rcmpy.commands.apply import add_apply_cmd
+from rcmpy.commands.dump import add_dump_cmd
+from rcmpy.commands.use import add_use_cmd
+from rcmpy.commands.variant import add_variant_cmd
+from rcmpy.commands.watch import add_watch_cmd
+
+
+def commands() -> _List[_Tuple[str, str, _CommandRegister]]:
+    """Get this package's commands."""
+
+    return [
+        (
+            "apply",
+            "apply any pending changes from the active data repository",
+            add_apply_cmd,
+        ),
+        (
+            "dump",
+            "dump template data to stdout as JSON",
+            add_dump_cmd,
+        ),
+        (
+            "use",
+            "set the directory to use as the rcmpy data repository",
+            add_use_cmd,
+        ),
+        (
+            "variant",
+            "set the variant of configuration data to use",
+            add_variant_cmd,
+        ),
+        (
+            "watch",
+            "do a task whenever a file in a specified directory changes",
+            add_watch_cmd,
+        ),
+        ("noop", "command stub (does nothing)", lambda _: lambda _: 0),
+    ]
