bfdfa80f7d93415b806f8eb134ba6ba2972a6324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 16:04:59 +0000

    Add package kr8s/0.20.7/kr8s-0.20.7.tar.gz

---
+++ packages/kr8s/kr8s-0.20.7.tar.gz/kr8s-0.20.7/kr8s/asyncio/__init__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2025, Kr8s Developers (See LICENSE for list)
+# SPDX-License-Identifier: BSD 3-Clause License
+"""The `kr8s` asynchronous API.
+
+This module provides an asynchronous API for interacting with a Kubernetes cluster.
+"""
+from kr8s._api import Api
+
+from . import objects, portforward
+from ._api import api
+from ._helpers import api_resources, get, version, watch, whoami
+
+__all__ = [
+    "api",
+    "api_resources",
+    "get",
+    "objects",
+    "portforward",
+    "version",
+    "watch",
+    "whoami",
+    "Api",
+]
