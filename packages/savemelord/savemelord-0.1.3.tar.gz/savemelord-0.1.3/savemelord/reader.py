79221443891f81778350fb318e8bc991d4647207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:19 +0000

    Add package savemelord/0.1.3/savemelord-0.1.3.tar.gz

---
+++ packages/savemelord/savemelord-0.1.3.tar.gz/savemelord-0.1.3/savemelord/reader.py
@@ -0,0 +1,42 @@
+import pkgutil
+
+def exp1():
+    data = pkgutil.get_data(__package__, 'gridworld.txt')
+    print(data.decode('utf-8'))
+
+def sarsa_v_q():
+    data = pkgutil.get_data(__package__, 'sarsavq.txt')
+    print(data.decode('utf-8'))
+
+def bandit_problem():
+    data = pkgutil.get_data(__package__, 'banditproblem.txt')
+    print(data.decode('utf-8'))
+
+def sampleAverage():
+    data = pkgutil.get_data(__package__, 'sampleaverage.txt')
+    print(data.decode('utf-8'))
+
+def ucb_and_optimal_val():
+    data = pkgutil.get_data(__package__, 'ucb_and_optimal_val.txt')
+    print(data.decode('utf-8'))
+
+def policyiter_policyeval():
+    data = pkgutil.get_data(__package__, 'policyiter_policyeval.txt')
+    print(data.decode('utf-8'))
+
+def value_iteration_gridworld():
+    data = pkgutil.get_data(__package__, 'value_iteration_gridworld.txt')
+    print(data.decode('utf-8'))
+
+def doubleQL():
+    data = pkgutil.get_data(__package__, 'doubleQL.txt')
+    print(data.decode('utf-8'))
+
+def montecarlo():
+    data = pkgutil.get_data(__package__, 'montecarlo.txt')
+    print(data.decode('utf-8'))
+
+
+def helpme():
+    data = pkgutil.get_data(__package__, 'help.txt')
+    print(data.decode('utf-8'))
