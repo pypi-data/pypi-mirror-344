a52ff706b57211c1937e822106c5282631a0d2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 16:04:02 +0000

    Add package uncertainty-engine-types/0.3.0/uncertainty_engine_types-0.3.0-py3-none-any.whl

---
+++ packages/uncertainty-engine-types/uncertainty_engine_types-0.3.0-py3-none-any.whl/uncertainty_engine_types/model_config.py
@@ -0,0 +1,12 @@
+from typing import Literal, Optional
+
+from pydantic import BaseModel
+
+
+class ModelConfig(BaseModel):
+    input_variance: Optional[float] = None
+    input_retained_dimensions: Optional[int] = None
+    output_variance: Optional[float] = None
+    output_retained_dimensions: Optional[int] = None
+    model_type: Literal["SingleTaskGPTorch"] = "SingleTaskGPTorch"
+    seed: Optional[int] = None
