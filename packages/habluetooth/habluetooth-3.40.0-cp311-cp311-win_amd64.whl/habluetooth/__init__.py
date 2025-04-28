da1819dd2252f87e1783c19095a904973e363682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:39 +0000

    Add package habluetooth/3.40.0/habluetooth-3.40.0-cp311-cp311-win_amd64.whl

---
+++ packages/habluetooth/habluetooth-3.40.0-cp311-cp311-win_amd64.whl/habluetooth/__init__.py
@@ -0,0 +1,56 @@
+__version__ = "3.40.0"
+
+from .advertisement_tracker import (
+    TRACKER_BUFFERING_WOBBLE_SECONDS,
+    AdvertisementTracker,
+)
+from .base_scanner import BaseHaRemoteScanner, BaseHaScanner
+from .central_manager import get_manager, set_manager
+from .const import (
+    CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
+    FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
+    SCANNER_WATCHDOG_INTERVAL,
+    SCANNER_WATCHDOG_TIMEOUT,
+    UNAVAILABLE_TRACK_SECONDS,
+)
+from .manager import BluetoothManager
+from .models import (
+    BluetoothServiceInfo,
+    BluetoothServiceInfoBleak,
+    HaBluetoothConnector,
+    HaBluetoothSlotAllocations,
+    HaScannerDetails,
+    HaScannerRegistration,
+    HaScannerRegistrationEvent,
+)
+from .scanner import BluetoothScanningMode, HaScanner, ScannerStartError
+from .scanner_device import BluetoothScannerDevice
+from .wrappers import HaBleakClientWrapper, HaBleakScannerWrapper
+
+__all__ = [
+    "CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS",
+    "FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS",
+    "SCANNER_WATCHDOG_INTERVAL",
+    "SCANNER_WATCHDOG_TIMEOUT",
+    "TRACKER_BUFFERING_WOBBLE_SECONDS",
+    "UNAVAILABLE_TRACK_SECONDS",
+    "AdvertisementTracker",
+    "BaseHaRemoteScanner",
+    "BaseHaScanner",
+    "BluetoothManager",
+    "BluetoothScannerDevice",
+    "BluetoothScanningMode",
+    "BluetoothServiceInfo",
+    "BluetoothServiceInfoBleak",
+    "HaBleakClientWrapper",
+    "HaBleakScannerWrapper",
+    "HaBluetoothConnector",
+    "HaBluetoothSlotAllocations",
+    "HaScanner",
+    "HaScannerDetails",
+    "HaScannerRegistration",
+    "HaScannerRegistrationEvent",
+    "ScannerStartError",
+    "get_manager",
+    "set_manager",
+]
