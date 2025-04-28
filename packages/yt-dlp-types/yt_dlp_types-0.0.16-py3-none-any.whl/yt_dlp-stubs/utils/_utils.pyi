0ac82251b790edfba617ad58e45122d5c2e3b0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 14:04:52 +0000

    Add package yt-dlp-types/0.0.16/yt_dlp_types-0.0.16-py3-none-any.whl

---
+++ packages/yt-dlp-types/yt_dlp_types-0.0.16-py3-none-any.whl/yt_dlp-stubs/utils/_utils.pyi
@@ -0,0 +1,116 @@
+import urllib.request
+from collections.abc import Callable
+from inspect import Traceback
+from typing import Any, Literal, Protocol, TypeVar
+from typing_extensions import Self
+
+from ..YoutubeDL import YoutubeDL
+
+from _typeshed import ExcInfo
+
+__all__ = ('ExtractorError', 'HEADRequest', 'YoutubeDLError', 'int_or_none', 'parse_iso8601',
+           'sanitize_filename', 'try_get', 'unified_timestamp')
+
+_T = TypeVar('_T')
+_U = TypeVar('_U')
+
+
+def try_get(src: _T, getter: Callable[[_T], _U], type_: type[_U] = ...) -> _U | None:
+    ...
+
+
+def parse_iso8601(s: str, delimiter: str) -> int | None:
+    ...
+
+
+def int_or_none(x: Any) -> int | None:
+    ...
+
+
+class HEADRequest(urllib.request.Request):
+    def get_method(self) -> Literal['HEAD']:
+        ...
+
+
+class NO_DEFAULT:
+    ...
+
+
+def sanitize_filename(s: str, restricted: bool = ..., is_id: bool | NO_DEFAULT = ...) -> str:
+    ...
+
+
+class YoutubeDLError(Exception):
+    msg: str
+
+    def __init__(self, msg: str | None = ...) -> None:
+        ...
+
+
+class ExtractorError(YoutubeDLError):
+    def __init__(self,
+                 msg: str,
+                 tb: Traceback | None = ...,
+                 expected: bool = ...,
+                 cause: BaseException | None = ...,
+                 video_id: str | None = ...,
+                 ie: str | None = ...) -> None:
+        ...
+
+    def format_traceback(self) -> str:
+        ...
+
+    @property
+    def __msg(self) -> str:
+        ...
+
+
+class DownloadError(YoutubeDLError):
+    exc_info: ExcInfo | None
+
+    def __init__(self, msg: str | None = ..., exc_info: ExcInfo | None = None) -> None:
+        ...
+
+
+def unified_timestamp(date_str: str | None, day_first: bool = ...) -> int | float | None:
+    ...
+
+
+class classproperty:
+    def __new__(cls,
+                func: Callable[[Any], Any] | None = ...,
+                *args: list[Any],
+                **kwargs: dict[str, Any]) -> Self:
+        ...
+
+    def __init__(self,
+                 func: Callable[[Any], Any],
+                 *,
+                 cache: dict[Any, Any] | Literal[False] = ...) -> None:
+        ...
+
+    def __get__(self, _: Any, cls: type) -> Any:
+        ...
+
+
+class SupportedLogger(Protocol):
+    def __init__(self, ydl: YoutubeDL = ...) -> None:
+        ...
+
+    def debug(self, message: str) -> None:
+        ...
+
+    def info(self, message: str) -> None:
+        ...
+
+    def warning(self, message: str, *, once: bool = ...) -> None:
+        ...
+
+    def error(self, message: str, *, is_error: bool = ...) -> None:
+        ...
+
+    def stdout(self, message: str) -> None:
+        ...
+
+    def stderr(self, message: str) -> None:
+        ...
