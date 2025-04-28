9dd81fc634ff42158cfc8c642d97bc00fb4b4525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 16:04:44 +0000

    Add package PyQWebWindow/0.6.3/pyqwebwindow-0.6.3-py3-none-any.whl

---
+++ packages/PyQWebWindow/pyqwebwindow-0.6.3-py3-none-any.whl/PyQWebWindow/all.py
@@ -0,0 +1,8 @@
+from .QAppManager  import QAppManager
+from .QWebWindow   import QWebWindow
+from .QContextMenu import QContextMenu
+from .ipc.QIpc.server  import QIpcServer
+from .ipc.QIpc.client  import QIpcClient
+from .ipc.MqIpc.server import IpcServer
+from .ipc.MqIpc.client import IpcClient
+from .utils import Serializable, SerializableCallable
---
+++ packages/PyQWebWindow/pyqwebwindow-0.6.3-py3-none-any.whl/PyQWebWindow/ipc/__init__.py
@@ -0,0 +1,2 @@
+from . import QIpc
+from . import MqIpc
