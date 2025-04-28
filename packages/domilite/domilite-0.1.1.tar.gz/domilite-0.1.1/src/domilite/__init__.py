bd5a449ca12cebfee351ec187e924d87a1666443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:29 +0000

    Add package domilite/0.1.1/domilite-0.1.1.tar.gz

---
+++ packages/domilite/domilite-0.1.1.tar.gz/domilite-0.1.1/src/domilite/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present Alex Rudy <github@alexrudy.net>
+#
+# SPDX-License-Identifier: MIT
+from . import __about__
+from ._version import __version__
+
+__all__ = ["__version__", "__about__"]
