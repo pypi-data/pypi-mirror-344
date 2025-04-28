972a808671420b7ce01d3ab89f98c818df3955f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 04:04:36 +0000

    Add package Myosotis-Researches/0.0.26/myosotis_researches-0.0.26.tar.gz

---
+++ packages/Myosotis-Researches/myosotis_researches-0.0.26.tar.gz/myosotis_researches-0.0.26/README.md
@@ -0,0 +1,3 @@
+# Myosotis-Researches
+
+A repository for storing my progress of researches.
---
+++ packages/Myosotis-Researches/myosotis_researches-0.0.26.tar.gz/myosotis_researches-0.0.26/myosotis_researches/CcGAN/internal/__init__.py
@@ -0,0 +1,4 @@
+from .install_datasets import install_datasets
+from .uninstall_datasets import uninstall_datasets
+
+__all__ = ["install_datasets", "uninstall_datasets"]
