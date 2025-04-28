a8efc6bf3127e51beaa3bfd969926024ac9b8356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 15:04:15 +0000

    Add package mlc-python/0.3.4/mlc_python-0.3.4-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/mlc-python/mlc_python-0.3.4-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/mlc/dataclasses/__init__.py
@@ -0,0 +1,10 @@
+from .c_class import c_class
+from .py_class import PyClass, py_class
+from .utils import (
+    Structure,
+    add_vtable_method,
+    field,
+    prototype,
+    replace,
+    vtable_method,
+)
