c644ed85669ef42c02d796bdf979220f5fad83bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 15:04:30 +0000

    Add package blackbolt/2.0.0/blackbolt-2.0.0-py3-none-any.whl

---
+++ packages/blackbolt/blackbolt-2.0.0-py3-none-any.whl/blackbolt/main.py
@@ -0,0 +1,19 @@
+from blackbolt.utils.shell_utils import get_folder
+from blackbolt.utils.shell_utils import get_file
+from blackbolt.utils.shell_utils import remove_folder
+
+available = {'0 ' : "ML TS XAI HC(Folder)",
+             '-1' : "Remove Folder"}
+
+def echo(name = None, open = False):
+    try:
+        if name is not None:
+            name = str(name)
+        if name in ['0']    :   get_folder("ML TS XAI HC", loc = True)
+        elif name in ['-1'] :   remove_folder("ML TS XAI HC")
+        else:
+            for k, v in available.items():
+                sep = " : " if v else ""
+                print(k,v,sep = sep)
+    except Exception as error:
+        print(error)
