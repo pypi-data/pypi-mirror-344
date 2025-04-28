a60c0ef7620fdbdb202a364aeb7b25e1c282f82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:11 +0000

    Add package NeXpy/2.0.0b1/nexpy-2.0.0b1-py3-none-any.whl

---
+++ packages/NeXpy/nexpy-2.0.0b1-py3-none-any.whl/nexpy/gui/pyqt.py
@@ -0,0 +1,43 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2013-2022, NeXpy Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING, distributed with this software.
+# -----------------------------------------------------------------------------
+import os
+
+from qtpy import QtCore, QtGui, QtWidgets
+
+if QtCore.PYQT5:
+    QtVersion = 'PyQt5' + ' v' + QtCore.__version__
+    os.environ['QT_API'] = 'pyqt5'
+elif QtCore.PYQT6:
+    QtVersion = 'PyQt6' + ' v' + QtCore.__version__
+    os.environ['QT_API'] = 'pyqt6'
+elif QtCore.PYSIDE2:
+    QtVersion = 'PySide2' + ' v' + QtCore.__version__
+    os.environ['QT_API'] = 'pyside2'
+elif QtCore.PYSIDE6:
+    QtVersion = 'PySide6' + ' v' + QtCore.__version__
+    os.environ['QT_API'] = 'pyside6'
+
+
+def convert_paths(*args):
+    return [str(arg) if isinstance(arg, os.PathLike) else arg for arg in args]
+
+
+def getOpenFileName(*args, **kwargs):
+    args = convert_paths(*args)
+    fname = QtWidgets.QFileDialog.getOpenFileName(*args, **kwargs)
+    if isinstance(fname, tuple):
+        fname = fname[0]
+    return fname
+
+
+def getSaveFileName(*args, **kwargs):
+    args = convert_paths(*args)
+    fname = QtWidgets.QFileDialog.getSaveFileName(*args, **kwargs)
+    if isinstance(fname, tuple):
+        fname = fname[0]
+    return fname
