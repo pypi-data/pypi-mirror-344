5bab93d7b2fb11075052c3c23d80bcd0294479dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 14:05:06 +0000

    Add package arcsecond/3.4.0/arcsecond-3.4.0-py2.py3-none-any.whl

---
+++ packages/arcsecond/arcsecond-3.4.0-py2.py3-none-any.whl/arcsecond/api/__init__.py
@@ -0,0 +1,7 @@
+from .config import ArcsecondConfig
+from .main import ArcsecondAPI
+from .endpoint import ArcsecondAPIEndpoint
+
+__all__ = ["ArcsecondAPI",
+           "ArcsecondConfig",
+           "ArcsecondAPIEndpoint"]
