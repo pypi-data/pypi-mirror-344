009a543432e29ea840d92706f5c64d0d42f15e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 14:05:06 +0000

    Add package patreon-archiver/0.1.2/patreon_archiver-0.1.2.tar.gz

---
+++ packages/patreon-archiver/patreon_archiver-0.1.2.tar.gz/patreon_archiver-0.1.2/patreon_archiver/typing.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal, TypedDict
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from pathlib import Path
+
+
+class CommonAttributes(TypedDict):
+    url: str
+
+
+class AudioEmbedAttributes(CommonAttributes, TypedDict):
+    post_type: Literal['audio_embed']
+
+
+class AudioFileAttributes(CommonAttributes, TypedDict):
+    post_type: Literal['audio_file']
+
+
+class _PostFile(TypedDict):
+    name: str
+    url: str
+
+
+class ImageFileAttributesPostMetadata(TypedDict):
+    image_order: Sequence[str]
+
+
+class ImageFileAttributes(CommonAttributes, TypedDict):
+    post_file: _PostFile
+    post_metadata: ImageFileAttributesPostMetadata | None
+    post_type: Literal['image_file']
+
+
+class VideoEmbedAttributes(CommonAttributes, TypedDict):
+    post_type: Literal['video_embed']
+
+
+class LivestreamCrowdcastAttributes(CommonAttributes, TypedDict):
+    post_type: Literal['livestream_crowdcast']
+
+
+class PostsData(TypedDict):
+    attributes: (AudioEmbedAttributes | AudioFileAttributes
+                 | ImageFileAttributes | VideoEmbedAttributes | LivestreamCrowdcastAttributes)
+    id: str
+
+
+class _Links(TypedDict):
+    next: str | None
+
+
+class Posts(TypedDict):
+    data: Sequence[PostsData]
+    links: _Links
+
+
+class MediaDataAttributesImageURLs(TypedDict):
+    original: str
+
+
+class MediaDataAttributes(TypedDict):
+    image_urls: MediaDataAttributesImageURLs
+    mimetype: str
+
+
+class MediaData(TypedDict):
+    attributes: MediaDataAttributes
+    id: str
+
+
+class Media(TypedDict):
+    data: MediaData
+
+
+class SaveInfo(TypedDict):
+    post_data_dict: PostsData
+    target_dir: Path
