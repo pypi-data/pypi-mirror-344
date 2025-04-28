5b11505d5d2fdd78ad349ba4c988d4bd74594710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:31 +0000

    Add package neatfile/3.0.4/neatfile-3.0.4.tar.gz

---
+++ packages/neatfile/neatfile-3.0.4.tar.gz/neatfile-3.0.4/src/neatfile/utils/nlp.py
@@ -0,0 +1,19 @@
+"""Natural language processing utilities."""
+
+import subprocess  # noqa: S404
+import sys
+
+import cappa
+import spacy
+
+from .pretty_print import pp
+
+try:
+    nlp = spacy.load("en_core_web_md")
+except OSError as e:
+    python = str(sys.executable)
+    pp.rule("Downloading spaCy model...")
+    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_md"], check=False)  # noqa: S603
+    pp.rule()
+    pp.info(":rocket: Model downloaded successfully. Run `neatfile` again.")
+    raise cappa.Exit() from e
