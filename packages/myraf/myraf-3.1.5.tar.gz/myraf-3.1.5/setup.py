36945a9e265ba2ac6d955d2a1037f56a84b4ee07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 15:04:13 +0000

    Add package myraf/3.1.5/myraf-3.1.5.tar.gz

---
+++ packages/myraf/myraf-3.1.5.tar.gz/myraf-3.1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myraf',
+    version='3.1.5',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    py_modules=['main'],
+    entry_points={
+        'console_scripts': [
+            'myraf = myraf_gui:main',
+            'im = commands:main',
+        ],
+    },
+    install_requires=[
+    ],
+)
