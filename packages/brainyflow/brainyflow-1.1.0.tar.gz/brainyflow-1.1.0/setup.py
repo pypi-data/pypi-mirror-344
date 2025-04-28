9097e94368c69a3bdeb496334971f1d50927a0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:11 +0000

    Add package brainyflow/1.1.0/brainyflow-1.1.0.tar.gz

---
+++ packages/brainyflow/brainyflow-1.1.0.tar.gz/brainyflow-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from setuptools import setup
+
+# Read the README.md for the long description
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='brainyflow',
+    version='1.1.0',
+    py_modules=['brainyflow'],
+    author="Victor Duarte",
+    description="Minimalist AI framework in 200 Lines. Enable LLMs to Program Themselves.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://flow.brainy.sh",
+)
