0076492cdb16a7e13b2324726f26775f386c57b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:23 +0000

    Add package pan-scm-sdk/0.3.27/pan_scm_sdk-0.3.27-py3-none-any.whl

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.3.27-py3-none-any.whl/scm/models/setup/__init__.py
@@ -0,0 +1,23 @@
+from .folder import (
+    FolderCreateModel,
+    FolderResponseModel,
+    FolderUpdateModel,
+)
+from .snippet import (
+    FolderReference,
+    SnippetBaseModel,
+    SnippetCreateModel,
+    SnippetResponseModel,
+    SnippetUpdateModel,
+)
+
+__all__ = [
+    "FolderCreateModel",
+    "FolderResponseModel",
+    "FolderUpdateModel",
+    "FolderReference",
+    "SnippetBaseModel",
+    "SnippetCreateModel",
+    "SnippetResponseModel",
+    "SnippetUpdateModel",
+]
