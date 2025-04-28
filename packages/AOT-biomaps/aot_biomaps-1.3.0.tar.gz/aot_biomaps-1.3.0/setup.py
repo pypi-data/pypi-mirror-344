cea66fd1148f3c08b72260625415c110303650f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:17 +0000

    Add package AOT-biomaps/1.3.0/aot_biomaps-1.3.0.tar.gz

---
+++ packages/AOT-biomaps/aot_biomaps-1.3.0.tar.gz/aot_biomaps-1.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys
+import os
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)  # Run the standard install process
+        subprocess.check_call([sys.executable, os.path.join(os.path.dirname(__file__), 'scripts', 'post_install.py')])
+
+setup(
+    name='AOT_biomaps',
+    version='1.3.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'k-wave-python',
+        'GPUtil'
+    ],
+    author='Lucas Duclos',
+    author_email='lucas.duclos@universite-paris-saclay.fr',
+    description='Acousto-Optic Tomography',
+    url='https://github.com/LucasDuclos/AcoustoOpticTomography',
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
