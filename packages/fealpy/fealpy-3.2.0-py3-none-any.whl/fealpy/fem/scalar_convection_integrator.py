646977d0cd67d3bbe0e075fbcc9ce3b49228b2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 16:04:01 +0000

    Add package fealpy/3.2.0/fealpy-3.2.0-py3-none-any.whl

---
+++ packages/fealpy/fealpy-3.2.0-py3-none-any.whl/fealpy/fem/scalar_convection_integrator.py
@@ -0,0 +1,82 @@
+from typing import Optional
+
+from ..backend import backend_manager as bm
+from ..typing import TensorLike, Index, _S
+from ..utils import is_tensor
+
+from ..mesh import HomogeneousMesh
+from ..functionspace.space import FunctionSpace as _FS
+from ..utils import process_coef_func
+from ..functional import bilinear_integral
+from .integrator import (
+    LinearInt, OpInt, CellInt,
+    enable_cache,
+    assemblymethod,
+    CoefLike
+)
+
+class ScalarConvectionIntegrator(LinearInt, OpInt, CellInt):
+    r"""The convection integrator for function spaces based on homogeneous meshes."""
+    def __init__(self, coef: Optional[CoefLike]=None, q: Optional[int]=None, *,
+                 index: Index=_S,
+                 batched: bool=False,
+                 method: Optional[str]=None) -> None:
+        method = 'assembly' if (method is None) else method
+        super().__init__(method=method)
+        self.coef = coef
+        self.q = q
+        self.index = index
+        self.batched = batched
+
+    @enable_cache
+    def to_global_dof(self, space: _FS) -> TensorLike:
+        return space.cell_to_dof()[self.index]
+
+    @enable_cache
+    def fetch(self, space: _FS):
+        index = self.index
+        mesh = getattr(space, 'mesh', None)
+
+        if not isinstance(mesh, HomogeneousMesh):
+            raise RuntimeError("The ScalarConvectionIntegrator only support spaces on"
+                               f"homogeneous meshes, but {type(mesh).__name__} is"
+                               "not a subclass of HomoMesh.")
+
+        cm = mesh.entity_measure('cell', index=index)
+        q = space.p+3 if self.q is None else self.q
+        qf = mesh.quadrature_formula(q, 'cell')
+        bcs, ws = qf.get_quadrature_points_and_weights()
+        gphi = space.grad_basis(bcs, index=index)
+        phi = space.basis(bcs, index=index)
+        return bcs, ws, phi, gphi, cm, index
+
+    def assembly(self, space: _FS) -> TensorLike:
+        coef = self.coef
+        mesh = getattr(space, 'mesh', None)
+        bcs, ws, phi, gphi, cm, index = self.fetch(space)
+        coef = process_coef_func(coef, bcs=bcs, mesh=mesh, etype='cell', index=index)
+        if is_tensor(coef):
+            gphi = bm.einsum('cqi...j, cq...j->cqi...' ,gphi, coef)
+            result = bilinear_integral(phi, gphi, ws, cm, coef=None, batched=self.batched)
+        else:
+            raise TypeError(f"coef should be Tensor, but got {type(coef)}.")
+        return result
+    
+    @assemblymethod('isopara')
+    def isopara_assembly(self, space: _FS) -> TensorLike:
+        coef = self.coef
+        mesh = getattr(space, 'mesh', None)
+        bcs, ws, phi, gphi, cm, index = self.fetch(space)
+        coef = process_coef_func(coef, bcs=bcs, mesh=mesh, etype='cell', index=index)
+
+        rm = space.mesh.reference_cell_measure()
+        J = space.mesh.jacobi_matrix(bcs)
+        G = space.mesh.first_fundamental_form(J)
+        d = bm.sqrt(bm.linalg.det(G))
+
+        if is_tensor(coef):
+            gphi = bm.einsum('cqi...j, cq...j->cqi...' ,gphi, coef)
+            result = bm.einsum('q, cqi, cqj , cq -> cij', ws*rm, phi, gphi, d)
+        else:
+            raise TypeError(f"coef should be Tensor, but got {type(coef)}.")
+        return result
---
+++ packages/fealpy/fealpy-3.2.0-py3-none-any.whl/fealpy/pde/semilinear_2d.py
@@ -0,0 +1,69 @@
+
+from ..backend import backend_manager as bm
+from ..decorator import cartesian
+from typing import Sequence
+
+PI = bm.pi
+class SemilinearData():
+    def __init__(self, domain:Sequence[float]):
+
+        self.domain = domain
+
+    #扩散项系数
+    @cartesian
+    def diffusion_coefficient(self, p):
+
+        return 10
+
+    @cartesian
+    def diffusion_coefficient_right(self, p):
+
+        return -self.diffusion_coefficient(p)
+
+    #反应项系数
+    @cartesian
+    def reaction_coefficient(self, p):
+
+        return 1
+
+    @cartesian
+    def reaction_coefficient_right(self, p):
+
+        return -self.reaction_coefficient(p)
+
+    #真解
+    @cartesian
+    def solution(self, p):
+
+        x = p[..., 0]
+        y = p[..., 1]
+        sol = bm.zeros(p.shape[:-1], dtype=bm.float64)
+        sol[:] = bm.sin(PI * x) * bm.sin(PI * y)
+        return sol
+
+    #真解的梯度
+    @cartesian
+    def gradient(self, p):
+
+        x = p[..., 0]
+        y = p[..., 1]
+        grad = bm.zeros(p.shape, dtype = bm.float64)
+        grad[..., 0] = PI * bm.cos(PI * x) * bm.sin(PI * y)
+        grad[..., 1] = PI * bm.sin(PI * x) * bm.cos(PI * y)
+        return grad
+
+    #源项
+    @cartesian
+    def source(self, p):
+
+        sol = self.solution(p)
+        a = self.diffusion_coefficient(p)
+        b = self.reaction_coefficient(p)
+        f = bm.zeros(p.shape[:-1], dtype = bm.float64)
+        f[:] = 2 * a * PI**2 * sol + b * sol**3
+        return f
+
+    #边界条件
+    @cartesian                       
+    def dirichlet(self,p):
+        return self.solution(p)
