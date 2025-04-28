d6a1fba5545558f980301ac8506f726076946813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-28 03:20:15 +0000

    Add package torchio/0.20.8/torchio-0.20.8-py3-none-any.whl

---
+++ packages/torchio/torchio-0.20.8-py3-none-any.whl/torchio/transforms/augmentation/spatial/__init__.py
@@ -0,0 +1,17 @@
+from .random_affine import Affine
+from .random_affine import RandomAffine
+from .random_anisotropy import RandomAnisotropy
+from .random_elastic_deformation import ElasticDeformation
+from .random_elastic_deformation import RandomElasticDeformation
+from .random_flip import Flip
+from .random_flip import RandomFlip
+
+__all__ = [
+    'RandomFlip',
+    'Flip',
+    'RandomAffine',
+    'Affine',
+    'RandomAnisotropy',
+    'RandomElasticDeformation',
+    'ElasticDeformation',
+]
