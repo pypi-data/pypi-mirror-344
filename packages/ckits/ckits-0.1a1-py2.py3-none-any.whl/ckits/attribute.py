e2998264f44d60247874e042f2a4feac65644ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:57 +0000

    Add package ckits/0.1a1/ckits-0.1a1-py2.py3-none-any.whl

---
+++ packages/ckits/ckits-0.1a1-py2.py3-none-any.whl/ckits/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "ckits"
+__version__ = "0.1.alpha.1"
+__urlhome__ = "https://github.com/podboy/ckits"
+__description__ = "containers kits"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
