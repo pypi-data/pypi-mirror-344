64d0f52bb71646c7234c31c98e78ad36004834f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:22 +0000

    Add package caesartool/0.1.5/caesartool-0.1.5.tar.gz

---
+++ packages/caesartool/caesartool-0.1.5.tar.gz/caesartool-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="caesartool",  # Updated name
+    version="0.1.5",
+    packages=find_packages(),
+    entry_points={  # Updated entry point to reflect new package name
+        'console_scripts': [
+            'caesartool = caesartool.cli:main',  # Updated CLI entry point
+        ],
+    },
+    install_requires=[],
+    author="Farhan Khan",
+    author_email="ftkover9k@gmail.com",
+    description="A Caesar Cipher encryption and decryption program",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ftkovr9k/Caesar-Encryption-Program",
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
