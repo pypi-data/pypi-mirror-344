99d78266249d2eac100c433ec23beab9cbf59348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 16:05:26 +0000

    Add package evn/0.7.8/evn-0.7.8-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/evn/evn-0.7.8-cp312-cp312-musllinux_1_2_aarch64.whl/evn/testing/__init__.py
@@ -0,0 +1,8 @@
+from evn.testing.pytest import *
+import evn.testing.quicktest as qt
+from evn.testing.quicktest import *
+from evn.testing.parametrized import *
+from evn.testing.gen_tests import *
+from evn.testing.testapp import *
+
+__all__ = ['quicktest', 'TestApp']
