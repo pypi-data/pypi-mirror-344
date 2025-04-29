dfcff139a58db454d1e3867c4eda622e47f682cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:04:31 +0000

    Add package lumaai/1.8.0/lumaai-1.8.0-py3-none-any.whl

---
+++ packages/lumaai/lumaai-1.8.0-py3-none-any.whl/lumaai/resources/generations/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .image import (
+    ImageResource,
+    AsyncImageResource,
+    ImageResourceWithRawResponse,
+    AsyncImageResourceWithRawResponse,
+    ImageResourceWithStreamingResponse,
+    AsyncImageResourceWithStreamingResponse,
+)
+from .video import (
+    VideoResource,
+    AsyncVideoResource,
+    VideoResourceWithRawResponse,
+    AsyncVideoResourceWithRawResponse,
+    VideoResourceWithStreamingResponse,
+    AsyncVideoResourceWithStreamingResponse,
+)
+from .generations import (
+    GenerationsResource,
+    AsyncGenerationsResource,
+    GenerationsResourceWithRawResponse,
+    AsyncGenerationsResourceWithRawResponse,
+    GenerationsResourceWithStreamingResponse,
+    AsyncGenerationsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ImageResource",
+    "AsyncImageResource",
+    "ImageResourceWithRawResponse",
+    "AsyncImageResourceWithRawResponse",
+    "ImageResourceWithStreamingResponse",
+    "AsyncImageResourceWithStreamingResponse",
+    "VideoResource",
+    "AsyncVideoResource",
+    "VideoResourceWithRawResponse",
+    "AsyncVideoResourceWithRawResponse",
+    "VideoResourceWithStreamingResponse",
+    "AsyncVideoResourceWithStreamingResponse",
+    "GenerationsResource",
+    "AsyncGenerationsResource",
+    "GenerationsResourceWithRawResponse",
+    "AsyncGenerationsResourceWithRawResponse",
+    "GenerationsResourceWithStreamingResponse",
+    "AsyncGenerationsResourceWithStreamingResponse",
+]
---
+++ packages/lumaai/lumaai-1.8.0-py3-none-any.whl/lumaai/types/generations/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .image_create_params import ImageCreateParams as ImageCreateParams
+from .video_create_params import VideoCreateParams as VideoCreateParams
