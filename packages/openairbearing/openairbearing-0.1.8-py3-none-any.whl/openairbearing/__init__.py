cf86e91035c889ecb9d95bb81e61e114da6aa4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:21:02 +0000

    Add package openairbearing/0.1.8/openairbearing-0.1.8-py3-none-any.whl

---
+++ packages/openairbearing/openairbearing-0.1.8-py3-none-any.whl/openairbearing/__init__.py
@@ -0,0 +1,81 @@
+"""OpenAir - Air Bearing Analysis Tool.
+
+This package provides tools for analyzing and visualizing air bearing performance.
+"""
+
+# Version information
+__version__ = "0.1.8"
+
+# Import constants from config (where they're actually defined)
+from .config import DEMO_MODE
+
+# Import specific bearing classes
+from .bearings import (
+    RectangularBearing,
+    CircularBearing,
+    AnnularBearing,
+    InfiniteLinearBearing,
+    JournalBearing,
+)
+
+# Import utility functions from where they're defined
+from .utils import (
+    get_kappa,
+    get_Qsc,
+    get_beta,
+    Result,
+    get_geom,
+    get_area,
+)
+
+# Import solver function
+from .solvers import solve_bearing, get_pressure_numeric
+
+# Import visualization functions
+from .plots import (
+    plot_bearing_shape,
+    plot_key_results,
+    plot_load_capacity,
+    plot_stiffness,
+    plot_pressure_distribution,
+    plot_supply_flow_rate,
+    plot_chamber_flow_rate,
+    plot_ambient_flow_rate,
+)
+
+# Import app
+from .app.app import app
+
+# Define what should be available when using 'from openairbearing import *'
+__all__ = [
+    # App
+    "app",
+    # Bearing types
+    "RectangularBearing",
+    "CircularBearing",
+    "AnnularBearing",
+    "InfiniteLinearBearing",
+    "JournalBearing",
+    # Bearing parameters
+    "get_kappa",
+    "get_Qsc",
+    "get_beta",
+    "get_geom",
+    "get_area",
+    # Result type
+    "Result",
+    # Solver
+    "solve_bearing",
+    "get_pressure_numeric",
+    # Configuration
+    "DEMO_MODE",
+    # Visualization
+    "plot_bearing_shape",
+    "plot_key_results",
+    "plot_load_capacity",
+    "plot_stiffness",
+    "plot_pressure_distribution",
+    "plot_supply_flow_rate",
+    "plot_chamber_flow_rate",
+    "plot_ambient_flow_rate",
+]
