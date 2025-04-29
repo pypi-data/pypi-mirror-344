f5778823c0fcd5d6ad258a73e857f273720fcb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:03:45 +0000

    Add package jb-qt/0.1.16/jb_qt-0.1.16-py3-none-any.whl

---
+++ packages/jb-qt/jb_qt-0.1.16-py3-none-any.whl/jb_qt/widgets/__init__.py
@@ -0,0 +1,16 @@
+"""Collection of widget exports"""
+
+from jb_qt.widgets.chip_button import ChipButton
+from jb_qt.widgets.chips import ChipsWidget
+from jb_qt.widgets.multiselect import MultiSelectComboBox
+from jb_qt.widgets.simple import ClickableLabel, LongIntSpinBox
+from jb_qt.widgets.toast import Toast
+
+__all__ = [
+    "ChipButton",
+    "ChipsWidget",
+    "MultiSelectComboBox",
+    "ClickableLabel",
+    "LongIntSpinBox",
+    "Toast",
+]
