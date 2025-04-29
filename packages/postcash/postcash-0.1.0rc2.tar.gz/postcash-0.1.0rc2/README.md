5e6211a6b6884b95b9c1696f01554e6c7ce337ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:04:38 +0000

    Add package postcash/0.1.0rc2/postcash-0.1.0rc2.tar.gz

---
+++ packages/postcash/postcash-0.1.0rc2.tar.gz/postcash-0.1.0rc2/README.md
@@ -0,0 +1,3 @@
+# postcash
+
+Asynchronous Notification[SMTP/Discord/Telegram] for posting notifications with Pure Python and FastAPI.
---
+++ packages/postcash/postcash-0.1.0rc2.tar.gz/postcash-0.1.0rc2/postcash/__init__.py
@@ -0,0 +1,5 @@
+# postcash/__init__.py
+
+"""Asynchronous Notification[SMTP/Discord/Telegram] for posting notifications with Pure Python and FastAPI."""
+
+__version__ = "0.1.0.pre2"
