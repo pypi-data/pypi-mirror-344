3a2dfbfacbf404c526a9ad98667ce1757f60ee61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:48 +0000

    Add package encord/0.1.162/encord-0.1.162.tar.gz

---
+++ packages/encord/encord-0.1.162.tar.gz/encord-0.1.162/encord/constants/string_constants.py
@@ -0,0 +1,20 @@
+# Label row constants
+
+LABELS = "labels"
+OBJECTS = "objects"
+CLASSIFICATIONS = "classifications"
+OBJECT_HASH = "objectHash"
+CLASSIFICATION_HASH = "classificationHash"
+OBJECT_ANSWERS = "object_answers"
+CLASSIFICATION_ANSWERS = "classification_answers"
+
+
+# Labeling algorithm names
+INTERPOLATION = "interpolation"
+FITTED_BOUNDING_BOX = "fitted_bounding_box"
+
+
+# Type of Cord API key
+TYPE_PROJECT = "project"
+TYPE_DATASET = "dataset"
+TYPE_ONTOLOGY = "ontology"
