5f2e0196399404477776e64e7c233c29a22d4475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:39 +0000

    Add package snapdroid/1.0.4/snapdroid-1.0.4.tar.gz

---
+++ packages/snapdroid/snapdroid-1.0.4.tar.gz/snapdroid-1.0.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+import re
+
+# Hardcoded version for now
+version = '1.0.4'
+
+# Read long description from README.md
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="snapdroid",
+    version=version,
+    description="Android Screenshot and Screen Recording Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Siddharth Joshi",
+    url="https://github.com/dr34mhacks/snapdroid",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "snapdroid=snapdroid.snapdroid:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Monitoring",
+    ],
+    python_requires=">=3.6",
+    keywords="android, screenshot, screen recording, adb, testing, security",
+)
---
+++ packages/snapdroid/snapdroid-1.0.4.tar.gz/snapdroid-1.0.4/snapdroid/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Command-line entry point for SnapDroid
+"""
+
+from snapdroid.snapdroid import main
+
+if __name__ == '__main__':
+    main()
