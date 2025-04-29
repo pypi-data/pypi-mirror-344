c228d59f9f43b72ffbdec4d6d4b8415cd9fcfd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 16:04:51 +0000

    Add package cidp/0.0.1/cidp-0.0.1.tar.gz

---
+++ packages/cidp/cidp-0.0.1.tar.gz/cidp-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="cidp", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="skt.mccho@sk.com",
+    description="CIDP Python SDK",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://gitlab.cidp.io/common/pypi/cidp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
