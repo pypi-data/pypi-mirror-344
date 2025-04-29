f1c4392b367603f4f60effd969f3720b8388275c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:19:53 +0000

    Add package glassgen/0.1.13/glassgen-0.1.13.tar.gz

---
+++ packages/glassgen/glassgen-0.1.13.tar.gz/glassgen-0.1.13/glassgen/generator/__init__.py
@@ -0,0 +1,6 @@
+from glassgen.generator.generator import Generator
+from glassgen.generator.batch_controller import DynamicBatchController
+from glassgen.generator.duplication import DuplicateController
+from glassgen.generator.generators import GeneratorRegistry
+
+__all__ = ["Generator", "DynamicBatchController", "DuplicateController", "GeneratorRegistry"]
---
+++ packages/glassgen/glassgen-0.1.13.tar.gz/glassgen-0.1.13/glassgen/sinks/__init__.py
@@ -0,0 +1,22 @@
+from glassgen.sinks.base import BaseSink
+from glassgen.sinks.csv_sink import CSVSink
+from glassgen.sinks.webhook_sink import WebHookSink
+from glassgen.sinks.yield_sink import YieldSink
+from glassgen.sinks.kafka_sink import KafkaSink
+
+__all__ = ["BaseSink", "CSVSink", "SinkFactory", "KafkaSink", "WebHookSink", "YieldSink"]
+
+
+class SinkFactory:
+    _sinks = {
+        "csv": CSVSink,        
+        "webhook": WebHookSink,
+        "yield": YieldSink,
+        "kafka": KafkaSink,
+    }
+
+    @classmethod
+    def create(cls, class_type: str, sink_config: dict):
+        if class_type not in cls._sinks:
+            raise ValueError(f"Unknown class_type: {class_type}")
+        return cls._sinks[class_type](sink_config)
