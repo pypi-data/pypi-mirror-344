37e9f8177555accf44ad66880afeb9ff6c4d1956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:05:09 +0000

    Add package kudet/0.0.1/kudet-0.0.1.tar.gz

---
+++ packages/kudet/kudet-0.0.1.tar.gz/kudet-0.0.1/src/kudet/math_operations.py
@@ -0,0 +1,7 @@
+def add(a: float, b: float) -> float:
+    """Return the sum of two numbers."""
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    """Return the difference between two numbers."""
+    return a - b
