0cc969982afc4df2ed820f91f670b352465c4483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 17:04:55 +0000

    Add package ail-typo-squatting/2.7.5/ail_typo_squatting-2.7.5-py3-none-any.whl

---
+++ packages/ail-typo-squatting/ail_typo_squatting-2.7.5-py3-none-any.whl/ail_typo_squatting/__init__.py
@@ -0,0 +1,27 @@
+from ail_typo_squatting.typo import runAll
+
+from ail_typo_squatting.typo import formatOutput
+from ail_typo_squatting.typo import dnsResolving
+
+from ail_typo_squatting.typo import omission
+from ail_typo_squatting.typo import repetition
+from ail_typo_squatting.typo import changeOrder
+from ail_typo_squatting.typo import replacement
+from ail_typo_squatting.typo import doubleReplacement
+from ail_typo_squatting.typo import addition
+from ail_typo_squatting.typo import missingDot
+from ail_typo_squatting.typo import stripDash
+from ail_typo_squatting.typo import vowelSwap
+from ail_typo_squatting.typo import addDash
+from ail_typo_squatting.typo import homoglyph
+from ail_typo_squatting.typo import commonMisspelling
+from ail_typo_squatting.typo import homophones
+from ail_typo_squatting.typo import wrongTld
+from ail_typo_squatting.typo import wrongSld
+from ail_typo_squatting.typo import addTld
+from ail_typo_squatting.typo import subdomain
+from ail_typo_squatting.typo import singularPluralize
+from ail_typo_squatting.typo import changeDotDash
+from ail_typo_squatting.typo import addDynamicDns
+from ail_typo_squatting.generator.utils.generator_functions import check_valid_domain
+from ail_typo_squatting.typo import numeralSwap
