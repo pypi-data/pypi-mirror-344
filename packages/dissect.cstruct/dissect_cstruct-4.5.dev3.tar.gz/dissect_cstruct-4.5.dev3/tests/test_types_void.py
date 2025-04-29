a7647e375f98d5cd96563ebdeb10a8c541fb6493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:03:51 +0000

    Add package dissect.cstruct/4.5.dev3/dissect_cstruct-4.5.dev3.tar.gz

---
+++ packages/dissect.cstruct/dissect_cstruct-4.5.dev3.tar.gz/dissect_cstruct-4.5.dev3/tests/test_types_void.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import io
+from typing import TYPE_CHECKING
+
+from .utils import verify_compiled
+
+if TYPE_CHECKING:
+    from dissect.cstruct.cstruct import cstruct
+
+
+def test_void_read(cs: cstruct) -> None:
+    assert not cs.void
+
+    stream = io.BytesIO(b"AAAA")
+    assert not cs.void(stream)
+
+    assert stream.tell() == 0
+
+
+def test_void_write(cs: cstruct) -> None:
+    assert cs.void().dumps() == b""
+
+
+def test_void_array_read(cs: cstruct) -> None:
+    assert not cs.void[4]
+
+    stream = io.BytesIO(b"AAAA")
+    assert not any(cs.void[4](stream))
+    assert not any(cs.void[None](stream))
+    assert stream.tell() == 0
+
+
+def test_void_array_write(cs: cstruct) -> None:
+    assert cs.void[4](b"AAAA").dumps() == b""
+    assert cs.void[None](b"AAAA").dumps() == b""
+
+
+def test_void_default(cs: cstruct) -> None:
+    assert cs.void() == cs.void.__default__()
+    assert not cs.void()
+    assert not cs.void.__default__()
+
+    assert cs.void[1].__default__() == [cs.void()]
+    assert cs.void[None].__default__() == []
+
+
+def test_void_struct(cs: cstruct, compiled: bool) -> None:
+    cdef = """
+    struct test {
+        void    a;
+        void    b[4];
+        void    c[];
+    };
+    """
+    cs.load(cdef, compiled=compiled)
+
+    assert verify_compiled(cs.test, compiled)
+
+    stream = io.BytesIO(b"AAAA")
+
+    obj = cs.test(stream)
+    assert not obj.a
+    assert not any(obj.b)
+    assert not any(obj.c)
+
+    assert stream.tell() == 0
+
+    assert obj.dumps() == b""
