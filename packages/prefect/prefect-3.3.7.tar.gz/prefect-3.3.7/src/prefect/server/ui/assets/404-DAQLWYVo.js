11b686fb5dc6e587c2270efe41b8918eca5bb857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:04:34 +0000

    Add package prefect/3.3.7/prefect-3.3.7.tar.gz

---
+++ packages/prefect/prefect-3.3.7.tar.gz/prefect-3.3.7/src/prefect/server/ui/assets/404-DAQLWYVo.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dB as n}from"./index-DHPxdPUs.js";import{u as r}from"./usePageTitle-N_Vwn3MG.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-DAQLWYVo.js.map
