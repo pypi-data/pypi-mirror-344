5d7d8836f9a8f8dd43c5f6c4d27e3da6ce82abe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:20:10 +0000

    Add package gtec-gds/1.0.0/gtec_gds-1.0.0-cp312-cp312-win_amd64.whl

---
+++ packages/gtec-gds/gtec_gds-1.0.0-cp312-cp312-win_amd64.whl/gtec_gds/__init__.py
@@ -0,0 +1,9 @@
+# compatibility
+from __future__ import absolute_import, division, print_function
+
+# get version
+from .__version__ import __version__
+
+# allow lazy loading
+from .g_nautilus import GNautilus
+
