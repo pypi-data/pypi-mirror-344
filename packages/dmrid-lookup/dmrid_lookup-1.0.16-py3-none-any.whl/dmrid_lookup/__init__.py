b34a1b0143e6712792936e9a0e43edf4b1697be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:05:14 +0000

    Add package dmrid-lookup/1.0.16/dmrid_lookup-1.0.16-py3-none-any.whl

---
+++ packages/dmrid-lookup/dmrid_lookup-1.0.16-py3-none-any.whl/dmrid_lookup/__init__.py
@@ -0,0 +1,19 @@
+"""
+DMR ID Lookup Tool
+
+A Python package for looking up DMR IDs from radioid.net
+"""
+
+__version__ = "1.0.15"
+
+from .dmrid_lookup import main, get_dmr_ids, lookup_by_id, pretty_print, save_to_csv, ensure_venv
+
+__all__ = [
+    'main',
+    'get_dmr_ids',
+    'lookup_by_id',
+    'pretty_print',
+    'save_to_csv',
+    'ensure_venv',
+    '__version__'
+] 
