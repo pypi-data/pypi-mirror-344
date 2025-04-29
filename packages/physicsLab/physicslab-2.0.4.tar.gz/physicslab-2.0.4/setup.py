10f1b4126af22eeca7af43a7aff612cef0348956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:05:08 +0000

    Add package physicsLab/2.0.4/physicslab-2.0.4.tar.gz

---
+++ packages/physicsLab/physicslab-2.0.4.tar.gz/physicslab-2.0.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="physicsLab",
+    version="2.0.4",
+    license="MIT",
+    author="Arendelle",
+    author_email="2381642961@qq.com",
+    description="Python API for Quantum-Physics App",
+    long_description="show description in [github](https://github.com/GoodenoughPhysicsLab/physicsLab)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/GoodenoughPhysicsLab/physicsLab",
+    packages=setuptools.find_packages(include=["physicsLab", "physicsLab.*"]),
+    install_requires=["typing-extensions", "requests==2.32.3", "executing==2.2.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Chinese (Simplified)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
