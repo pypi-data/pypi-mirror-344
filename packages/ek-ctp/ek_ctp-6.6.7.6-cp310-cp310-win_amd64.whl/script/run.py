648266988da46b5eebbddc492ff0084956125ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:05:21 +0000

    Add package ek-ctp/6.6.7.6/ek_ctp-6.6.7.6-cp310-cp310-win_amd64.whl

---
+++ packages/ek-ctp/ek_ctp-6.6.7.6-cp310-cp310-win_amd64.whl/script/run.py
@@ -0,0 +1,23 @@
+from bothpy.event import EventEngine
+from bothpy.trader.engine import MainEngine
+from bothpy.trader.ui import MainWindow, create_qapp
+
+from ek_ctp import CtpGateway
+
+
+def main():
+    """主入口函数"""
+    qapp = create_qapp()
+
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)
+    main_engine.add_gateway(CtpGateway)
+
+    main_window = MainWindow(main_engine, event_engine)
+    main_window.showMaximized()
+
+    qapp.exec()
+
+
+if __name__ == "__main__":
+    main()
