55729b29bb230e8958554f596892df1010af9629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:52 +0000

    Add package factrainer-base/0.1.19/factrainer_base-0.1.19-py3-none-any.whl

---
+++ packages/factrainer-base/factrainer_base-0.1.19-py3-none-any.whl/factrainer/base/dataset.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from typing import Any, Generator, Self
+
+import numpy as np
+import numpy.typing as npt
+from pydantic import BaseModel, ConfigDict
+from sklearn.model_selection._split import _BaseKFold
+
+# type Prediction = npt.NDArray[Any] | scipy.sparse.spmatrix | list[scipy.sparse.spmatrix]
+type Prediction = npt.NDArray[np.number[Any]]
+type RowIndex = list[int]
+type RowIndices = Sequence[RowIndex]
+type Rows = int | slice | RowIndex
+type RowsAndColumns = Rows | tuple[Rows, ...]
+
+
+class BaseDataset(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class IndexableDataset(BaseDataset):
+    @abstractmethod
+    def __getitem__(self, index: RowsAndColumns) -> Self:
+        raise NotImplementedError
+
+    @abstractmethod
+    def k_fold_split(
+        self, k_fold: _BaseKFold
+    ) -> Generator[tuple[RowIndex, RowIndex], None, None]:
+        raise NotImplementedError
+
+
+class BaseDatasetEqualityChecker[T](ABC):
+    @abstractmethod
+    def check(self, left: T, right: T) -> bool:
+        raise NotImplementedError
+
+
+class BaseDatasetSlicer[T](ABC):
+    @abstractmethod
+    def slice(self, data: T, index: RowIndex) -> T:
+        raise NotImplementedError
