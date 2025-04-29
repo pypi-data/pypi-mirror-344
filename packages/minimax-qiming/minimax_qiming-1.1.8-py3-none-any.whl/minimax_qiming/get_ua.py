966a28d0cd8ffffa8abfb51c728db160908e4131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:46 +0000

    Add package minimax-qiming/1.1.8/minimax_qiming-1.1.8-py3-none-any.whl

---
+++ packages/minimax-qiming/minimax_qiming-1.1.8-py3-none-any.whl/minimax_qiming/get_ua.py
@@ -0,0 +1,32 @@
+import random
+import json
+from importlib.resources import files
+
+__all__ = ["get_pc_ua", "get_mobile_ua"]
+
+PC_USER_AGENTS = []
+
+# 动态获取 CSV 文件路径
+csv_path = files('minimax_qiming').joinpath('data/merged_unique_ua.jsonl')
+
+mb_ua_list = []
+
+# 打开 CSV 文件
+with open(csv_path, mode='r', encoding='utf-8') as file:
+    for line in file:
+        try:
+            ua = json.loads(line)['user_agent']
+            mb_ua_list.append(ua.strip('"'))
+        except:
+            continue
+        
+MOBILE_USER_AGENTS = mb_ua_list
+
+def get_pc_ua() -> str:
+    """随机选择一个 PC 端 UserAgent"""
+    return random.choice(PC_USER_AGENTS)
+
+
+def get_mobile_ua() -> str:
+    """随机选择一个 MOBILE 端 UserAgent"""
+    return random.choice(MOBILE_USER_AGENTS)
