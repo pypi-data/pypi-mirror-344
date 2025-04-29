9d2c609839a97d80da93ed5f2e4d4f212f82db14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:49 +0000

    Add package pytest-gradescope/0.1.4/pytest_gradescope-0.1.4.tar.gz

---
+++ packages/pytest-gradescope/pytest_gradescope-0.1.4.tar.gz/pytest_gradescope-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="pytest-gradescope",
+    version="0.1.4",
+    description="A pytest plugin for Gradescope integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Sebastian Ingino",
+    author_email="sebastian@ingino.me",
+    license="MIT",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    # the following makes a plugin available to pytest
+    entry_points={
+        "pytest11": [
+            "pytest-gradescope = pytest_gradescope.pytest_plugin",
+        ]
+    },
+    # custom PyPI classifier for pytest plugins
+    classifiers=[
+        "Framework :: Pytest",
+    ],
+)
