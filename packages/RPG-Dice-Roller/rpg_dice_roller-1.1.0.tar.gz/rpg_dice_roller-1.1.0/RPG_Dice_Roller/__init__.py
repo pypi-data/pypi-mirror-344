69a2595b4c9eb3f28bc2f59932b89d1066cd9e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:34 +0000

    Add package RPG-Dice-Roller/1.1.0/rpg_dice_roller-1.1.0.tar.gz

---
+++ packages/RPG-Dice-Roller/rpg_dice_roller-1.1.0.tar.gz/rpg_dice_roller-1.1.0/RPG_Dice_Roller/__init__.py
@@ -0,0 +1,8 @@
+from .src.functions.dice_roll import roll_dice
+from .src.functions.statistics import get_dice_range, get_dice_rolled
+
+__all__ = [
+    'roll_dice',
+    'get_dice_range',
+    'get_dice_rolled'
+]
