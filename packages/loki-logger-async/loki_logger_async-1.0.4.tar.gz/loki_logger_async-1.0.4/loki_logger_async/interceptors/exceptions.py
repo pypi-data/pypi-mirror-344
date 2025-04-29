09c55b33fc0ff3c923a18d0ffbbff06f3d2f03c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:20 +0000

    Add package loki-logger-async/1.0.4/loki_logger_async-1.0.4.tar.gz

---
+++ packages/loki-logger-async/loki_logger_async-1.0.4.tar.gz/loki_logger_async-1.0.4/loki_logger_async/interceptors/exceptions.py
@@ -0,0 +1,23 @@
+import sys
+import traceback
+import asyncio
+
+def intercept_exceptions(logger):
+    original_hook = sys.excepthook
+
+    def custom_hook(exc_type, exc_value, exc_traceback):
+        ts = logger.options.dynamic_labels.get("timestamp", lambda: "")()
+        error_message = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+        logger.log_buffer.append((ts, f"[EXCEPTION] {error_message}"))
+        try:
+            loop = asyncio.get_event_loop()
+            if loop.is_running():
+                loop.create_task(logger.flush_logs())
+            else:
+                asyncio.run(logger.flush_logs())
+        except Exception:
+            pass  
+
+        original_hook(exc_type, exc_value, exc_traceback)
+
+    sys.excepthook = custom_hook
---
+++ packages/loki-logger-async/loki_logger_async-1.0.4.tar.gz/loki_logger_async-1.0.4/loki_logger_async/interceptors/logging.py
@@ -0,0 +1,24 @@
+import logging
+
+
+class LokiHandler(logging.Handler):
+    def __init__(self, logger):
+        super().__init__()
+        self.logger = logger
+
+    def emit(self, record):
+        try:
+            ts = self.logger.options.dynamic_labels.get("timestamp", lambda: "")()
+            msg = self.format(record)
+            self.logger.log_buffer.append((ts, f"[{record.levelname}] {msg}"))
+            self.logger.flush_manager.check_and_flush()
+        except Exception:
+            pass
+
+def intercept_logging(logger):
+    handler = LokiHandler(logger)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(handler)
