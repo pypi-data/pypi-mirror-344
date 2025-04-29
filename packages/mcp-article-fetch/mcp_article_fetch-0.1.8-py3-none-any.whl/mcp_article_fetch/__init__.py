572907d04cc4853916d32d875b6272067ded9c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 16:04:56 +0000

    Add package mcp-article-fetch/0.1.8/mcp_article_fetch-0.1.8-py3-none-any.whl

---
+++ packages/mcp-article-fetch/mcp_article_fetch-0.1.8-py3-none-any.whl/mcp_article_fetch/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: pangff
+Date: 2025-04-27 16:06:28
+LastEditTime: 2025-04-27 16:12:37
+LastEditors: pangff
+Description: 
+FilePath: /Cline/MCP/mcp-article-fetch/src/mcp_article_fetch/__init__.py
+stay hungry,stay foolish
+'''
+__all__ = ["mcp_logger", "server"]
