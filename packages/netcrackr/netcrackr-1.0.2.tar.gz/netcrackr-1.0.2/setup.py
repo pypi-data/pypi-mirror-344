0042fd762ebd783061cb4e8e8178c5085ad777bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:48 +0000

    Add package netcrackr/1.0.2/netcrackr-1.0.2.tar.gz

---
+++ packages/netcrackr/netcrackr-1.0.2.tar.gz/netcrackr-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netcrackr",
+    version="1.0.2",
+    author="Eytin",
+    description="Network live monitor with ping, speedtest, and traceroute",
+    packages=find_packages(),
+    install_requires=[
+        "speedtest-cli",
+        "rich",
+    ],
+    entry_points={
+        "console_scripts": [
+            "netcrackr=netcrackr:cli",
+        ],
+    },
+    python_requires=">=3.7",
+)
