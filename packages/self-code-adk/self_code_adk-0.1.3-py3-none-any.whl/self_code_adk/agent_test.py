9e52f90bef258e06516e91df79be1d543df3c4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:35 +0000

    Add package self-code-adk/0.1.3/self_code_adk-0.1.3-py3-none-any.whl

---
+++ packages/self-code-adk/self_code_adk-0.1.3-py3-none-any.whl/self_code_adk/agent_test.py
@@ -0,0 +1,9 @@
+import pytest
+from self_code_adk.agent import get_my_application_code
+
+
+@pytest.mark.parametrize("dir,expected_files_number", [("self_code_adk", 3)])
+def test_autodiscover(dir, expected_files_number):
+    files, errors = get_my_application_code(dir)
+    assert expected_files_number == len(files)
+    assert 0 == len(errors)
