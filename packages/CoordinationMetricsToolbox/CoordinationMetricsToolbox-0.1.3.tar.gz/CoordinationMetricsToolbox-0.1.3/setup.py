6df14355f892ca15a2300d013733b9009faab904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:41 +0000

    Add package CoordinationMetricsToolbox/0.1.3/CoordinationMetricsToolbox-0.1.3.tar.gz

---
+++ packages/CoordinationMetricsToolbox/CoordinationMetricsToolbox-0.1.3.tar.gz/CoordinationMetricsToolbox-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CoordinationMetricsToolbox',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        # Add other dependencies here
+    ],
+     entry_points={
+        'console_scripts': [
+            'run_metrics=coordination_metrics_toolbox.metrics:main',  # Example CLI entry point
+        ],
+    },
+    author='Océane Dubois',
+    author_email='',
+    description='A toolbox for measuring and analyzing coordination metrics.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/oceanedbs/CoordinationMetricsToolbox',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
