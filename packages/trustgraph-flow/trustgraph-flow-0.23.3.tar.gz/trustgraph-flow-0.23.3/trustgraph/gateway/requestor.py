2f8b1bbc1dd3627d5be297994cb502eb9daa8d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:05:22 +0000

    Add package trustgraph-flow/0.23.3/trustgraph-flow-0.23.3.tar.gz

---
+++ packages/trustgraph-flow/trustgraph-flow-0.23.3.tar.gz/trustgraph-flow-0.23.3/trustgraph/gateway/requestor.py
@@ -0,0 +1,99 @@
+
+import asyncio
+import uuid
+import logging
+
+from .. base import Publisher
+from .. base import Subscriber
+
+logger = logging.getLogger("requestor")
+logger.setLevel(logging.INFO)
+
+class ServiceRequestor:
+
+    def __init__(
+            self,
+            pulsar_client,
+            request_queue, request_schema,
+            response_queue, response_schema,
+            subscription="api-gateway", consumer_name="api-gateway",
+            timeout=600,
+    ):
+
+        self.pub = Publisher(
+            pulsar_client, request_queue,
+            schema=request_schema,
+        )
+
+        self.sub = Subscriber(
+            pulsar_client, response_queue,
+            subscription, consumer_name,
+            response_schema
+        )
+
+        self.timeout = timeout
+
+    async def start(self):
+
+        await self.pub.start()
+        await self.sub.start()
+
+    def to_request(self, request):
+        raise RuntimeError("Not defined")
+
+    def from_response(self, response):
+        raise RuntimeError("Not defined")
+
+    async def process(self, request, responder=None):
+
+        id = str(uuid.uuid4())
+
+        try:
+
+            q = await self.sub.subscribe(id)
+
+            await self.pub.send(id, self.to_request(request))
+
+            while True:
+
+                try:
+                    resp = await asyncio.wait_for(
+                        q.get(), timeout=self.timeout
+                    )
+                except Exception as e:
+                    raise RuntimeError("Timeout")
+
+                if resp.error:
+                    err = { "error": {
+                        "type": resp.error.type,
+                        "message": resp.error.message,
+                    } }
+                    if responder:
+                        await responder(err, True)
+                    return err
+
+                resp, fin = self.from_response(resp)
+
+                print(resp, fin)
+
+                if responder:
+                    await responder(resp, fin)
+
+                if fin:
+                    return resp
+
+        except Exception as e:
+
+            logging.error(f"Exception: {e}")
+
+            err = { "error": {
+                "type": "gateway-error",
+                "message": str(e),
+            } }
+            if responder:
+                await responder(err, True)
+            return err
+
+        finally:
+            await self.sub.unsubscribe(id)
+
---
+++ packages/trustgraph-flow/trustgraph-flow-0.23.3.tar.gz/trustgraph-flow-0.23.3/trustgraph/gateway/sender.py
@@ -0,0 +1,52 @@
+
+# Like ServiceRequestor, but just fire-and-forget instead of request/response
+
+import asyncio
+import uuid
+import logging
+
+from .. base import Publisher
+
+logger = logging.getLogger("sender")
+logger.setLevel(logging.INFO)
+
+class ServiceSender:
+
+    def __init__(
+            self,
+            pulsar_client,
+            request_queue, request_schema,
+    ):
+
+        self.pub = Publisher(
+            pulsar_client, request_queue,
+            schema=request_schema,
+        )
+
+    async def start(self):
+
+        await self.pub.start()
+
+    def to_request(self, request):
+        raise RuntimeError("Not defined")
+
+    async def process(self, request, responder=None):
+
+        try:
+
+            await self.pub.send(None, self.to_request(request))
+
+            if responder:
+                await responder({}, True)
+
+        except Exception as e:
+
+            logging.error(f"Exception: {e}")
+
+            err = { "error": str(e) }
+
+            if responder:
+                await responder(err, True)
+
+            return err
+
