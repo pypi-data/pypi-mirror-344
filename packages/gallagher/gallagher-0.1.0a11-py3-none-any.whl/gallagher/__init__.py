3050a7b2c405051f03dc8ad1699ef9fc29a5a167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:04:49 +0000

    Add package gallagher/0.1.0a11/gallagher-0.1.0a11-py3-none-any.whl

---
+++ packages/gallagher/gallagher-0.1.0a11-py3-none-any.whl/gallagher/__init__.py
@@ -0,0 +1,10 @@
+"""" Gallagher Python idiomatic client
+
+ Copyright (c) 2025, Anomaly Software Pty Ltd
+
+ This project is **NOT** officially affiliated with Gallagher Security.
+
+ Distributed under the terms of the MIT License.
+"""
+
+__version__ = "0.1.0-alpha.9"
