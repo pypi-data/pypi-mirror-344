bfb3c3416a95143b53aa9e0d819c90f381b4aed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:40 +0000

    Add package annet/3.4.1/annet-3.4.1-py3-none-any.whl

---
+++ packages/annet/annet-3.4.1-py3-none-any.whl/annet_generators/rpl_example/items.py
@@ -0,0 +1,21 @@
+from annet.rpl_generators import AsPathFilter, CommunityList, CommunityType, RDFilter, ip_prefix_list
+
+AS_PATH_FILTERS = [
+    AsPathFilter("ASP_EXAMPLE", [".*123456.*"]),
+]
+COMMUNITIES = [
+    CommunityList("COMMUNITY_EXAMPLE_ADD", ["1234:1000", "1234:1001"]),
+    CommunityList("COMMUNITY_EXAMPLE_REMOVE", ["1234:999"]),
+    CommunityList("EXTCOMMUNITY_EXAMPLE_ADD", ["12345:1000"], CommunityType.RT),
+    CommunityList("EXTCOMMUNITY_EXAMPLE_REMOVE", ["12345:999"], CommunityType.RT),
+]
+
+RD_FILTERS = [
+    RDFilter("RD_EXAMPLE1", 1, ["100:1", "200:1"]),
+    RDFilter("RD_EXAMPLE2", 2, ["10.2.2.2:1", "10.3.3.3:1"]),
+]
+
+PREFIX_LISTS = [
+    ip_prefix_list("IPV6_LIST_EXAMPLE", ["2a13:5941::/32"]),
+    ip_prefix_list("IPV4_LIST_EXAMPLE", ["0.0.0.0/8", "10.0.0.0/8"]),
+]
