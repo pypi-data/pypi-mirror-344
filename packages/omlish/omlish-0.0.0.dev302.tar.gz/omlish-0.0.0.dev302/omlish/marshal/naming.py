1884a1585a1e9946317be8934f28fa640b6e2bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:04:54 +0000

    Add package omlish/0.0.0.dev302/omlish-0.0.0.dev302.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev302.tar.gz/omlish-0.0.0.dev302/omlish/marshal/naming.py
@@ -0,0 +1,26 @@
+"""
+TODO:
+ - Namer: ta.TypeAlias = ta.Callable[[str], str] ?
+  - this interface is ~intentionally~ limited, but custom overrides would be useful
+"""
+import enum
+
+from .. import lang
+from .base import Option
+
+
+class Naming(Option, enum.Enum):
+    SNAKE = 'snake'
+    CAMEL = 'camel'
+    LOW_CAMEL = 'low_camel'
+
+
+def translate_name(n: str, e: Naming) -> str:
+    if e is Naming.SNAKE:
+        return lang.snake_case(n)
+    if e is Naming.CAMEL:
+        return lang.camel_case(n)
+    if e is Naming.LOW_CAMEL:
+        r = lang.camel_case(n)
+        return (r[0].lower() + r[1:]) if r else r
+    raise ValueError(e)
