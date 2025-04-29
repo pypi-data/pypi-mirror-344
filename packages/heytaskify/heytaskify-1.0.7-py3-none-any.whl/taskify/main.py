bf5a78ec88843efafa4f812ea4b281e920f1a924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:52 +0000

    Add package heytaskify/1.0.7/heytaskify-1.0.7-py3-none-any.whl

---
+++ packages/heytaskify/heytaskify-1.0.7-py3-none-any.whl/taskify/main.py
@@ -0,0 +1,95 @@
+import pyfiglet
+import argparse
+from art import text2art
+from .task_manager import Task, TasksController
+from inspirational_quotes import quote
+import click
+from colorama import Fore, Back, Style, init
+import time
+
+init(autoreset=True)
+
+
+def main():
+    # Greetings and motivational Quote
+    quote_txt = quote()
+    print(Fore.BLACK + "Hello & Welcome to Taskify")
+    time.sleep(0.5)
+    print(Fore.YELLOW + Back.BLACK +
+          f"\nMotivational Quote: {Fore.YELLOW}{quote_txt.get("author")}: {Fore.CYAN} {quote_txt.get("quote")}")
+
+    # PARSERS CONFIGS
+    help_message = "Use these commands to use Taskify 👇\n"
+    parser = argparse.ArgumentParser(
+        prog="taskify", description="Commands for Taskify")
+    subparser = parser.add_subparsers(dest="command", required=True)
+
+    # parsers for adding tasks
+    add_task_parser = subparser.add_parser(
+        'addtask', help=f'''add a task with task title {Fore.YELLOW} EXAMPLE: py .\\main.py addtask "My Task" --description "This is a new task" --status "In Progress" --duedate 2025-12-01''')
+    add_task_parser.add_argument(
+        'taskname', nargs='+', type=str, help="add task name")
+    add_task_parser.add_argument(
+        '--description', nargs='+', type=str, help="add task description(OPTIONAL)")
+    add_task_parser.add_argument(
+        '--duedate', nargs='+', type=str, help="add duedate in dd-mm-yyyy format(OPTIONAL)")
+    add_task_parser.add_argument(
+        '--status', nargs='+', type=str, help="add status (OPTIONAL)")
+
+    # parsers for showing tasks
+    subparser.add_parser(
+        "showtasks", help=f'''show all the tasks in the db/json {Fore.YELLOW} EXAMPLE: py .\\main.py showtasks''')
+
+    # parsers for showing tasks
+    subparser.add_parser("stats", help=f'''show stats''')
+
+    # parsers for deleting tasks
+    delete_task_parser = subparser.add_parser(
+        "deletetask", help=f'''delete tasks commands {Fore.YELLOW} EXAMPLE: py .\\main.py deletetask 3''')
+    delete_task_parser.add_argument(
+        "task_number", type=int, help=f'''delete a particular task from the tasks db/json with the task number''')
+
+    # parsers for changing status
+    task_status_parser = subparser.add_parser(
+        "updatestatus", help=f'''chaning tasks status commands {Fore.YELLOW} EXAMPLE: py .\\main.py updatestatus 3 completed ''')
+    task_status_parser.add_argument(
+        "task_number", type=int, help="task number for which status needs to be changed")
+    task_status_parser.add_argument("new_status", help="add new status")
+
+    args = parser.parse_args()
+    # parser.print_help()
+    if args.command == 'addtask':
+        try:
+            if ((len(args.taskname)) > 1 and (len(args.description)) > 1 and (len(args.duedate)) > 1 and (len(args.status)) > 1):
+                for task, description, duedate, status in zip(args.taskname, args.description, args.duedate, args.status):
+                    TasksController.add_task(
+                        task, description, duedate, status or "pending")
+            elif ((len(args.taskname)) == 1):
+                task = args.taskname[0]
+                description = (args.description[0] if args.description else "")
+                duedate = (args.duedate[0] if args.duedate else "")
+                status = (args.status[0] if args.status else "pending")
+                TasksController.add_task(task, description, duedate, status)
+
+            show_tasks = input(
+                Fore.BLUE + "want to see all tasks? press y for yes and n for no? ")
+            if show_tasks == "y":
+                TasksController.show_tasks()
+            else:
+                raise Exception
+        except Exception as e:
+            print(
+                f"Please give more all properties when adding more than one task like name,status,desc,duedate ---- ERROR {e}")
+
+    elif args.command == 'showtasks':
+        TasksController.show_tasks()
+    elif args.command == 'deletetask':
+        TasksController.delete_task(args.task_number)
+    elif args.command == 'updatestatus':
+        TasksController.change_task_status(args.task_number, args.new_status)
+    elif args.command == 'stats':
+        TasksController.check_stats()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/heytaskify/heytaskify-1.0.7-py3-none-any.whl/taskify/task_manager.py
@@ -0,0 +1,90 @@
+
+import json
+import uuid
+from tabulate import tabulate
+from .storage import StorageManager
+import colorama
+from colorama import Fore, Back, Style
+import pyfiglet
+import time
+from datetime import datetime
+
+colorama.init()
+
+# class blueprint of a Task Object
+class Task:
+    def __init__(self, title, description, due_date=None, status="pending"):
+        self.title = title
+        self.description = description
+        self.status = status
+        self.task_id = str(uuid.uuid1())
+        self.due_date = due_date
+
+    def __str__(self):
+        return f"{self.task_id} - {self.title} - {self.description}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+ # class methods for creating,reading,deleting and changing task status
+
+class TasksController:
+    @staticmethod
+    def add_task(title, description, due_date=None, status="pending"):
+        try:
+            status_choices = ("pending", "in-progress", "completed")
+            if status not in status_choices:
+                raise ValueError(
+                    "Status should be one of pending or in-progress or completed")
+            if due_date:
+                if not datetime.strptime(due_date, "%d-%m-%Y"):
+                    raise ValueError("add due date in dd-mm-yyyy format")
+            task = Task(title, description, due_date, status)
+            print(f"\n{Fore.WHITE}Adding Task with title -->{Fore.YELLOW + Back.BLACK}{title} {Style.RESET_ALL}with status -->{Fore.YELLOW + Back.BLACK} {status if status else "pending"} {Style.RESET_ALL} and due date --> {Fore.YELLOW + Back.BLACK}{due_date if due_date else None} ")
+            StorageManager.add_object(task)
+            TasksController.default_formatting("Task added")
+        except Exception as e:
+            print(f" {Fore.RED} ERROR {e}")
+
+    @staticmethod
+    def get_task_index(task_number):
+        StorageManager.get(task_number)
+
+    @staticmethod
+    def show_tasks():
+        try:
+            with open("./tasks.json", "r") as file:
+                data = json.load(file)
+                tasks = data.get("tasks", [])
+                headers = ["Task Number", "Title",
+                        "Description", "Status", "Due Date", "Task ID"]
+                rows = [(task["task_number"], task["title"], task["description"],
+                        task["status"], task["due_date"] ,task["task_id"]) for task in tasks]
+            print(tabulate(rows, headers=headers, tablefmt="grid"))
+        except Exception as e:
+            print(e)
+
+
+    @staticmethod
+    def change_task_status(task_number, status):
+        StorageManager.update_object(task_number, status)
+
+    @staticmethod
+    def delete_task(task_number):
+        StorageManager.remove_objects(task_number)
+        TasksController.defaut_formatting("Task Deleted")
+
+    @staticmethod
+    def default_formatting(text):
+        time.sleep(0.5)
+        ascii_text = pyfiglet.figlet_format(f"\n{text}\n")
+        border = "+" + "-" * (len(ascii_text.splitlines()[0]) + 1) + "+"
+        for line in ascii_text.splitlines():
+            print(f"| {line} |")
+        time.sleep(0.5)
+
+    @staticmethod
+    def check_stats():
+        stats = StorageManager.json_stats()
+        print(f'''"total_tasks":{stats.get("total_tasks")}, pending_tasks":{stats.get("pending_tasks")}"completed_tasks" : {stats.get("completed_tasks")},"in_progress_tasks" : {stats.get("in_progress_tasks")},\n''')
