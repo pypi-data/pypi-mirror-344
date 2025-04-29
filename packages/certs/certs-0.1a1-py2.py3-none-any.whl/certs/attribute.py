d329a62a42c3d6e3a3835f8149d7ebec65d8926b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:19:46 +0000

    Add package certs/0.1a1/certs-0.1a1-py2.py3-none-any.whl

---
+++ packages/certs/certs-0.1a1-py2.py3-none-any.whl/certs/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "certs"
+__version__ = "0.1.alpha.1"
+__urlhome__ = "https://github.com/podboy/CustomCertificate/"
+__description__ = "Create Self-Signed SSL Certificates"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
