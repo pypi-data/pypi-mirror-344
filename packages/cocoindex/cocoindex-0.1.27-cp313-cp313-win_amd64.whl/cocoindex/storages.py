ca6b70f93cae1b752d64f6783e3753ba7553bce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:05:28 +0000

    Add package cocoindex/0.1.27/cocoindex-0.1.27-cp313-cp313-win_amd64.whl

---
+++ packages/cocoindex/cocoindex-0.1.27-cp313-cp313-win_amd64.whl/cocoindex/storages.py
@@ -0,0 +1,79 @@
+"""All builtin storages."""
+from dataclasses import dataclass
+from typing import Sequence
+
+from . import op
+from . import index
+from .auth_registry import AuthEntryReference
+
+class Postgres(op.StorageSpec):
+    """Storage powered by Postgres and pgvector."""
+
+    database: AuthEntryReference | None = None
+    table_name: str | None = None
+
+@dataclass
+class Qdrant(op.StorageSpec):
+    """Storage powered by Qdrant - https://qdrant.tech/."""
+
+    collection_name: str
+    grpc_url: str = "http://localhost:6334/"
+    api_key: str | None = None
+
+@dataclass
+class Neo4jConnection:
+    """Connection spec for Neo4j."""
+    uri: str
+    user: str
+    password: str
+    db: str | None = None
+
+@dataclass
+class TargetFieldMapping:
+    """Mapping for a graph element (node or relationship) field."""
+    source: str
+    # Field name for the node in the Knowledge Graph.
+    # If unspecified, it's the same as `field_name`.
+    target: str | None = None
+
+@dataclass
+class NodeReferenceMapping:
+    """Spec for a referenced graph node, usually as part of a relationship."""
+    label: str
+    fields: list[TargetFieldMapping]
+
+@dataclass
+class ReferencedNode:
+    """Storage spec for a graph node."""
+    label: str
+    primary_key_fields: Sequence[str]
+    vector_indexes: Sequence[index.VectorIndexDef] = ()
+
+@dataclass
+class NodeMapping:
+    """Spec to map a row to a graph node."""
+    kind = "Node"
+
+    label: str
+
+@dataclass
+class RelationshipMapping:
+    """Spec to map a row to a graph relationship."""
+    kind = "Relationship"
+
+    rel_type: str
+    source: NodeReferenceMapping
+    target: NodeReferenceMapping
+
+class Neo4j(op.StorageSpec):
+    """Graph storage powered by Neo4j."""
+
+    connection: AuthEntryReference
+    mapping: NodeMapping | RelationshipMapping
+
+class Neo4jDeclarations(op.DeclarationSpec):
+    """Declarations for Neo4j."""
+
+    kind = "Neo4j"
+    connection: AuthEntryReference
+    referenced_nodes: Sequence[ReferencedNode] = ()
