bb1f35667d141f7a9ea08835441d3f1336b7c206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:23 +0000

    Add package vercelpy/0.1.0/vercelpy-0.1.0-py3-none-any.whl

---
+++ packages/vercelpy/vercelpy-0.1.0-py3-none-any.whl/vercelpy/__init__.py
@@ -0,0 +1,10 @@
+"""
+vercelpy is a Python Wrapper around the Vercel Storage APIs.
+"""
+
+from . import blob
+from . import edge_config
+
+__author__ = 'Surya Sekhar Datta <hello@surya.dev>'
+__version__ = '0.1.0'
+__package__ = 'vercelpy'
