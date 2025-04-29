59033dde881e5795e3a30a1674e4509043cf6253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:32 +0000

    Add package tools-jsyoo61/2025.4.28.0/tools_jsyoo61-2025.4.28.0-py3-none-any.whl

---
+++ packages/tools-jsyoo61/tools_jsyoo61-2025.4.28.0-py3-none-any.whl/tools/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = "2025.04.28.0"
+from .tools import *
+from . import os
+
+'''
+For third-party packages, import manually by something like:
+import tools.pytorch
+import tools.pytorch as tpytorch
+import tools.pytorch as tpt
+import tools.pytorch as Tpt
+'''
+
+# TODO: make animator in plot/
