13c55f2f43f2ef45b53583cdd07209b49c0d5985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:19 +0000

    Add package types-grpcio/1.0.0.20250429/types_grpcio-1.0.0.20250429.tar.gz

---
+++ packages/types-grpcio/types_grpcio-1.0.0.20250429.tar.gz/types_grpcio-1.0.0.20250429/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'grpc_reflection-stubs': ['__init__.pyi', 'v1alpha/__init__.pyi', 'v1alpha/_base.pyi', 'v1alpha/reflection.pyi', 'v1alpha/reflection_pb2.pyi', 'METADATA.toml', 'py.typed'], 'grpc_health-stubs': ['__init__.pyi', 'v1/__init__.pyi', 'v1/health.pyi', 'v1/health_pb2.pyi', 'v1/health_pb2_grpc.pyi', 'METADATA.toml', 'py.typed'], 'grpc_channelz-stubs': ['__init__.pyi', 'v1/__init__.pyi', 'v1/_servicer.pyi', 'v1/channelz.pyi', 'v1/channelz_pb2.pyi', 'v1/channelz_pb2_grpc.pyi', 'METADATA.toml', 'py.typed'], 'grpc_status-stubs': ['__init__.pyi', 'rpc_status.pyi', 'METADATA.toml', 'py.typed'], 'grpc-stubs': ['__init__.pyi', 'aio/__init__.pyi', 'METADATA.toml', 'py.typed']})
