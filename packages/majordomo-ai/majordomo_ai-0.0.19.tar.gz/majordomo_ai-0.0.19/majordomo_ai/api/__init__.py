c04e2c70c0dd9f5886ae879005d895c4e5c948eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:04:27 +0000

    Add package majordomo-ai/0.0.19/majordomo_ai-0.0.19.tar.gz

---
+++ packages/majordomo-ai/majordomo_ai-0.0.19.tar.gz/majordomo_ai-0.0.19/majordomo_ai/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from majordomo_ai.api.admin_api import AdminApi
+from majordomo_ai.api.inference_api import InferenceApi
+from majordomo_ai.api.ingest_api import IngestApi
+from majordomo_ai.api.monitor_api import MonitorApi
+from majordomo_ai.api.query_api import QueryApi
+from majordomo_ai.api.workflow_api import WorkflowApi
+
