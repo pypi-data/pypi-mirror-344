0efebf2c7d1706365b2d3a9ee810fdccf185f990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:33 +0000

    Add package PyAdvys/0.3.4/pyadvys-0.3.4.tar.gz

---
+++ packages/PyAdvys/pyadvys-0.3.4.tar.gz/pyadvys-0.3.4/src/advys_configuracoes/logs.py
@@ -0,0 +1,41 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+from advys_configuracoes.constantes import ENCODING
+
+def configurar_logging(
+    arquivo_log,
+    max_bytes=5 * 1024 * 1024,  # 5MB
+    backup_count=3,
+    habilitar_log_console=False,
+    nivel_log=logging.DEBUG,
+):
+    caminho = Path(arquivo_log)
+    caminho.parent.mkdir(exist_ok=True, parents=True)
+
+    logger = logging.getLogger()
+    logger.setLevel(nivel_log)
+
+    # Formato do log
+    formato = logging.Formatter(
+        '%(asctime)s | %(levelname)s | %(name)s | %(message)s'
+    )
+
+    # Handler para arquivo com rotação
+    file_handler = RotatingFileHandler(
+        arquivo_log, maxBytes=max_bytes, backupCount=backup_count, encoding=ENCODING
+    )
+    file_handler.setFormatter(formato)
+
+    # Handler opcional para console
+    console = None
+    if habilitar_log_console:
+        console = logging.StreamHandler()
+        console.setFormatter(formato)
+
+
+    # Adiciona os handlers caso não existam
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        if console:
+            logger.addHandler(console)
