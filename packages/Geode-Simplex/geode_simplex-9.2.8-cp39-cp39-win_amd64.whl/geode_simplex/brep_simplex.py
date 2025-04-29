f347758fce7d427ef6f1b4835eb3d4c69432cbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:04:51 +0000

    Add package Geode-Simplex/9.2.8/geode_simplex-9.2.8-cp39-cp39-win_amd64.whl

---
+++ packages/Geode-Simplex/geode_simplex-9.2.8-cp39-cp39-win_amd64.whl/geode_simplex/brep_simplex.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_inspector
+import geode_common
+import geode_conversion
+import geode_numerics
+import geode_background
+
+from .bin.geode_simplex_py_brep import *
+SimplexBRepLibrary.initialize()
