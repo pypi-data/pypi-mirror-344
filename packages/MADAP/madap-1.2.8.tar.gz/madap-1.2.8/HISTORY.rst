424f36593c20cdc8e4d54945d9054bfc95311442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:05:03 +0000

    Add package MADAP/1.2.8/madap-1.2.8.tar.gz

---
+++ packages/MADAP/madap-1.2.8.tar.gz/madap-1.2.8/HISTORY.rst
@@ -0,0 +1,63 @@
+=======
+History
+=======
+
+1.2.8 (2025-04-28)
+-------------------
+* Fixed bug from merge conflict
+
+1.2.7 (2025-04-24)
+-------------------
+* Added support for no time data in CV
+* Fixed bug in CV for no time data and CLI
+
+1.2.6 (2023-12-16)
+-------------------
+* Fixed bug in CP for differential capacity plot
+* Handled None and Infinite values in CP plots
+* Fixed index in CV
+
+
+1.2.5 (2023-12-15)
+-------------------
+* Fixed bug in CA for zero reaction rate constant
+* Fixed bug in index handling for CA
+* Fixed bug in plot ticks
+
+1.2.3 (2023-12-11)
+-------------------
+* Include python 3.8
+
+1.2.1 (2023-12-11)
+-------------------
+* Added Ciclic Voltammetry to the functions
+* CA, CP and CV can be used with multiple plots to chose from
+* New and Imrpoved GUI
+* Fixed saving bug
+
+1.1.0 (2023-08-07)
+-------------------
+* Fixes issue with mismatch array length when positive imaginary data is given
+
+0.11.0 (2022-10-16)
+-------------------
+
+* Fixed bugs concering the package installation.
+* Improved the documentation.
+* Imrpoved the file structure.
+* madap_gui and madap_cli are now in the same package and can be used as standalone scripts/commands.
+
+0.10.0 (2022-10-03)
+-------------------
+
+* Updated support of the python versions
+
+0.9.0 (2022-10-02)
+------------------
+
+* Update documentation
+
+0.8.0 (2022-10-02)
+------------------
+
+* First release on PyPI.
---
+++ packages/MADAP/madap-1.2.8.tar.gz/madap-1.2.8/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open('README.rst', encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst', encoding="utf-8") as history_file:
+    history = history_file.read()
+
+requirements = ["attrs==21.4.0",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "PySimpleGUI",
+        "pytest",
+        "scikit_learn",
+        "ruptures",
+        "impedance==1.4.1"]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Fuzhan Rahmanian",
+    author_email='fuzhanrahmanian@gmail.com',
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+    description="This is MADAP, a software package for the analysis of electrochemical data.",
+    entry_points={
+        'console_scripts': [
+            'madap_cli=madap_cli:main',
+            'madap_gui=madap_gui:main',
+        ],
+    },
+    extras_require={
+        "dev": ["pytest>=3", ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='madap',
+    name='MADAP',
+    packages=find_packages(),
+    py_modules=['madap_cli', 'madap_gui'],
+    package_data={'madap/plotting/styles': ['*.mplstyle']},
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/fuzhanrahmanian/MADAP',
+    version='1.2.8',
+    zip_safe=False,
+)
