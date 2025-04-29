c397a6215e05366d4282b853700fe1770ac5bb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:50 +0000

    Add package autoprocessor/0.3.0/autoprocessor-0.3.0.tar.gz

---
+++ packages/autoprocessor/autoprocessor-0.3.0.tar.gz/autoprocessor-0.3.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="autoprocessor",                # Package name
+    version="0.3.0",                  # Version
+    author="Your Name",               # Author name
+    author_email="your.email@example.com",  # Author email
+    description="A simple example package", # Short description
+     # Long description (from README)
+    long_description_content_type="text/markdown", # Content type
+    url="https://github.com/yourusername/my_package", # Project URL
+    packages=find_packages(),         # Finds all packages within the directory
+    classifiers=[                     # Classifiers to categorize the package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",          # Python version requirements
+)
