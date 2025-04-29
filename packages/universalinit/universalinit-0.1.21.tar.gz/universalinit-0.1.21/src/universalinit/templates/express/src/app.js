020d5661782c8ce70638d645fef4d126c797a580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:49 +0000

    Add package universalinit/0.1.21/universalinit-0.1.21.tar.gz

---
+++ packages/universalinit/universalinit-0.1.21.tar.gz/universalinit-0.1.21/src/universalinit/templates/express/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../swagger');
+
+// Initialize express app
+const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Parse JSON request body
+app.use(express.json());
+
+// Mount routes
+app.use('/', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+module.exports = app;
