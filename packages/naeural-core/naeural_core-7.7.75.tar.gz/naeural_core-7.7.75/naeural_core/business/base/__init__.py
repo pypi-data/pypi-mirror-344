2b597eb462e8119fb6e7daf08de8f40661c68b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:25 +0000

    Add package naeural-core/7.7.75/naeural_core-7.7.75.tar.gz

---
+++ packages/naeural-core/naeural_core-7.7.75.tar.gz/naeural_core-7.7.75/naeural_core/business/base/__init__.py
@@ -0,0 +1,4 @@
+from .base_plugin_biz import BasePluginExecutor
+from .cv_plugin_executor import CVPluginExecutor
+from .sd_plugin_executor import BasicStructuredDataPluginExecutor
+from .simple_rest_executor import SimpleRestExecutor
