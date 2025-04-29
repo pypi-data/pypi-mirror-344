1cb238a7df85e6c2650afb2ffdd4cb7b44974a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:20:05 +0000

    Add package starling-sim/1.1.0/starling_sim-1.1.0-py3-none-any.whl

---
+++ packages/starling-sim/starling_sim-1.1.0-py3-none-any.whl/starling_sim/simulation_scenario.py
@@ -0,0 +1,196 @@
+"""
+This module manages the parameters of the simulation
+"""
+
+import datetime
+import logging
+import os
+import json
+from copy import deepcopy
+
+from starling_sim.utils.utils import json_load, add_defaults_and_validate
+from starling_sim.utils import paths
+from starling_sim.utils.config import config
+from starling_sim.version import __version__
+
+
+class SimulationScenario:
+    """
+    This class describes a simulation scenario folders and data.
+    """
+
+    BASE_PARAM_SCHEMA = "parameters.schema.json"
+
+    def __init__(self, scenario_folder_path: str):
+        # scenario model code
+        self.model = None
+
+        # scenario name
+        self.name = None
+
+        # simulation parameters (can be accessed using SimulationScenario["key"])
+        self.parameters = None
+
+        # run summary
+        self.runSummary = None
+
+        # scenario folder path
+        self.scenario_folder = None
+
+        # scenario inputs folder path
+        self.inputs_folder = None
+
+        # scenario outputs folder path
+        self.outputs_folder = None
+
+        # set the scenario folders
+        self._set_scenario_folders(scenario_folder_path)
+
+    def _set_scenario_folders(self, scenario_folder_path: str):
+        """
+        Set the folder attributes from the scenario folder path.
+
+        Paths are built according to the structure enforced by the functions of starling_sim.utils.paths.
+
+        :param scenario_folder_path: path to the scenario folder
+        """
+
+        # check scenario folder
+        self.scenario_folder = os.path.join(scenario_folder_path, "")
+        if not (os.path.exists(self.scenario_folder) and os.path.isdir(self.scenario_folder)):
+            raise ValueError(
+                "The scenario folder does not exist or is not a folder : {}".format(
+                    self.scenario_folder
+                )
+            )
+
+        # check inputs folder
+        self.inputs_folder = paths.scenario_inputs_folder(self.scenario_folder)
+        if not (os.path.exists(self.inputs_folder) and os.path.isdir(self.inputs_folder)):
+            raise ValueError(
+                "The inputs folder does not exist or is not a folder : {}".format(
+                    self.inputs_folder
+                )
+            )
+
+        # create outputs folder if it does not exist
+        if "OUTPUT_FOLDER" in os.environ:
+            output_folder = os.path.join(os.environ["OUTPUT_FOLDER"], "")
+        else:
+            output_folder = paths.scenario_outputs_folder(self.scenario_folder)
+        self.outputs_folder = output_folder
+        if not os.path.exists(self.outputs_folder):
+            os.mkdir(self.outputs_folder)
+
+    def get_scenario_parameters(self):
+        """
+        Get and validate the scenario simulation parameters.
+
+        Also get the scenario model and name from the parameters.
+
+        The parameter file path is enforced by the scenario_parameters_filepath function.
+        """
+
+        try:
+            parameters_path = paths.scenario_parameters_filepath(self.scenario_folder)
+            self.parameters = json_load(parameters_path)
+
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            raise ValueError("Error while loading the scenario parameters : {}".format(str(e)))
+
+        # check scenario folder against scenario name
+        # in fact this is not necessary, scenario name is not used for pathing anymore
+        if os.path.basename(os.path.dirname(self.scenario_folder)) != self.parameters["scenario"]:
+            raise ValueError("The scenario folder must be named after the scenario name")
+
+        # parameters validation
+        schema = json_load(paths.schemas_folder() + self.BASE_PARAM_SCHEMA)
+        self.parameters = add_defaults_and_validate(self.parameters, schema)
+
+        # change date format from YYYY-MM-DD to YYYYMMDD
+        if "date" in self.parameters:
+            self.parameters["date"] = self.parameters["date"].replace("-", "")
+
+        # get model and scenario
+        self.model = self.parameters["code"]
+        self.name = self.parameters["scenario"]
+
+        self.init_run_summary()
+
+    def __getitem__(self, item):
+        """
+        Method called when using 'SimulationScenario[item]'
+
+        :param item: Name of the parameter accessed
+        :return: self.parameters[item]
+        """
+
+        if item not in self.parameters:
+            logging.error(
+                "Trying to access unknown parameter '"
+                + str(item)
+                + "'. Does it appears in the parameter entry ?"
+            )
+            raise KeyError("The parameter '" + str(item) + "' does not exist")
+
+        return self.parameters[item]
+
+    def __contains__(self, item):
+        """
+        Method called when using 'item in simulationParameters'
+
+        :param item:
+        :return: True if item is in self.parameters, False otherwise
+        """
+
+        return item in self.parameters
+
+    def init_run_summary(self):
+        """
+        Initialise the run summary.
+        """
+
+        summary = dict()
+
+        # get run date
+        summary["date"] = str(datetime.datetime.today())
+
+        # get starling version
+        summary["starling_version"] = __version__
+
+        # copy scenario parameters
+        summary["parameters"] = self.copy_parameters()
+
+        # copy config
+        summary["config"] = config.copy()
+
+        # scenario output files
+        summary["output_files"] = dict()
+
+        # run statistics
+        summary["stats"] = dict()
+
+        self.runSummary = summary
+
+    def copy_parameters(self):
+        """
+        Return a deepcopy of the simulation parameters.
+
+        :return: deepcopy of simulation parameters
+        """
+
+        return deepcopy(self.parameters)
+
+    def set_stat(self, key, value):
+        """
+        Set a value for the given stat key.
+
+        :param key: stat key
+        :param value: stat value
+        """
+        stats = self.runSummary["stats"]
+
+        if key in stats:
+            logging.warning("Overwriting '{}' scenario statistic".format(key))
+
+        stats[key] = value
---
+++ packages/starling-sim/starling_sim-1.1.0-py3-none-any.whl/starling_sim/utils/simulation_logging.py
@@ -0,0 +1,176 @@
+"""
+Simulation logs offer an insight of the flow of events during the simulation run.
+They are displayed in chronological order and contain various information such as simulation setup,
+output generation or agent activity.
+
+Simulation logs are managed with the Python
+`logging <https://docs.python.org/3.6/library/logging>`_ library.
+
+*************************
+Simulation logging levels
+*************************
+
+In addition to the base levels of *logging*, we introduce two new ones:
+
+- An agent level that displays the agents' activities
+- An algorithm level that displays the algorithms' steps
+
+This results in the following available levels:
+
+.. list-table:: **STARLING LOGGING LEVELS**
+   :widths: auto
+   :header-rows: 1
+   :align: center
+
+   * - Level
+     - Numeric value
+   * - CRITICAL
+     - 50
+   * - ERROR
+     - 40
+   * - WARNING
+     - 30
+   * - INFO
+     - 20
+   * - AGENT
+     - 15
+   * - ALGO
+     - 13
+   * - DEBUG
+     - 10
+
+The default logging level is 13.
+
+To run a simulation with a different logging level, you can use the ``-l`` (or ``--level``) option of main.py.
+For instance:
+
+.. code-block:: bash
+
+    python3 main.py data/models/SB_VS/example_nantes/ -l 20
+
+******************
+Simulation loggers
+******************
+
+We also create specific loggers for the :class:`~starling_sim.basemodel.trace.trace.Traced` and
+:class:`~starling_sim.basemodel.algorithms.algorithm.Algorithm` classes with
+different logging formats, in order to display additional information (such as the simulation time). For instance:
+
+.. code-block:: text
+
+    AGENT :: [50904], S1 : Picked up u-11
+
+In order to display logs, instances of these classes call their
+internal method log_message so the relevant information is fetched
+directly from their attributes and a default level is applied.
+
+These additional loggers may also be used by any module developed around
+Starling to display simulation logs.
+"""
+
+import logging
+
+
+#: logging format of the base logger
+BASE_LOGGER_FORMAT = "%(levelname)s :: %(message)s"
+
+#: default logging level of the simulations
+DEFAULT_LOGGER_LEVEL = 13
+
+#: additional logging level AGENT for agent activity logs
+AGENT_LEVEL, AGENT_LEVEL_NAME = 15, "AGENT"
+
+#: additional logging level for algorithms logs
+ALGO_LEVEL, ALGO_LEVEL_NAME = 13, "ALGO"
+
+
+def setup_logging(logger_level):
+    """
+    Setup the logging configuration for the run.
+
+    :param logger_level: integer describing the logger level
+    """
+
+    # add levels specific to the project
+
+    # new logging level for traced agents
+    logging.addLevelName(AGENT_LEVEL, AGENT_LEVEL_NAME)
+
+    # new logging level for algorithms
+    logging.addLevelName(ALGO_LEVEL, ALGO_LEVEL_NAME)
+
+    # set the base logger level
+    logging.basicConfig(format=BASE_LOGGER_FORMAT, level=logger_level)
+
+
+#: logging format of TRACED_LOGGER
+TRACED_LOGGER_FORMAT = "%(levelname)s :: [%(timestamp)s], %(id)s : %(message)s"
+
+
+def new_traced_logger():
+    """
+    Create and return a logger for the traced agents.
+
+    :return: Logger object from the logging library
+    """
+
+    traced_logger = logging.getLogger("traced_logger")
+    traced_logger.propagate = False
+    formatter = logging.Formatter(TRACED_LOGGER_FORMAT)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    traced_logger.addHandler(stream_handler)
+
+    return traced_logger
+
+
+#: logging format of ALGO_LOGGER
+ALGO_LOGGER_FORMAT = "%(levelname)s :: %(alg_name)s : %(message)s"
+
+
+def new_algo_logger():
+    """
+    Create and return a logger for the simulation algorithms.
+
+    :return: Logger object from the logging library
+    """
+
+    algo_logger = logging.getLogger("algo_logger")
+    algo_logger.propagate = False
+    formatter = logging.Formatter(ALGO_LOGGER_FORMAT)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    algo_logger.addHandler(stream_handler)
+
+    return algo_logger
+
+
+#: logging format of BLANK_LOGGER
+BLANK_LOGGER_FORMAT = "%(message)s"
+
+
+def new_blank_logger():
+    """
+    Create and return a logger without prefix.
+
+    :return: Logger object from the logging library
+    """
+
+    blank_logger = logging.getLogger("blank_logger")
+    blank_logger.propagate = False
+    formatter = logging.Formatter(BLANK_LOGGER_FORMAT)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    blank_logger.addHandler(stream_handler)
+
+    return blank_logger
+
+
+#: Traced objects logger
+TRACED_LOGGER = new_traced_logger()
+
+#: algorithms logger
+ALGO_LOGGER = new_algo_logger()
+
+#: blank logger
+BLANK_LOGGER = new_blank_logger()
