7ab8158810acbb3fd6f36a84adb61687aad00456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:05:07 +0000

    Add package filedetect/0.0.1/filedetect-0.0.1-py3-none-any.whl

---
+++ packages/filedetect/filedetect-0.0.1-py3-none-any.whl/filedetect/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025-present Marceau <git@marceau-h.fr>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .Formats import Formats
+from .FileFinder import FileFinder
+
+__all__ = [
+    "Formats",
+    "FileFinder",
+]
