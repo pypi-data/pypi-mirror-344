374253f268fbfe162546f1964dd73559837dc961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:17 +0000

    Add package geoconformal/0.1.1/geoconformal-0.1.1.tar.gz

---
+++ packages/geoconformal/geoconformal-0.1.1.tar.gz/geoconformal-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geoconformal',
+    version='0.1.1',
+    description='Geographically Weighted Conformal Prediction Methods',
+    author='Peng Luo',
+    author_email='pengluo@mit.edu',
+    url='https://github.com/pengluo/geoconformal',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'scipy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
