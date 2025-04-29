b7ad1a160736cfe5eab47accd1d7340675ddd48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:19:22 +0000

    Add package mini-LiTOY/0.1.5/mini_litoy-0.1.5-py3-none-any.whl

---
+++ packages/mini-LiTOY/mini_litoy-0.1.5-py3-none-any.whl/mini_LiTOY/__init__.py
@@ -0,0 +1,15 @@
+import fire
+
+from .mini_LiTOY import mini_LiTOY
+
+__all__ = ["mini_LiTOY"]
+
+__VERSION__ = mini_LiTOY.VERSION
+
+
+def cli_launcher() -> None:
+    fire.Fire(mini_LiTOY)
+
+
+if __name__ == "__main__":
+    cli_launcher()
