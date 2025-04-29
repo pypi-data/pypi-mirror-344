43683f729e5e20653b43671f47a8df4cd98f259d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:19:48 +0000

    Add package uart-ble/0.1.4/uart_ble-0.1.4-py3-none-any.whl

---
+++ packages/uart-ble/uart_ble-0.1.4-py3-none-any.whl/uart_ble/definitions.py
@@ -0,0 +1,7 @@
+"""Sample doc string."""
+
+UART_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+TX_CHAR_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"  # from device → host
+RX_CHAR_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"  # from host → device
+
+BLE_TIMEOUT = 2.0
