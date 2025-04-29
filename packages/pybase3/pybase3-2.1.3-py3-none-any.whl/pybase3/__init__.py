6d3fcaba3532b29e3182513d4ef54a00f186c8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:54 +0000

    Add package pybase3/2.1.3/pybase3-2.1.3-py3-none-any.whl

---
+++ packages/pybase3/pybase3-2.1.3-py3-none-any.whl/pybase3/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__version__ = "2.1.3"
+__author__ = "Domingo E. Savoretti"
+__email__ = "esavoretti@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/sandy98/oxi"
+__description__ = "A suite of tools to handle legacy dBase III files."
+__copyright__ = "Copyright (c) 2025 Domingo E. Savoretti"
+
