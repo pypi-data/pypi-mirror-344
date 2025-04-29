3f605a8402bad3cb0f8a9d04c8a2712822ac68d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:04:01 +0000

    Add package kauldron/1.2.2/kauldron-1.2.2.tar.gz

---
+++ packages/kauldron/kauldron-1.2.2.tar.gz/kauldron-1.2.2/kauldron/checkpoints/partial_loader.py
@@ -0,0 +1,214 @@
+# Copyright 2024 The kauldron Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Partial checkpoint utils."""
+
+from __future__ import annotations
+
+import abc
+from collections.abc import MutableMapping
+import dataclasses
+import functools
+import typing
+from typing import Any, Iterable, Self, TypeVar
+
+from etils import epath
+from etils import epy
+import flax
+from kauldron import kontext
+from kauldron.checkpoints import checkpoint_items
+from kauldron.checkpoints import checkpointer
+from kauldron.utils import from_xid
+import orbax.checkpoint as ocp
+
+with epy.lazy_imports():
+  from orbax.checkpoint.experimental.model_surgery import standard_checkpoint_handler  # pylint: disable=g-import-not-at-top
+
+FrozenDict = dict if typing.TYPE_CHECKING else flax.core.FrozenDict
+_T = TypeVar('_T')
+
+
+# TODO(epot): rename to `InitTransform`
+class AbstractPartialLoader(abc.ABC):
+  """Abstract class for partial checkpoint loaders."""
+
+  @abc.abstractmethod
+  def transform(self, state: _T) -> _T:
+    """Transform the state by updating it with pre-trained values.
+
+    Notes:
+
+    * `transform` functions can modify the `state` values but should NOT
+      modify its structure, shape or dtypes.
+    * `transform` should correctly propagate the sharding information from the
+      given state.
+
+    Args:
+      state: The `state` object to transform
+
+    Returns:
+      The updated `state`
+    """
+    raise NotImplementedError
+
+
+class NoopTransform(AbstractPartialLoader):
+  """`init_transform` that does nothing."""
+
+  def transform(self, state):
+    return state
+
+
+class MultiTransform(AbstractPartialLoader):
+  """Transform which applies multiple transformations sequentially."""
+
+  def __init__(self, **transforms: AbstractPartialLoader):
+    self._transforms = transforms
+
+  def transform(self, state):
+    for k, tr in self._transforms.items():
+      try:
+        state = tr.transform(state)
+      except Exception as e:  # pylint: disable=broad-exception-caught
+        epy.reraise(e, prefix=f'init_transform {k!r} failed: ')
+    return state
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class PartialKauldronLoader(epy.ContextManager, AbstractPartialLoader):
+  """Partial loader for Kauldron checkpoints.
+
+  Allow to use pretrained weights from another Kauldron checkpoint.
+
+  Usage:
+
+  ```python
+  cfg.init_transform = kd.ckpts.PartialKauldronLoader(
+      workdir='/path/to/original/work_unit/',
+      new_to_old={  # Mapping params
+          # '<new_path>':            '<source_path>'
+          'params.decoder.layers_0': 'params.encoder',
+      },
+  )
+
+  trainer = konfig.resolve(cfg)
+
+  # When initializing the weights, the `init_transform` is applied
+  init_state = trainer.init_state()
+
+  # `init_state.params['decoder']['layers_0']` now contain the previous encoder
+  # weights
+  ```
+
+  Attributes:
+    workdir: The work directory from which the checkpoint should be loaded ( can
+      be created from `kd.ckpts.workdir_from_xid`).
+    new_to_old: Mapping the pytree to copy to the new state from the original
+      checkpoint. By default, copy all model `params` and `collections`
+    step: Which step to load (default to last one)
+  """
+
+  workdir: epath.PathLike
+  new_to_old: MutableMapping[str, str] = dataclasses.field(
+      default_factory=lambda: FrozenDict({
+          'params': 'params',
+          'collections': 'collections',
+      })
+  )
+  step: int = -1
+
+  def transform(self, state: _T) -> _T:
+    # This could potentially be extended to non-Kauldron checkpoints (like
+    # `kd.ckpts.PartialOrbaxLoader`)
+    return self._ckpt_mgr.restore(
+        _PartialRestoreCheckpointItem(state, new_to_old=self.new_to_old),
+        step=self.step,
+    )
+
+  @functools.cached_property
+  def _ckpt_mgr(self) -> checkpointer.Checkpointer:
+    return checkpointer.Checkpointer(
+        workdir=self.workdir,
+        save_interval_steps=1,
+    )
+
+  def close(self) -> None:
+    self._ckpt_mgr.close()
+
+  def __contextmanager__(
+      self,
+  ) -> Iterable[Self]:
+    try:
+      yield self
+    finally:
+      self.close()
+
+
+@dataclasses.dataclass(frozen=True)
+class _PartialRestoreCheckpointItem(checkpoint_items.CheckpointItem):
+  """Restore a partial checkpoint."""
+
+  state: Any
+  _: dataclasses.KW_ONLY
+  new_to_old: MutableMapping[str, str]
+
+  def __kd_ocp_handlers__(self) -> ocp.CheckpointHandler:
+    return standard_checkpoint_handler.StandardCheckpointHandler()
+
+  def __kd_ocp_save_args__(self) -> ocp.args.CheckpointArgs:
+    raise ValueError('Partial checkpoint do not support save.')
+
+  def __kd_ocp_restore_args__(self) -> ocp.args.CheckpointArgs:
+    # Extract the sub-tree from the new state
+    sub_state = {
+        new_path: kontext.get_by_path(self.state, new_path)
+        for new_path, _ in self.new_to_old.items()
+    }
+    return standard_checkpoint_handler.StandardRestoreArgs(
+        sub_state, transform_fn=self._transform_fn
+    )
+
+  def __kd_ocp_restore_post__(self, partial_restore: Any):
+    """Post-processing after `ocp.CheckpointManager.restore(args=)`."""
+    # Mutate the state to overwrite the sub-tree with the partially restored
+    # checkpoint.
+
+    # Deep copy (mutable)
+    # `orbax` uses nested `dict`, rather than PyTree
+    state = ocp.utils.serialize_tree(self.state, keep_empty_nodes=True)
+
+    for new_path, _ in self.new_to_old.items():
+      new_partial_restore = ocp.utils.serialize_tree(
+          partial_restore[new_path], keep_empty_nodes=True
+      )
+      kontext.set_by_path(state, new_path, new_partial_restore)
+
+    state = ocp.utils.deserialize_tree(
+        state, target=self.state, keep_empty_nodes=True
+    )
+    return state
+
+  def _transform_fn(self, restored):
+    # Extract the sub-tree from the old state
+    sub_state = {
+        new_path: kontext.get_by_path(restored, old_path)
+        for new_path, old_path in self.new_to_old.items()
+    }
+    return sub_state
+
+
+# TODO(epot): Deprecate the alias and migrate everyone to
+# `kd.from_xid.get_workdir`
+def workdir_from_xid(xid: int, wid: int = 1) -> epath.Path:
+  return from_xid.get_workdir(xid=xid, wid=wid)
---
+++ packages/kauldron/kauldron-1.2.2.tar.gz/kauldron-1.2.2/kauldron/data/py/base.py
@@ -0,0 +1,236 @@
+# Copyright 2024 The kauldron Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""PyGrain data pipeline."""
+
+from __future__ import annotations
+
+import dataclasses
+import functools
+import math
+import typing
+from typing import Optional
+
+from etils import enp
+from etils import epy
+from etils.etree import jax as etree  # pylint: disable=g-importing-member
+import grain.python as grain
+import jax
+from kauldron import random
+from kauldron.data import iterators
+from kauldron.data import pipelines
+from kauldron.data.py import transform_utils
+from kauldron.data.transforms import normalize as tr_normalize
+from kauldron.typing import PRNGKeyLike, PyTree  # pylint: disable=g-importing-member,g-multiple-import
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True, eq=True)
+class PyGrainPipeline(pipelines.Pipeline):
+  """Abstract base class to construct PyGrain data pipeline.
+
+  See doc:
+
+  Attributes:
+    transforms: A list of transformations to apply to the dataset. Each
+      transformation should be either a `grain.MapTransform` or a
+      `grain.RandomMapTransform`.
+    num_epochs: Number of epoch. If missing, iterate indefinitely (number of
+      iteration is given by `cfg.num_training_steps`)
+    batch_drop_remainder: Whether or not drop the last examples if `len(ds) %
+      batch_size != 0`
+    num_workers: how many worker processes to use for data loading (0 to disable
+      multiprocessing)
+    read_options: Options for reading data from the DataSource.
+    enable_profiling: If True data worker process 0 will be profiled.
+  """
+
+  # TODO(epot): Need to duplicate `batch_size` because pytype fail to detect
+  # the parent class
+  if typing.TYPE_CHECKING:
+    batch_size: int | None = ...
+    seed: PRNGKeyLike | None = ...
+
+  transforms: tr_normalize.Transformations = dataclasses.field(
+      default_factory=tuple
+  )
+
+  # Params only relevant for the root top-level dataset (when dataset mixture)
+  num_epochs: Optional[int] = None
+  batch_drop_remainder: bool = True
+  num_workers: int = 16
+  read_options: grain.ReadOptions | None = None
+  enable_profiling: bool = False
+
+  # The pipeline is constructed in 4 functions:
+  # * `ds_for_current_process`
+  # * `ds_with_transforms`
+  # * `_root_map_ds`
+  # * `_root_ds`
+
+  def ds_for_current_process(self, rng: random.PRNGKey) -> grain.MapDataset:
+    raise NotImplementedError
+
+  def ds_with_transforms(self, rng: random.PRNGKey) -> grain.MapDataset:
+    """Create the `tf.data.Dataset` and apply all the transforms."""
+    ds = self.ds_for_current_process(rng)
+
+    ds = transform_utils.apply_transforms(ds, self.transforms)
+
+    return ds
+
+  @functools.cached_property
+  def _root_map_ds(self) -> grain.MapDataset:
+    self._assert_root_cfg_resolved()
+
+    # Loader sometimes uses `jax.random` to generate integer seeds, so allow
+    # host<>device here.
+    # Instead could force the `PRNGKey` to stay in the CPU.
+    with jax.transfer_guard("allow"):
+      rng = random.PRNGKey(self.seed)
+      rng = rng.fold_in("kmix")
+      ds = self.ds_with_transforms(rng)
+
+    # Apply repeat at the end
+    ds = ds.repeat(self.num_epochs)
+    return ds
+
+  def _make_root_ds(self, *, num_workers: int) -> grain.IterDataset:
+    # TODO(b/362920968): We're forced to split `_root_ds` into both
+    # `_root_map_ds` because `_root_map_ds` does not propagate `len`
+    ds = self._root_map_ds
+
+    if num_workers == 0 or self.read_options is None:
+      # TODO(epot): Fix adhoc import thread-safety and restore this !!!
+      # PyGrain multi-thread has various issues, so disable it:
+      # * einops backend registration is not thread-safe.
+      # * Adhoc imports are not thread-safe, creating issues when lazy-imports
+      #   get triggered inside the data pipeline.
+      # So disable pre-fetching added by `to_iter_dataset`.
+      read_options = grain.ReadOptions(
+          num_threads=0,
+          prefetch_buffer_size=0,
+      )
+    else:
+      read_options = self.read_options
+
+    # a prefetch at the end. When.
+    ds = ds.to_iter_dataset(read_options=read_options)
+    # We do batching after conversion to `IterDataset` to avoid None during
+    # batching.
+    if self.batch_size:
+      ds = ds.batch(
+          self.host_batch_size, drop_remainder=self.batch_drop_remainder
+      )
+
+    # Distribute the execution across multiple worker processes.
+    if num_workers > 0:
+      multiprocessing_options = grain.MultiprocessingOptions(
+          num_workers=num_workers,
+          enable_profiling=self.enable_profiling,
+      )
+      ds = ds.mp_prefetch(multiprocessing_options)
+    return ds
+
+  @functools.cached_property
+  def _root_ds(self) -> grain.IterDataset:
+    # Distribute the execution across multiple worker processes.
+    num_workers = _get_num_workers(self.num_workers)
+
+    return self._make_root_ds(num_workers=num_workers)
+
+  @functools.cached_property
+  def element_spec(self) -> PyTree[enp.ArraySpec]:
+    """Returns the element specs of a single batch."""
+    # To avoid the memory overhead of multiprocessing when `num_workers` is
+    # large, explicitly turn off multiprocessing here since we only require
+    # the first element.
+    ds = self._make_root_ds(num_workers=0)
+    first_elem = next(iter(ds))
+    return etree.spec_like(first_elem)
+
+  def __iter__(self) -> iterators.Iterator:
+    """Iterate over the dataset elements."""
+    return iterators.PyGrainIterator(source=self, iter=iter(self._root_ds))
+
+  def __len__(self) -> int:
+    if self.num_epochs is None:
+      raise TypeError("Cannot get length of infinite dataset.")
+
+    ds_len = len(self._root_map_ds)
+    if not self.batch_size:
+      return ds_len
+
+    if self.batch_drop_remainder:
+      return ds_len // self.batch_size
+    else:
+      return math.ceil(ds_len / self.batch_size)
+
+  def __getitem__(self, record_key: int):
+    """Get an item from the dataset."""
+    ds = self._root_map_ds
+    # Re-apply the batching, as in the standard pipeline, it is applied after
+    # conversion to `IterDataset`.
+    if self.batch_size:
+      ds = ds.batch(
+          self.host_batch_size, drop_remainder=self.batch_drop_remainder
+      )
+    return ds[record_key]
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True, eq=True)
+class DataSourceBase(PyGrainPipeline):
+  """Base class to implement a data source.
+
+  Child classes should overwrite the `data_source` property.
+  See `kd.data.py.Tfds` for an example.
+
+  Attributes:
+    shuffle: whether to shuffle
+  """
+
+  shuffle: bool
+  data_source: grain.RandomAccessDataSource = dataclasses.field(
+      init=False,
+      repr=False,
+  )
+
+  def ds_for_current_process(self, rng: random.PRNGKey) -> grain.MapDataset:
+    ds = grain.MapDataset.source(self.data_source)
+    ds = ds.seed(rng.as_seed())
+
+    # Shard the dataset
+    ds = ds[jax.process_index() :: jax.process_count()]
+
+    # Global shuffle
+    if self.shuffle:
+      # Here we explicitly pass the seed.
+      # This allow the example order to be constant even if transformations
+      # changes. Note that this might be the case anyway but I haven't looked
+      # at grain's internal and how seed is derived to the individual
+      # transformation.
+      ds = ds.shuffle(seed=rng.fold_in("shuffle").as_seed())
+    return ds
+
+
+def _get_num_workers(num_workers: int) -> int:
+  """Set the number of workers."""
+  if epy.is_notebook() or epy.is_test():  # in colab worker_count has to be 0
+    # TODO(klausg): autodetect if Kernel supports multiprocessing
+    # Could check
+    # from multiprocessing import spawn
+    # spawn.get_executable() is not None
+    print("Disabling pygrain multi-processing (unsupported in colab).")
+    return 0
+  else:
+    return num_workers
