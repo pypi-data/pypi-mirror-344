a2f6dc20ba3eb009515695ba664330b39aef63ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:34 +0000

    Add package numpysocket/2.0.0/numpysocket-2.0.0.tar.gz

---
+++ packages/numpysocket/numpysocket-2.0.0.tar.gz/numpysocket-2.0.0/examples/Simple/simple_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import logging
+import numpy as np
+from numpysocket import NumpySocket
+
+
+logger = logging.getLogger("simple client")
+logger.setLevel(logging.INFO)
+
+with NumpySocket() as s:
+    s.connect(("localhost", 9999))
+
+    logger.info("sending numpy array:")
+    frame = np.arange(1000)
+    s.sendall(frame)
