a1d86d3faa78047ffa660853770c4e1b55d57312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:55 +0000

    Add package melobot/3.2.1/melobot-3.2.1.tar.gz

---
+++ packages/melobot/melobot-3.2.1.tar.gz/melobot-3.2.1/src/melobot/adapter/__init__.py
@@ -0,0 +1,20 @@
+from .base import (
+    AbstractEchoFactory,
+    AbstractEventFactory,
+    AbstractOutputFactory,
+    Adapter,
+    AdapterLifeSpan,
+)
+from .content import Content
+from .model import (
+    Action,
+    ActionHandle,
+    ActionHandleGroup,
+    ActionT,
+    Echo,
+    EchoT,
+    Event,
+    EventT,
+    TextEvent,
+    lazy_action,
+)
