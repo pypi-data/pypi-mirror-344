1ade75017f6d7b1a9c6e455f8caa053b14a32f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 17:05:11 +0000

    Add package PyMieSim/3.5.0/pymiesim-3.5.0-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/PyMieSim/pymiesim-3.5.0-cp310-cp310-macosx_14_0_arm64.whl/PyMieSim/single/scatterer/core_shell.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyOptik.material.base_class import BaseMaterial
+
+import numpy
+from typing import Optional
+from pydantic.dataclasses import dataclass
+from PyMieSim.units import Quantity
+from PyMieSim.single.scatterer.base import BaseScatterer, config_dict
+
+
+@dataclass(config=config_dict, kw_only=True)
+class CoreShell(BaseScatterer):
+    """
+    Class representing a core/shell spherical scatterer.
+
+    Parameters
+    ----------
+    core_diameter : Quantity
+        Diameter of the core of the scatterer.
+    shell_thickness : Quantity
+        Thickness of the shell surrounding the core.
+    core_property : Quantity | BaseMaterial
+        Defines either the refractive index (`Quantity`) or material (`BaseMaterial`) of the scatterer's core. Only one can be provided.
+    shell_property : Quantity | BaseMaterial
+        Defines either the refractive index (`Quantity`) or material (`BaseMaterial`) of the scatterer's shell. Only one can be provided.
+
+    """
+
+    core_diameter: Quantity
+    shell_thickness: Quantity
+
+    core_property: Quantity | BaseMaterial
+    shell_property: Quantity | BaseMaterial
+
+    property_names = [
+        "size_parameter", "area", "g",
+        "Qsca", "Qext", "Qabs", "Qback", "Qratio", "Qpr",
+        "Csca", "Cext", "Cabs", "Cback", "Cratio", "Cpr"
+    ]
+
+    def __post_init__(self):
+        self.core_index, self.core_material = self._assign_index_or_material(self.core_property)
+
+        self.shell_index, self.shell_material = self._assign_index_or_material(self.core_property)
+
+        self.cross_section = numpy.pi * ((self.core_diameter / 2 + self.shell_thickness)) ** 2
+
+        super().__post_init__()
+
+    def set_binding(self) -> None:
+        """
+        Bind the C++ scatterer class.
+        """
+        from PyMieSim.binary.interface_scatterer import CORESHELL
+
+        self.binding = CORESHELL(
+            shell_refractive_index=self.shell_index.to_base_units().magnitude,
+            core_refractive_index=self.core_index.to_base_units().magnitude,
+            shell_thickness=self.shell_thickness.to_base_units().magnitude,
+            core_diameter=self.core_diameter.to_base_units().magnitude,
+            medium_refractive_index=self.medium_index.to_base_units().magnitude,
+            source=self.source.binding
+        )
+
+    def an(self) -> numpy.ndarray:
+        r"""
+        Compute :math:`a_n` coefficient.
+
+        Returns
+        -------
+        numpy.ndarray
+            Array of :math:`a_n` coefficients.
+        """
+        return self.binding.an()
+
+    def bn(self) -> numpy.ndarray:
+        r"""
+        Compute :math:`b_n` coefficient.
+
+        Returns
+        -------
+        numpy.ndarray
+            Array of :math:`b_n` coefficients.
+        """
+        return self.binding.bn()
