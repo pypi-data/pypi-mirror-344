f55726bbe52f2cb0323e02fe1e493e59d364c9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 14:05:19 +0000

    Add package bingefriend-shows-core/0.2.0/bingefriend_shows_core-0.2.0-py3-none-any.whl

---
+++ packages/bingefriend-shows-core/bingefriend_shows_core-0.2.0-py3-none-any.whl/bingefriend/shows/core/models/web_channel.py
@@ -0,0 +1,29 @@
+"""Web Channel Model"""
+
+from typing import Optional, TYPE_CHECKING
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from bingefriend.shows.core.models.base import Base
+
+if TYPE_CHECKING:
+    from bingefriend.shows.core.models.season import Season
+    from bingefriend.shows.core.models.show import Show
+
+
+class WebChannel:
+    """Represents a web channel"""
+
+    __tablename__ = "web_channel"
+
+    # Attributes
+    id: Mapped[int] = mapped_column(primary_key=True)
+    maze_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    name: Mapped[Optional[str]] = mapped_column(String(255))
+    country_name: Mapped[Optional[str]] = mapped_column(String(255))
+    country_code: Mapped[Optional[str]] = mapped_column(String(255))
+    country_timezone: Mapped[Optional[str]] = mapped_column(String(255))
+    official_site: Mapped[Optional[str]] = mapped_column(String(255))
+
+    # Relationships - referencing this model
+    shows: Mapped[list["Show"]] = mapped_column(back_populates="web_channel")
+    seasons: Mapped[list["Season"]] = mapped_column(back_populates="web_channel")
