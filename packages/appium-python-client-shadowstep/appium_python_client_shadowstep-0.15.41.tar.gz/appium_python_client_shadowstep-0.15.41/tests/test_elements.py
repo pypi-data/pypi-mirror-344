64a8b7549c783ea9c1ac9ccf99769aebec478409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:20:39 +0000

    Add package appium-python-client-shadowstep/0.15.41/appium_python_client_shadowstep-0.15.41.tar.gz

---
+++ packages/appium-python-client-shadowstep/appium_python_client_shadowstep-0.15.41.tar.gz/appium_python_client_shadowstep-0.15.41/tests/test_elements.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import time
+
+from icecream import ic
+
+from shadowstep.element.element import Element
+from shadowstep.shadowstep import Shadowstep
+
+
+class TestElements:
+    """
+    A class to test element interactions within the Shadowstep application.
+    """
+
