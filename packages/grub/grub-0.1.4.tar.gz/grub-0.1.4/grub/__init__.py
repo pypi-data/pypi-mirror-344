d25880d9d7d97d00c3a2efbfb5bcdfb94bd04809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 17:04:40 +0000

    Add package grub/0.1.4/grub-0.1.4.tar.gz

---
+++ packages/grub/grub-0.1.4.tar.gz/grub-0.1.4/grub/__init__.py
@@ -0,0 +1,11 @@
+"""Make a search object in the simplest way possible"""
+
+from grub.base import (
+    SearchStore,
+    CodeSearcher,
+    TfidfKnnSearcher,
+    grub,
+    grubber,
+    camelcase_and_underscore_tokenizer,
+)
+from grub.pycode import search_documented_attributes
