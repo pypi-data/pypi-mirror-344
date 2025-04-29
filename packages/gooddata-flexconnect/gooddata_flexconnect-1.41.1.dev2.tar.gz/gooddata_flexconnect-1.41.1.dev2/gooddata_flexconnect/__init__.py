22675ded01e7c0808757c49f8d3d1eeac1c10a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:19:51 +0000

    Add package gooddata-flexconnect/1.41.1.dev2/gooddata_flexconnect-1.41.1.dev2.tar.gz

---
+++ packages/gooddata-flexconnect/gooddata_flexconnect-1.41.1.dev2.tar.gz/gooddata_flexconnect-1.41.1.dev2/gooddata_flexconnect/__init__.py
@@ -0,0 +1,18 @@
+# (C) 2024 GoodData Corporation
+
+from gooddata_flexconnect.function.execution_context import (
+    ExecutionContext,
+    ExecutionContextAbsoluteDateFilter,
+    ExecutionContextAttribute,
+    ExecutionContextAttributeSorting,
+    ExecutionContextFilter,
+    ExecutionContextNegativeAttributeFilter,
+    ExecutionContextPositiveAttributeFilter,
+    ExecutionContextRelativeDateFilter,
+    ExecutionRequest,
+    ExecutionType,
+    LabelElementsExecutionRequest,
+    ReportExecutionRequest,
+)
+from gooddata_flexconnect.function.flight_methods import create_flexconnect_flight_methods
+from gooddata_flexconnect.function.function import FlexConnectFunction
