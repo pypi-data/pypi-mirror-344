34488050c229bb0083f3945d6b40d828ec44173b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:27 +0000

    Add package prometheus-swarm/0.1.6/prometheus_swarm-0.1.6-py3-none-any.whl

---
+++ packages/prometheus-swarm/prometheus_swarm-0.1.6-py3-none-any.whl/prometheus_swarm/tools/repo_operations/Types.py
@@ -0,0 +1,58 @@
+from enum import Enum
+import json
+
+class RepoType(Enum):
+    LIBRARY = "library"
+    WEB_APP = "web_app"
+    API_SERVICE = "api_service"
+    MOBILE_APP = "mobile_app"
+    TUTORIAL = "tutorial"
+    TEMPLATE = "template"
+    CLI_TOOL = "cli_tool"
+    FRAMEWORK = "framework"
+    DATA_SCIENCE = "data_science"
+    NONE = "none"
+    OTHER = "other"
+    @classmethod
+    def to_string_list(cls):
+        return [repo_type.value for repo_type in cls]
+
+class Language(Enum):
+    PYTHON = "python"
+    JAVASCRIPT = "javascript"
+    TYPESCRIPT = "typescript"
+    JAVA = "java"
+    C = "c"
+    CPP = "cpp"
+    GO = "go"
+    RUST = "rust"
+    RUBY = "ruby"
+    PHP = "php"
+    SWIFT = "swift"
+    KOTLIN = "kotlin"
+    SCALA = "scala"
+    R = "r"
+    SHELL = "shell"
+    NONE = "none"
+    OTHER = "other"
+    @classmethod
+    def to_string_list(cls):
+        return [lang.value for lang in cls]
+
+class TestFramework(Enum):
+    PYTEST = "pytest"
+    UNITTEST = "unittest"
+    JEST = "jest"
+    MOCHA = "mocha"
+    JUNIT = "junit"
+    TESTNG = "testng"
+    GOTESTING = "go_testing"
+    RSPEC = "rspec"
+    PHPUNIT = "phpunit"
+    XCTEST = "xctest"
+    KOTEST = "kotest"
+    NONE = "none"
+    OTHER = "other"
+    @classmethod
+    def to_string_list(cls):
+        return [test_framework.value for test_framework in cls]
