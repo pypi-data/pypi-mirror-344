7ecc0ca5b9d3385298d5efca9ef361b666b1dc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 15:04:34 +0000

    Add package numpysocket/2.0.0/numpysocket-2.0.0-py3-none-any.whl

---
+++ packages/numpysocket/numpysocket-2.0.0-py3-none-any.whl/numpysocket/numpysocket.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from io import BytesIO
+import logging
+import socket
+from typing import Any, Union
+
+import numpy as np
+
+
+class NumpySocket(socket.socket):
+    def sendall(self, frame: np.ndarray) -> None:  # type: ignore[override]
+        out = self.__pack_frame(frame)
+        super().sendall(out)
+        logging.debug("frame sent")
+
+    def recv(self, bufsize: int = 1024) -> np.ndarray:  # type: ignore[override]
+        length = None
+        frame_buffer = bytearray()
+        while True:
+            data = super().recv(bufsize)
+            if len(data) == 0:
+                return np.array([])
+            frame_buffer += data
+            if len(frame_buffer) == length:
+                break
+            while True:
+                if length is None:
+                    if b":" not in frame_buffer:
+                        break
+                    length_str, _, frame_buffer = frame_buffer.partition(b":")
+                    length = int(length_str)
+                if len(frame_buffer) < length:
+                    break
+
+                frame_buffer = frame_buffer[length:]
+                length = None
+                break
+
+        frame = np.load(BytesIO(frame_buffer), allow_pickle=True)["frame"]
+        logging.debug("frame received")
+        return frame
+
+    def accept(self) -> tuple["NumpySocket", Union[tuple[str, int], tuple[Any, ...]]]:
+        fd, addr = super()._accept()  # type: ignore
+        sock = NumpySocket(super().family, super().type, super().proto, fileno=fd)
+
+        if socket.getdefaulttimeout() is None and super().gettimeout():
+            sock.setblocking(True)
+        return sock, addr
+
+    @staticmethod
+    def __pack_frame(frame: np.ndarray) -> bytearray:
+        f = BytesIO()
+        np.savez(f, frame=frame)
+
+        packet_size = len(f.getvalue())
+        header = "{0}:".format(packet_size)
+        header_bytes = bytes(header.encode())  # prepend length of array
+
+        out = bytearray()
+        out += header_bytes
+
+        f.seek(0)
+        out += f.read()
+        return out
