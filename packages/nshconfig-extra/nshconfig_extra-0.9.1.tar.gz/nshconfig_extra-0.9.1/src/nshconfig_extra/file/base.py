e3c05010dff62fec148dba65f04c2e47e21651a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 06:04:20 +0000

    Add package nshconfig-extra/0.9.1/nshconfig_extra-0.9.1.tar.gz

---
+++ packages/nshconfig-extra/nshconfig_extra-0.9.1.tar.gz/nshconfig_extra-0.9.1/src/nshconfig_extra/file/base.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import contextlib
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any
+
+import nshconfig as C
+from typing_extensions import assert_never
+
+
+class BaseFileConfig(C.Config, ABC):
+    @abstractmethod
+    def resolve(self) -> Path:
+        """
+        Resolves the file and returns a local Path.
+        For remote files, this may involve downloading the file.
+        """
+
+    @abstractmethod
+    def open(self, mode: str = "rb") -> contextlib.AbstractContextManager[Any]:
+        """
+        Opens the file and returns a file handle wrapped in a context manager.
+        """
+
+
+def resolve_file_config(file: str | Path | BaseFileConfig):
+    match file:
+        case str() | Path():
+            return Path(file)
+        case BaseFileConfig():
+            return file.resolve()
+        case _:
+            assert_never(file)
+
+
+def open_file_config(
+    file: str | Path | BaseFileConfig,
+    mode: str = "rb",
+) -> contextlib.AbstractContextManager[Any]:
+    match file:
+        case str() | Path():
+            return open(file, mode)
+        case BaseFileConfig():
+            return file.open(mode)
+        case _:
+            assert_never(file)
