a9e98f8811104f0fb4f59a3ccb50b0c9a9025bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 16:05:04 +0000

    Add package chercher-plugin-pdf/0.1.4/chercher_plugin_pdf-0.1.4.tar.gz

---
+++ packages/chercher-plugin-pdf/chercher_plugin_pdf-0.1.4.tar.gz/chercher_plugin_pdf-0.1.4/chercher_plugin_pdf/__init__.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Generator
+from urllib.parse import urlparse, unquote
+import pymupdf
+from chercher import Document, hookimpl
+
+pymupdf.JM_mupdf_show_errors = 0
+
+
+def normalize_uri(uri: str) -> Path:
+    if uri.startswith("file://"):
+        parsed_uri = urlparse(uri)
+        decoded_path = unquote(parsed_uri.path)
+        return Path(decoded_path).resolve()
+
+    return Path(uri)
+
+
+@hookimpl
+def ingest(uri: str) -> Generator[Document, None, None]:
+    path = normalize_uri(uri)
+    if not path.exists() or not path.is_file() or path.suffix != ".pdf":
+        return
+
+    body = ""
+    with pymupdf.open(path) as doc:
+        for page in doc:
+            body += page.get_text()
+
+    yield Document(
+        uri=uri,
+        body=body,
+        metadata={},
+    )
