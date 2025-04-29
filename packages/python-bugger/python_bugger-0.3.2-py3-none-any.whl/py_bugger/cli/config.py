61f45a7edd628ee89efbe5b4c37a09c6dcf75a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:05:27 +0000

    Add package python-bugger/0.3.2/python_bugger-0.3.2-py3-none-any.whl

---
+++ packages/python-bugger/python_bugger-0.3.2-py3-none-any.whl/py_bugger/cli/config.py
@@ -0,0 +1,15 @@
+"""Config object to collect CLI options."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class PBConfig:
+    exception_type: str = ""
+    target_dir: Path = ""
+    target_file: Path = ""
+    num_bugs: int = 1
+
+
+pb_config = PBConfig()
