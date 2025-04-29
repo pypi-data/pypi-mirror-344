3c15a32ff7b3fa41b8f4db879ea6ea277edc995e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 03:20:36 +0000

    Add package terraform-runner/1.0.2/terraform_runner-1.0.2.tar.gz

---
+++ packages/terraform-runner/terraform_runner-1.0.2.tar.gz/terraform_runner-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terraform-runner",
+    version="1.0.2",
+    author="ctogaigenticai",
+    author_email="cto@gaigentic.ai",
+    description="A Terraform automation runner for AI-driven cloud deployments.",
+    long_description="A Python package to automate Terraform deployment workflows for agents and cloud services.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/monsterindian/gaigentic-infra",
+    project_urls={
+        "Bug Tracker": "https://github.com/monsterindian/gaigentic-infra/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    python_requires=">=3.7",
+)
