c6b20203a225dc2a4319d5f403e83b96075ddb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:23 +0000

    Add package django-enum/2.2.3/django_enum-2.2.3-py3-none-any.whl

---
+++ packages/django-enum/django_enum-2.2.3-py3-none-any.whl/django_enum/__init__.py
@@ -0,0 +1,25 @@
+r"""
+::
+
+  ____  _                           _____                       
+ |  _ \(_) __ _ _ __   __ _  ___   | ____|_ __  _   _ _ __ ___  
+ | | | | |/ _` | '_ \ / _` |/ _ \  |  _| | '_ \| | | | '_ ` _ \ 
+ | |_| | | (_| | | | | (_| | (_) | | |___| | | | |_| | | | | | |
+ |____// |\__,_|_| |_|\__, |\___/  |_____|_| |_|\__,_|_| |_| |_|
+     |__/             |___/                                     
+
+
+Full and natural support for enumerations as Django model fields.
+"""
+
+from django_enum.fields import EnumField
+
+__all__ = ["EnumField"]
+
+VERSION = (2, 2, 3)
+
+__title__ = "Django Enum"
+__version__ = ".".join(str(i) for i in VERSION)
+__author__ = "Brian Kohan"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-2025 Brian Kohan"
