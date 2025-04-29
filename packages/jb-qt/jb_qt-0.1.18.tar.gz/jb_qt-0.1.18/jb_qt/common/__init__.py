689b6d25047b1febc5f5929712c48d16f13d3a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 05:04:04 +0000

    Add package jb-qt/0.1.18/jb_qt-0.1.18.tar.gz

---
+++ packages/jb-qt/jb_qt-0.1.18.tar.gz/jb_qt-0.1.18/jb_qt/common/__init__.py
@@ -0,0 +1,6 @@
+"""Const exports"""
+
+from jb_qt.common.consts import COLORS, STYLES, SIZES, GlobalRefs, ICONS
+from jb_qt.common.qt_utils import register_app
+
+__all__ = ["COLORS", "STYLES", "SIZES", "GlobalRefs", "ICONS", "register_app"]
