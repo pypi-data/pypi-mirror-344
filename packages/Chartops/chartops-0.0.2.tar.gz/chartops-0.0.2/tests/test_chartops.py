04ab4c9a32ac751e371ae839e74e299494ded694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 07:04:51 +0000

    Add package Chartops/0.0.2/chartops-0.0.2.tar.gz

---
+++ packages/Chartops/chartops-0.0.2.tar.gz/chartops-0.0.2/tests/test_chartops.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `chartops` package."""
+
+
+import unittest
+
+from chartops import chartops
+
+
+class TestChartops(unittest.TestCase):
+    """Tests for `chartops` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
