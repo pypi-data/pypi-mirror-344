db47fc2832c63cde186b1cd978162ad71f013d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 04:04:24 +0000

    Add package norTools/1.2.1/nortools-1.2.1.tar.gz

---
+++ packages/norTools/nortools-1.2.1.tar.gz/nortools-1.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of README.md
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="norTools",  # Updated module name
+    version="1.2.1",  # Increment the version number here
+    description="A Python package for Dam, Dictor, and ScreenOpp utilities",
+    long_description=long_description,  # Use the contents of README.md
+    long_description_content_type="text/markdown",  # Specify the format of the long description
+    author="Syed",
+    packages=find_packages(),
+    install_requires=[
+        "flet",  # Add dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
