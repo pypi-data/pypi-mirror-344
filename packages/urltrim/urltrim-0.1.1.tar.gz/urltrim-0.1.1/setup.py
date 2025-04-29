eaa3c4cfd3f87e278bf7a82da3af63d3ec86d38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-29 16:04:54 +0000

    Add package urltrim/0.1.1/urltrim-0.1.1.tar.gz

---
+++ packages/urltrim/urltrim-0.1.1.tar.gz/urltrim-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="urltrim",
+    version="0.1.1",
+    author="PRANEETH KUMAR UDDARAJU",
+    author_email="upraneeth24@gmail.com",
+    description="A Python library to trim long URLs in code",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/upraneethkumar/urltrim",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+)
---
+++ packages/urltrim/urltrim-0.1.1.tar.gz/urltrim-0.1.1/urltrim/__init__.py
@@ -0,0 +1,3 @@
+from .core import URLTrim
+
+__version__ = "0.1.0"
