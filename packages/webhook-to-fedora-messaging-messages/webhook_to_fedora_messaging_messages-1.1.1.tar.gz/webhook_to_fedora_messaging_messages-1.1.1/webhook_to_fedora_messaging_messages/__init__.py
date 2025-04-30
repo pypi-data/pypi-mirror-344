299667b76a8234be5cc3716856dda19e7bfeca80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:47 +0000

    Add package webhook-to-fedora-messaging-messages/1.1.1/webhook_to_fedora_messaging_messages-1.1.1.tar.gz

---
+++ packages/webhook-to-fedora-messaging-messages/webhook_to_fedora_messaging_messages-1.1.1.tar.gz/webhook_to_fedora_messaging_messages-1.1.1/webhook_to_fedora_messaging_messages/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+from .forgejo import ForgejoMessageV1
+from .github import GitHubMessageV1
