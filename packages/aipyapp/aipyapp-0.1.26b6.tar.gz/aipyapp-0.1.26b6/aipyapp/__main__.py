e0eb1308df659f495b02ed3d49342a65c4007f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:21 +0000

    Add package aipyapp/0.1.26b6/aipyapp-0.1.26b6.tar.gz

---
+++ packages/aipyapp/aipyapp-0.1.26b6.tar.gz/aipyapp-0.1.26b6/aipyapp/__main__.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+
+class Logger:
+    def __init__(self, file_path=os.devnull):
+        self.terminal = sys.stdout
+        self.log = open(file_path, "a", encoding="utf-8")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
+
+def parse_args():
+    import argparse
+    from .aipy.config import CONFIG_DIR
+    config_help_message = (
+        f"Specify the configuration directory.\nDefaults to {CONFIG_DIR} if not provided."
+    )
+
+    parser = argparse.ArgumentParser(description="Python use - AIPython", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-c", '--config-dir', type=str, help=config_help_message)
+    parser.add_argument('-p', '--python', default=False, action='store_true', help="Python mode")
+    parser.add_argument('-g', '--gui', default=False, action='store_true', help="GUI mode")
+    parser.add_argument('--debug', default=False, action='store_true', help="Debug mode")
+    parser.add_argument('-f', '--fetch-config', default=False, action='store_true', help="login to trustoken and fetch token config")
+    parser.add_argument('cmd', nargs='?', default=None, help="Task to execute, e.g. 'Who are you?'")
+    return parser.parse_args()
+
+def ensure_wxpython():
+    try:
+        import wx
+    except:
+        import subprocess
+
+        cp = subprocess.run([sys.executable, "-m", "pip", "install", 'wxpython'])
+        assert cp.returncode == 0
+
+def mainw():
+    sys.stdout = Logger()
+    sys.stderr = Logger()
+    args = parse_args()
+    ensure_wxpython()
+    from .wxgui import main as aipy_main
+    aipy_main(args)
+
+def main():
+    args = parse_args()
+    if args.python:
+        from .main import main as aipy_main
+    elif args.gui:
+        ensure_wxpython()
+        from .wxgui import main as aipy_main
+    else:
+        from .saas import main as aipy_main
+    aipy_main(args)
+
+if __name__ == '__main__':
+    main()
