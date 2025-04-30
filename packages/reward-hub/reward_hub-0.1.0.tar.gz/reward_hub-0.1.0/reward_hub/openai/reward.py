3173d44df722998ab0d7723f765bff9c9cb7110e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 07:05:38 +0000

    Add package reward-hub/0.1.0/reward_hub-0.1.0.tar.gz

---
+++ packages/reward-hub/reward_hub-0.1.0.tar.gz/reward_hub-0.1.0/reward_hub/openai/reward.py
@@ -0,0 +1,123 @@
+from transformers import AutoTokenizer
+
+from typing import Union, List
+from reward_hub.base import AbstractOutcomeRewardModel, AbstractProcessRewardModel
+from reward_hub.openai.vllm_client import vllmClient
+from reward_hub.drsow import DrSow, DrSowConfig
+
+
+
+class OpenAIOutcomeRewardModel(AbstractOutcomeRewardModel):
+    def __init__(self, model_name: str = None, port: int = None, api_key: str = None, drsow_config: DrSowConfig = None, **kwargs):
+        self.model_name = model_name
+        self.drsow_config = drsow_config
+
+        if model_name == "drsow":
+            assert drsow_config is not None and isinstance(drsow_config, DrSowConfig)
+            strong_model = vllmClient(drsow_config.strong_model_name, drsow_config.strong_port)
+            weak_model = vllmClient(drsow_config.weak_model_name, drsow_config.weak_port)
+            self.tokenizer = AutoTokenizer.from_pretrained(drsow_config.strong_model_name)
+            self.weak_tokenizer = AutoTokenizer.from_pretrained(drsow_config.weak_model_name)
+            self.model = DrSow(strong_model, weak_model, self.tokenizer, self.weak_tokenizer)
+            self.tokenizer.truncation_side = "left"
+
+        elif port is not None:
+            self.model = vllmClient(model_name=model_name, port=port) # TODO: implement vllmClient
+            self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        elif api_key is not None:
+            raise NotImplementedError("OpenAI_OutcomeRM is not implemented")
+
+        else:
+            raise ValueError("Either port or api_key must be provided")
+
+    def score(self, messages: Union[List[List[dict]], List[dict]], max_input_tokens: int = 8192, num_workers: int = 40, return_raw_scores: bool = False, system_prompt: str = None, **kwargs) -> List[float]:
+        """
+        Score responses using the OpenAI chat completion format.
+        
+        Args:
+            messages: List of conversations in OpenAI chat completion format
+            max_input_tokens: Maximum number of input tokens
+            num_workers: Number of workers for parallel processing
+            return_raw_scores: Whether to return raw score details
+            **kwargs: Additional keyword arguments
+        """
+        if isinstance(messages[0], dict):
+            # ensure the input is a list of list of dicts   
+            messages = [messages]
+
+        if self.model_name == "drsow":
+            system_turn = [{
+                "role": "system",
+                "content": system_prompt
+            }] if system_prompt is not None else []
+            formatted_convs = [
+                self.tokenizer.apply_chat_template(
+                    system_turn + conv_messages,
+                    add_generation_prompt=False,
+                    tokenize=False,
+                    max_length=max_input_tokens,
+                    truncation=True,
+                )
+                for conv_messages in messages
+            ]
+            
+            # Get prompts by excluding the last assistant message
+            formatted_prompts = [
+                self.tokenizer.apply_chat_template(
+                    system_turn + conv_messages[:-1],
+                    add_generation_prompt=True,
+                    tokenize=False
+                )
+                for conv_messages in messages
+            ]
+            
+            prepared_batch = [
+                {
+                    "formatted_conv": conv,
+                    "prompt": prompt
+                }
+                for prompt, conv in zip(formatted_prompts, formatted_convs)
+            ]
+
+            reward_results = self.model.get_batch_logprobs(prepared_batch, num_workers=num_workers)
+            scores = [x["avg_drsow_reward"] for x in reward_results]
+
+            if return_raw_scores:
+                return reward_results
+            else:
+                return scores
+        else:
+            raise NotImplementedError("OpenAI_OutcomeRM is not implemented")
+
+class OpenAIProcessRewardModel(AbstractProcessRewardModel):
+    def __init__(self, model_name: str, **kwargs):
+        raise NotImplementedError("OpenAI_ProcessRM is not implemented")
+
+
+
+if __name__ == "__main__":
+    drsow_config = DrSowConfig(
+        strong_model_name="Qwen/Qwen2.5-32B-instruct",
+        strong_port=8305,
+        weak_model_name="Qwen/Qwen2.5-32B",
+        weak_port=8306
+        )
+
+    reward_model = OpenAIOutcomeRewardModel(model_name="drsow", drsow_config=drsow_config)
+    raw_results = reward_model.score(
+        messages=[
+            [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Who is Michael Jordan?"},
+                {"role": "assistant", "content": "Michael Jordan is the greatest basketball player of all time"}
+            ],
+            [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Who is Michael Jordan?"},
+                {"role": "assistant", "content": "Michael Jordan is a good friend of mine who is from Ohio."}
+            ]
+        ],
+        return_raw_scores=True
+    )
+    print(raw_results)
