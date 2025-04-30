3f7a9549e948ba21b93728125b9c6293043da904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 16:07:35 +0000

    Add package fds.sdk.Cabot/0.12.0/fds.sdk.Cabot-0.12.0-py3-none-any.whl

---
+++ packages/fds.sdk.Cabot/fds.sdk.Cabot-0.12.0-py3-none-any.whl/fds/sdk/Cabot/apis/__init__.py
@@ -0,0 +1,21 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.buying_models_api import BuyingModelsApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from fds.sdk.Cabot.api.buying_models_api import BuyingModelsApi
+from fds.sdk.Cabot.api.classifications_api import ClassificationsApi
+from fds.sdk.Cabot.api.overview_models_api import OverviewModelsApi
+from fds.sdk.Cabot.api.selling_models_api import SellingModelsApi
+from fds.sdk.Cabot.api.sizing_models_api import SizingModelsApi
