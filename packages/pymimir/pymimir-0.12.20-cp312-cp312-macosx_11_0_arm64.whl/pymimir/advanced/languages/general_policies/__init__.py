c9a5223e30cc42e3c2cd3c5496a49bfbfe377eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:20 +0000

    Add package pymimir/0.12.20/pymimir-0.12.20-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/pymimir/pymimir-0.12.20-cp312-cp312-macosx_11_0_arm64.whl/pymimir/advanced/languages/general_policies/__init__.py
@@ -0,0 +1,37 @@
+
+from pymimir.pymimir.advanced.languages.general_policies import (
+    NamedConcept,
+    NamedConceptList,
+    NamedRole,
+    NamedRoleList,
+    NamedBoolean,
+    NamedBooleanList,
+    NamedNumerical,
+    NamedNumericalList,
+
+    Condition,
+    ConditionList,
+    PositiveBooleanCondition,
+    NegativeBooleanCondition,
+    GreaterNumericalCondition,
+    EqualNumericalCondition,
+
+    Effect,
+    EffectList,
+    PositiveBooleanEffect,
+    NegativeBooleanEffect,
+    UnchangedBooleanEffect,
+    IncreaseNumericalEffect,
+    DecreaseNumericalEffect,
+    UnchangedNumericalEffect,
+
+    Rule,
+    RuleList,
+    
+    SolvabilityStatus,
+    GeneralPolicy,
+
+    Repositories,
+
+    GeneralPolicyFactory,
+)
