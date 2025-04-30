1d256514c432ca62c81f2c12ee59f66f1a95fdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 08:05:08 +0000

    Add package pwkit/1.3.1/pwkit-1.3.1.tar.gz

---
+++ packages/pwkit/pwkit-1.3.1.tar.gz/pwkit-1.3.1/CHANGELOG.md
@@ -0,0 +1,88 @@
+# pwkit 1.3.1 (2025-04-29)
+
+- *Actually* fix `pwkit.astutil.get_2mass_epoch` (#26, @pkgw)
+- In CASA, update `dftphotom`, `dftdynspec`, and `dftspect` to
+  work with CASA 6.
+
+The DOI of this release is [10.5281/zenodo.15306716][vdoi].
+
+[vdoi]: https://doi.org/10.5281/zenodo.15306716
+
+
+# pwkit 1.3.0 (2025-04-25)
+
+- Add support for CASA 6! (#25, @pkgw). It's still incomplete, and there
+  may be some lingering bugs, but I am currently able to run most of my
+  processing pipeline on real VLA data.
+- In `imtool show`, pressing `f` or `p` will now fit a source at the position of
+  the mouse cursor. The `f` key will fit either a point or a Gaussian, depending
+  on what fits better; `p` will force a point-source fit.
+- Fix `pwkit.astutil.get_2mass_epoch` to use HTTPS to contact the Vizier API
+- More Numpy 2 compatibility fixes, including the use of `np.asarray` with a
+  `float` dtype instead of `np.asfarray`. This could potentially reduce
+  efficiency if the input was a `float32` and we're upgrading it to a `float64`.
+
+The DOI of this release is [10.5281/zenodo.15283107][vdoi].
+
+[vdoi]: https://doi.org/10.5281/zenodo.15283107
+
+
+# pwkit 1.2.2 (2024-10-11)
+
+- Correct fixup processing of args with non-None defaults in `pwkit.kwargv` (#24)
+- Correct the docs link in the README! (#23)
+
+The version DOI of this release is [10.5281/zenodo.13920308][vdoi].
+
+[vdoi]: https://doi.org/10.5281/zenodo.13920308
+
+
+# pwkit 1.2.1 (2024-09-13)
+
+- Fix `pwkit.parallel.SerialHelper.get_map()` for Python 3 (#22)
+- Fix `pwkit.lmmin.Problem.set_npar()` for recent Numpys (#20). I'm not sure if
+  we'll be compatible with Numpy 2.0, though.
+- Get the ReadTheDocs docs building again, and make it so that we check RTD
+  builds as part of pull request processing
+- README.md: fix some links to the documentation (@AstroGenevieve)
+
+
+# pwkit 1.2.0 (2023-06-18)
+
+- Remove the dependency on `six`
+- Remove the long-unused `pwkit.ndshow_gtk2` module
+- A further attempt to fix the ReadTheDocs build.
+
+
+# pwkit 1.1.1 (2023-06-18)
+
+No code changes.
+
+- The last release did not publish to PyPI due to a problem with
+  the CI automation; attempt to fix that.
+- Also attempt to fix the ReadTheDocs build.
+
+
+# pwkit 1.1.0 (2023-06-18)
+
+A test release, after many years of inactivity, because the previous release
+won't even import noawadays due to the use of very old Numpy features that have
+been removed.
+
+- Switch to Cranko CI and release automation
+- Smattering of modernizations
+- Replace deprecated `np.{int,float,complex,bool}` with corresponding built-in types
+- Remove uses of `np.asscalar()`, which has been removed in latest Numpys
+- `pwkit/inifile.py`: untested hacks to get it working on Python 3
+- `pwkit/astutil.py`: fix up `sastrom`
+- `pwkit/colormaps.py`: update to use Gtk 3
+
+
+# Version 1.0.0 (2019 Dec 19)
+
+Call this version 1.0.0!
+
+- Fixed `casatask bpplot` to work when not all spectral windows in the
+  bandpass solution contain the same number of channels.
+- Fixed `casatask bpplot` to work when an amplitude or phase solution is
+  exactly constant.
---
+++ packages/pwkit/pwkit-1.3.1.tar.gz/pwkit-1.3.1/setup.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+# Copyright 2014-2023 Peter Williams <peter@newton.cx> and collaborators.
+# Licensed under the MIT License.
+
+# I don't use the ez_setup module because it causes us to automatically build
+# and install a new setuptools module, which I'm not interested in doing.
+
+from setuptools import setup
+
+
+def get_long_desc():
+    in_preamble = True
+    lines = []
+
+    with open("README.md", "rt", encoding="utf8") as f:
+        for line in f:
+            if in_preamble:
+                if line.startswith("<!--pypi-begin-->"):
+                    in_preamble = False
+            else:
+                if line.startswith("<!--pypi-end-->"):
+                    break
+                else:
+                    lines.append(line)
+
+    lines.append(
+        """
+
+For more information, including installation instructions, please visit [the
+project homepage].
+
+[the project homepage]: https://pwkit.readthedocs.io/
+"""
+    )
+    return "".join(lines)
+
+
+setup(
+    name="pwkit",  # cranko project-name
+    version="1.3.1",  # cranko project-version
+    # This package actually *is* zip-safe, but I've run into issues with
+    # installing it as a Zip: in particular, the install sometimes fails with
+    # "bad local file header", and reloading a module after a reinstall in
+    # IPython gives an ImportError with the same message. These are annoying
+    # enough and I don't really care so we just install it as flat files.
+    zip_safe=False,
+    packages=[
+        "pwkit",
+        "pwkit.cli",
+        "pwkit.environments",
+        "pwkit.environments.casa",
+        "pwkit.environments.ciao",
+        "pwkit.environments.jupyter",
+        "pwkit.environments.sas",
+    ],
+    package_data={
+        "pwkit": ["data/*/*"],
+    },
+    # We want to go easy on the requires; some modules are going to require
+    # more stuff, but others don't need much of anything. But, it's pretty
+    # much impossible to do science without Numpy. We may actually require
+    # version 1.8 but for now I'll optimistically hope that we can get away
+    # with 1.6.
+    install_requires=[
+        "numpy >= 1.6",
+    ],
+    extras_require={
+        "docs": [
+            "mock",
+            "numpydoc",
+            "sphinx",
+            "sphinx_rtd_theme",
+            "sphinx-automodapi",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "astrotool = pwkit.cli.astrotool:commandline",
+            "casatask = pwkit.environments.casa.tasks:commandline",
+            "imtool = pwkit.cli.imtool:commandline",
+            "latexdriver = pwkit.cli.latexdriver:commandline",
+            "pkcasascript = pwkit.environments.casa.scripting:commandline",
+            "pkenvtool = pwkit.environments:commandline",
+            "wrapout = pwkit.cli.wrapout:commandline",
+        ],
+    },
+    author="Peter Williams",
+    author_email="peter@newton.cx",
+    description="Miscellaneous scientific and astronomical tools",
+    license="MIT",
+    keywords="astronomy science",
+    url="https://github.com/pkgw/pwkit/",
+    long_description=get_long_desc(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+)
