917b713ddba502242f120118d5cd2ff075d9dee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:03 +0000

    Add package GeoBay/0.0.52/geobay-0.0.52.tar.gz

---
+++ packages/GeoBay/geobay-0.0.52.tar.gz/geobay-0.0.52/docs/index.md
@@ -0,0 +1,16 @@
+# Welcome to Geobay
+
+
+[![image](https://img.shields.io/pypi/v/GeoBay.svg)](https://pypi.python.org/pypi/GeoBay)
+
+
+**python package for geog 510**
+
+
+-   Free software: MIT License
+-   Documentation: <https://Caseyshea96.github.io/GeoBay>
+
+
+## Features
+
+-   TODO
---
+++ packages/GeoBay/geobay-0.0.52.tar.gz/geobay-0.0.52/tests/test_geobay.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geobay` package."""
+
+
+import unittest
+
+from geobay import geobay
+
+
+class TestGeobay(unittest.TestCase):
+    """Tests for `geobay` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
