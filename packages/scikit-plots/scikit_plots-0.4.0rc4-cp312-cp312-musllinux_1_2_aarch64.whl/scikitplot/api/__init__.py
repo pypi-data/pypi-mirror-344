ca19d9304908e99ae5c6b66d489bc0beec82fa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:31 +0000

    Add package scikit-plots/0.4.0rc4/scikit_plots-0.4.0rc4-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0rc4-cp312-cp312-musllinux_1_2_aarch64.whl/scikitplot/api/__init__.py
@@ -0,0 +1,25 @@
+"""
+scikit-plots Functional API
+"""
+
+# Authors: The scikit-plots developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+######################################################################
+## scikit-plots Functional API modules
+######################################################################
+
+# Modules
+# Deprecated namespaces, to be removed in v0.5.0
+from . import (
+    decomposition as decomposition,
+)
+from . import (
+    estimators as estimators,
+)
+from . import (
+    metrics as metrics,
+)
+from . import (
+    plotters,  # noqa: F401,F403
+)
