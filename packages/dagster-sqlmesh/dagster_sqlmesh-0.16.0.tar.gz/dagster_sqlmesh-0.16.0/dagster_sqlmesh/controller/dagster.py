56426d1fa459681858e6436e6b1395de4941c3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 16:06:09 +0000

    Add package dagster-sqlmesh/0.16.0/dagster_sqlmesh-0.16.0.tar.gz

---
+++ packages/dagster-sqlmesh/dagster_sqlmesh-0.16.0.tar.gz/dagster_sqlmesh-0.16.0/dagster_sqlmesh/controller/dagster.py
@@ -0,0 +1,56 @@
+# pyright: reportPrivateImportUsage=false
+import logging
+
+from dagster import AssetDep, AssetKey, AssetOut
+from dagster._core.definitions.asset_dep import CoercibleToAssetDep
+
+from ..translator import SQLMeshDagsterTranslator
+from ..types import SQLMeshModelDep, SQLMeshMultiAssetOptions
+from ..utils import sqlmesh_model_name_to_key
+from .base import ContextCls, SQLMeshController
+
+logger = logging.getLogger(__name__)
+
+
+class DagsterSQLMeshController(SQLMeshController[ContextCls]):
+    """An extension of the sqlmesh controller specifically for dagster use"""
+
+    def to_asset_outs(
+        self, environment: str, translator: SQLMeshDagsterTranslator
+    ) -> SQLMeshMultiAssetOptions:
+        with self.instance(environment, "to_asset_outs") as instance:
+            context = instance.context
+            output = SQLMeshMultiAssetOptions()
+            depsMap: dict[str, CoercibleToAssetDep] = {}
+
+            for model, deps in instance.non_external_models_dag():
+                asset_key = translator.get_asset_key_from_model(
+                    context,
+                    model,
+                )
+                model_deps = [
+                    SQLMeshModelDep(fqn=dep, model=context.get_model(dep))
+                    for dep in deps
+                ]
+                internal_asset_deps: set[AssetKey] = set()
+                asset_tags = translator.get_tags(context, model)
+
+                for dep in model_deps:
+                    if dep.model:
+                        internal_asset_deps.add(
+                            translator.get_asset_key_from_model(context, dep.model)
+                        )
+                    else:
+                        table = translator.get_fqn_to_table(context, dep.fqn)
+                        key = translator.get_asset_key_fqn(context, dep.fqn)
+                        internal_asset_deps.add(key)
+                        # create an external dep
+                        depsMap[table.name] = AssetDep(key)
+                model_key = sqlmesh_model_name_to_key(model.name)
+                output.outs[model_key] = AssetOut(
+                    key=asset_key, tags=asset_tags, is_required=False
+                )
+                output.internal_asset_deps[model_key] = internal_asset_deps
+
+            output.deps = list(depsMap.values())
+            return output
---
+++ packages/dagster-sqlmesh/dagster_sqlmesh-0.16.0.tar.gz/dagster_sqlmesh-0.16.0/dagster_sqlmesh/translator.py
@@ -0,0 +1,31 @@
+
+import sqlglot
+from dagster import AssetKey
+from sqlglot import exp
+from sqlmesh.core.context import Context
+from sqlmesh.core.model import Model
+
+
+class SQLMeshDagsterTranslator:
+    """Translates sqlmesh objects for dagster"""
+
+    def get_asset_key_from_model(self, context: Context, model: Model) -> AssetKey:
+        """Given the sqlmesh context and a model return the asset key"""
+        return AssetKey(model.view_name)
+
+    def get_asset_key_fqn(self, context: Context, fqn: str) -> AssetKey:
+        """Given the sqlmesh context and a fqn of a model return an asset key"""
+        table = self.get_fqn_to_table(context, fqn)
+        return AssetKey(table.name)
+
+    def get_fqn_to_table(self, context: Context, fqn: str) -> exp.Table:
+        """Given the sqlmesh context and a fqn return the table"""
+        dialect = self._get_context_dialect(context)
+        return sqlglot.to_table(fqn, dialect=dialect)
+
+    def _get_context_dialect(self, context: Context) -> str:
+        return context.engine_adapter.dialect
+
+    def get_tags(self, context: Context, model: Model) -> dict[str, str]:
+        """Given the sqlmesh context and a model return the tags for that model"""
+        return {k: "true" for k in model.tags}
