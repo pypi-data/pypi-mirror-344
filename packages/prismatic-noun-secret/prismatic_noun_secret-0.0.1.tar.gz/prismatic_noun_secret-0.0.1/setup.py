4ef9909a9df09702b4dd12fc55397dc73a365d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:36 +0000

    Add package prismatic-noun-secret/0.0.1/prismatic_noun_secret-0.0.1.tar.gz

---
+++ packages/prismatic-noun-secret/prismatic_noun_secret-0.0.1.tar.gz/prismatic_noun_secret-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='prismatic_noun_secret',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://prismaticnoun.xyz',
+    author='me',
+    author_email='james.jrg@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
