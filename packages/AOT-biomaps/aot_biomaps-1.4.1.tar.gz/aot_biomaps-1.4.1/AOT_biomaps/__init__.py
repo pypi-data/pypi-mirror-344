d826919d97bd17c2347b093207365c20ff2fb62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:33 +0000

    Add package AOT-biomaps/1.4.1/aot_biomaps-1.4.1.tar.gz

---
+++ packages/AOT-biomaps/aot_biomaps-1.4.1.tar.gz/aot_biomaps-1.4.1/AOT_biomaps/__init__.py
@@ -0,0 +1,16 @@
+from .AOT_Acoustic import *
+from .AOT_Optic import *
+from .AOT_AOsignal import *
+from .config import config
+
+__version__ = '1.4.1'
+
+if config.get_process() == 'gpu':
+    __process__ = 'gpu'
+else:
+    __process__ = 'cpu'
+
+def initialize(process='cpu'):
+    config.set_process(process)
+    print(f"Initialized with process: {config.get_process()}")
+
