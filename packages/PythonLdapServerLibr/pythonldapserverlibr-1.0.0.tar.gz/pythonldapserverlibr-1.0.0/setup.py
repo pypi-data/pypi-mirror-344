34efdc99e19d2e546afbe6e9ab8055cbcdb1a80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:37 +0000

    Add package PythonLdapServerLibr/1.0.0/pythonldapserverlibr-1.0.0.tar.gz

---
+++ packages/PythonLdapServerLibr/pythonldapserverlibr-1.0.0.tar.gz/pythonldapserverlibr-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+name='PythonLdapServerLibr',
+version='1.0.0',
+description='my one and only project!',
+long_description="* 'sup.\n\n* this project decodes as:\n\tp - python\n\tl - ldap\n\ts - server\n\tl - lib",
+long_description_content_type='text/plain',
+author='grlviv',
+classifiers=[
+    'Programming Language :: Python :: 3',
+],
+keywords='PythonLdapServerLibr',
+)
