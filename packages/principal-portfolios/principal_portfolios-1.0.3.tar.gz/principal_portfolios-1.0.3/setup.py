d2019610858722f87899a257ce9615e82443186a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:41 +0000

    Add package principal-portfolios/1.0.3/principal_portfolios-1.0.3.tar.gz

---
+++ packages/principal-portfolios/principal_portfolios-1.0.3.tar.gz/principal_portfolios-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='principal_portfolios',
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=[],
+    author='Amin Izadyar',
+    author_email='a.izadyar23@imperial.ac.uk',
+    description='A Python implementation of the Principal Portfolios methodology by Kelly, Malamud, and Pedersen (2023), enabling optimal asset allocation by exploiting cross-predictability among asset returns.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # ✅ Add this
+    url='https://github.com/aminizadyar/Principal-Portfolios',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
