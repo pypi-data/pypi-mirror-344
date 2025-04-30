fa5859cce4e2b742f6fb0d079ea6e290879996e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 08:05:12 +0000

    Add package charliehr-client/1.0.0/charliehr_client-1.0.0.tar.gz

---
+++ packages/charliehr-client/charliehr_client-1.0.0.tar.gz/charliehr_client-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='charliehr-client',
+    version='1.0.0',
+    packages=['charliehr-client'],
+    include_package_data=True,
+    install_requires=['requests'],
+)
