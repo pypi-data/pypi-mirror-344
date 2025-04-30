51dcf678b7b3b8c2570baedaccc92665da5043ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:29 +0000

    Add package lmoran-pack/0.1.2/lmoran_pack-0.1.2.tar.gz

---
+++ packages/lmoran-pack/lmoran_pack-0.1.2.tar.gz/lmoran_pack-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='lmoran_pack',
+version='0.1.2',
+author='Loic Moran',
+author_email='lmoran@student.42.fr',
+description='Is a luh package yer',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
