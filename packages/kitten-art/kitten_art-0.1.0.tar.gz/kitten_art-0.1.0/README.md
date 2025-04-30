cb0c3fc5a143d8d94474843df9c377892f4f6d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 06:04:40 +0000

    Add package kitten-art/0.1.0/kitten_art-0.1.0.tar.gz

---
+++ packages/kitten-art/kitten_art-0.1.0.tar.gz/kitten_art-0.1.0/README.md
@@ -0,0 +1,8 @@
+# Kitten Art
+
+A simple Python library for displaying cute kitten ASCII art.
+
+## Installation
+
+```bash
+pip install kitten-art
---
+++ packages/kitten-art/kitten_art-0.1.0.tar.gz/kitten_art-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kitten-art",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A library for displaying cute kitten ASCII art",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
