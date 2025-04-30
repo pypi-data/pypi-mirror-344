9df3c92e297c9eb2ea32cb7adc036a8d104e095e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:38 +0000

    Add package LockFileSemaphore/3.0.0/lockfilesemaphore-3.0.0.tar.gz

---
+++ packages/LockFileSemaphore/lockfilesemaphore-3.0.0.tar.gz/lockfilesemaphore-3.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LockFileSemaphore",
+    version="3.0.0",
+    author="Marcin Kowalczyk",
+    description="Robust file-based semaphore with stale lock recovery and context manager support.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
