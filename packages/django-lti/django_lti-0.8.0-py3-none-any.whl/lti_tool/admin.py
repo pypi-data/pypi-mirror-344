21106217c1f8e0a134a06c11d5d1a4f1de311809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:05:00 +0000

    Add package django-lti/0.8.0/django_lti-0.8.0-py3-none-any.whl

---
+++ packages/django-lti/django_lti-0.8.0-py3-none-any.whl/lti_tool/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.utils.translation import gettext as _
+
+from . import models
+
+
+class LtiDeploymentInline(admin.TabularInline):
+    model = models.LtiDeployment
+
+
+@admin.register(models.LtiRegistration)
+class LtiRegistrationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "name",
+                    "uuid",
+                    "issuer",
+                    "client_id",
+                    "audience",
+                    "auth_url",
+                    "token_url",
+                    "keyset_url",
+                    "is_active",
+                ]
+            },
+        ),
+        (
+            _("Static keys"),
+            {
+                "description": _("For platforms not supporting JWK Set use by tools."),
+                "fields": ["public_key", "private_key"],
+                "classes": ["collapse"],
+            },
+        ),
+    ]
+    inlines = [LtiDeploymentInline]
