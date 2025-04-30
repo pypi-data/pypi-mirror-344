9e74044bf6c2900e13dcc696e1463e72daf1cd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 07:05:14 +0000

    Add package invenio-oaiserver/2.4.0/invenio_oaiserver-2.4.0.tar.gz

---
+++ packages/invenio-oaiserver/invenio_oaiserver-2.4.0.tar.gz/invenio_oaiserver-2.4.0/CHANGES.rst
@@ -0,0 +1,139 @@
+..
+    This file is part of Invenio.
+    Copyright (C) 2016-2022 CERN.
+    Copyright (C) 2025 Graz University of Technology.
+
+    Invenio is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
+
+Changes
+=======
+
+Version v2.4.0 (released 2025-04-29)
+
+- i18n: pulled translations
+- i18n: push translations
+
+Version v2.3.1 (released 2025-03-19)
+
+- i18n: mark missing strings for translations
+
+Version v2.3.0 (released 2024-12-13)
+
+- percolator: use `OAISERVER_RECORD_INDEX` to determine percolator index
+    * Instead of relying on the default `RecordIndexer` class to determine
+      the percolator index, we reuse the `OAISERVER_RECORD_INDEX` config variable.
+
+Version v2.2.3 (released 2024-09-25)
+
+- percolator: allow lazy strings from config for deletions
+
+Version v2.2.2 (released 2024-09-11)
+
+- percolator: allow lazy strings from config
+
+Version 2.2.1 (released 2023-11-09)
+
+- verbs: fix `from` parameter
+
+Version 2.2.0 (released 2023-03-02)
+
+- remove deprecated flask-babelex dependency and imports
+- upgrade invenio_i18n
+
+Version 2.1.1 (released 2022-11-29)
+
+- Add translations
+
+Version 2.1.0 (released 2022-10-03)
+
+- Add support to OpenSearch v2
+- Remove `doc_type` using search APIs
+
+Version 2.0.1 (released 2022-09-27)
+
+- Bump invenio-records
+- Remove invenio-marc21 from tests
+
+Version 2.0.0 (released 2022-09-25)
+
+- Add support to OpenSearch
+- Drop support to Elasticsearch < 7
+- Upper pin Invenio dependencies
+- Breaking: rename kwargs `document_es_ids` to `document_search_ids` and
+  `document_es_indices` to `document_search_indices` in funcs
+  `create_percolate_query` and `percolate_query`
+
+Version 1.5.0 (released 2022-09-21)
+
+- Adds system created flag field to oai-pmh set model
+  to mark auto created sets
+
+Version 1.4.2 (released 2022-05-23)
+
+- Adds support for Flask v2.1
+
+Version 1.4.1 (released 2022-03-01)
+
+- Fixes marshmallow deprecation warning.
+
+Version 1.4.0 (released 2022-02-22)
+
+- OAI-PMH sets reimplementation via percolator queries during result fetching.
+- Removes Python 2.7 support.
+- Resumption token argument fixes.
+
+Version 1.3.0 (released 2021-10-20)
+
+- Unpin Flask version.
+
+Version 1.2.2 (released 2021-09-17)
+
+- Adds support for more easily integrating the OAI-PMH server in InvenioRDM.
+
+- Adds new configuration variables for dependency injection of search class,
+  ID fetcher, record getter tec.
+
+- The release is fully backward compatibility and does not change any
+  behaviour.
+
+- Fixes a bug with Elasticsearch 7 causing invalid OAI-PMH output for the
+  resumption token.
+
+Version 1.2.1 (released 2021-07-12)
+
+- Adds german translations
+
+Version 1.2.0 (released 2020-03-17)
+
+- Removes support for python 2.7
+- Centralises management of Flask dependency via invenio-base
+
+Version 1.1.2 (released 2019-07-19)
+
+- fixes default config OAISERVER_QUERY_PARSER_FIELDS
+- changes celery support module to invenio-celery
+
+Version 1.1.1 (released 2019-07-31)
+
+- Added support for Marshmallow v2 and v3.
+
+Version 1.1.0 (released 2019-07-31)
+
+- Added support for Eleasticsearch v7
+
+Version 1.0.3 (released 2019-02-15)
+
+- Pins marshmallow package to <3 in preparation for upcoming v3.0.0 release
+  which will break compatibility.
+
+Version 1.0.2 (released 2019-01-10)
+
+- Improved performance of the *Identify* verb response by fetching earliest
+  record date from Elasticsearch.
+
+Version 1.0.1 (released 2018-12-14)
+
+Version 1.0.0 (released 2018-03-23)
+
+- Initial public release.
---
+++ packages/invenio-oaiserver/invenio_oaiserver-2.4.0.tar.gz/invenio_oaiserver-2.4.0/invenio_oaiserver/percolator.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2017-2024 CERN.
+# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2024 KTH Royal Institute of Technology.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Percolator."""
+
+import json
+
+from flask import current_app
+from invenio_i18n import lazy_gettext as _
+from invenio_search import current_search, current_search_client
+from invenio_search.engine import search
+from invenio_search.utils import build_index_name
+
+from invenio_oaiserver.query import query_string_parser
+
+
+def _build_percolator_index_name(index):
+    """Build percolator index name."""
+    suffix = "-percolators"
+    return build_index_name(index, suffix=suffix, app=current_app)
+
+
+def _create_percolator_mapping(index, mapping_path=None):
+    """Update mappings with the percolator field.
+
+    .. note::
+
+        This is only needed from ElasticSearch v5 onwards, because percolators
+        are now just a special type of field inside mappings.
+    """
+    percolator_index = _build_percolator_index_name(index)
+    if not mapping_path:
+        mapping_path = current_search.mappings[index]
+    if not current_search_client.indices.exists(percolator_index):
+        with open(mapping_path, "r") as body:
+            mapping = json.load(body)
+            mapping["mappings"]["properties"].update(PERCOLATOR_MAPPING["properties"])
+            current_search_client.indices.create(index=percolator_index, body=mapping)
+
+
+PERCOLATOR_MAPPING = {"properties": {"query": {"type": "percolator"}}}
+
+
+def _new_percolator(spec, search_pattern):
+    """Create new percolator associated with the new set."""
+    if spec and search_pattern:
+        query = query_string_parser(search_pattern=search_pattern).to_dict()
+
+        # NOTE: We call `str` so that we can also handle lazy values (e.g. a LocalProxy)
+        oai_records_index = str(current_app.config["OAISERVER_RECORD_INDEX"])
+        for index, mapping_path in current_search.mappings.items():
+            # Skip indices/mappings not used by OAI-PMH
+            if not index.startswith(oai_records_index):
+                continue
+            # Create the percolator doc_type in the existing index for >= ES5
+            # TODO: Consider doing this only once in app initialization
+            try:
+                _create_percolator_mapping(index, mapping_path)
+                current_search_client.index(
+                    index=_build_percolator_index_name(index),
+                    id="oaiset-{}".format(spec),
+                    body={"query": query},
+                )
+            except Exception as e:
+                current_app.logger.warning(e)
+
+
+def _delete_percolator(spec, search_pattern):
+    """Delete percolator associated with the removed/updated oaiset."""
+    # NOTE: We call `str` so that we can also handle lazy values (e.g. a LocalProxy)
+    oai_records_index = str(current_app.config["OAISERVER_RECORD_INDEX"])
+    # Create the percolator doc_type in the existing index for >= ES5
+    for index, mapping_path in current_search.mappings.items():
+        # Skip indices/mappings not used by OAI-PMH
+        if not index.startswith(oai_records_index):
+            continue
+        current_search_client.delete(
+            index=_build_percolator_index_name(index),
+            id="oaiset-{}".format(spec),
+            ignore=[404],
+        )
+
+
+def create_percolate_query(
+    percolator_ids=None,
+    documents=None,
+    document_search_ids=None,
+    document_search_indices=None,
+):
+    """Create percolate query for provided arguments."""
+    queries = []
+    # documents or (document_search_ids and document_search_indices) has to be set
+    # TODO: discuss if this is needed or documents alone is enough.
+    if documents is not None:
+        queries.append(
+            {
+                "percolate": {
+                    "field": "query",
+                    "documents": documents,
+                }
+            }
+        )
+    elif (
+        document_search_ids is not None
+        and document_search_indices is not None
+        and len(document_search_ids) == len(document_search_indices)
+    ):
+        queries.extend(
+            [
+                {
+                    "percolate": {
+                        "field": "query",
+                        "index": search_index,
+                        "id": search_id,
+                        "name": f"{search_index}:{search_id}",
+                    }
+                }
+                for (search_id, search_index) in zip(
+                    document_search_ids, document_search_indices
+                )
+            ]
+        )
+    else:
+        raise Exception(
+            _(
+                "Either documents or (document_search_ids and document_search_indices) must be specified."
+            )
+        )
+
+    if percolator_ids:
+        queries.append({"ids": {"values": percolator_ids}})
+
+    query = {"query": {"bool": {"must": queries}}}
+    return query
+
+
+def percolate_query(
+    percolator_index,
+    percolator_ids=None,
+    documents=None,
+    document_search_ids=None,
+    document_search_indices=None,
+):
+    """Get results for a percolate query."""
+    query = create_percolate_query(
+        percolator_ids=percolator_ids,
+        documents=documents,
+        document_search_ids=document_search_ids,
+        document_search_indices=document_search_indices,
+    )
+    result = search.helpers.scan(
+        current_search_client,
+        index=percolator_index,
+        query=query,
+        scroll="1m",
+    )
+    return result
+
+
+def sets_search_all(records):
+    """Retrieve sets for provided records."""
+    if not records:
+        return []
+
+    record_index = str(current_app.config["OAISERVER_RECORD_INDEX"])
+    # TODO: We shouldn't have to always create the percolator mapping here
+    _create_percolator_mapping(record_index)
+    percolator_index = _build_percolator_index_name(record_index)
+    record_sets = [[] for _ in range(len(records))]
+
+    result = percolate_query(percolator_index, documents=records)
+
+    prefix = "oaiset-"
+    prefix_len = len(prefix)
+
+    for s in result:
+        set_index_id = s["_id"]
+        if set_index_id.startswith(prefix):
+            set_spec = set_index_id[prefix_len:]
+            for record_index in s.get("fields", {}).get(
+                "_percolator_document_slot", []
+            ):
+                record_sets[record_index].append(set_spec)
+    return record_sets
+
+
+def find_sets_for_record(record):
+    """Fetch a record's sets."""
+    return sets_search_all([record])[0]
