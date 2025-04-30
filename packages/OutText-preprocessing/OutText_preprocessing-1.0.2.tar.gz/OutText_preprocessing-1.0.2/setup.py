b452748d33c2481c9b4a953b79baec56e3728270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:46 +0000

    Add package OutText-preprocessing/1.0.2/OutText_preprocessing-1.0.2.tar.gz

---
+++ packages/OutText-preprocessing/OutText_preprocessing-1.0.2.tar.gz/OutText_preprocessing-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OutText_preprocessing',
+    version='1.0.2',  # <-- Changed this version
+    description='A package for outlier removal and text preprocessing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Anurag Raj',
+    author_email='anuragraj4483@gmail.com',
+    license="MIT",
+    url="https://github.com/Anurag-raj03/OutText_preprocessing_library.git",
+    packages=find_packages(),  # Automatically discover all packages
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "nltk",
+        "emoji",
+        "autocorrect",
+        "pytest",
+    ],
+    python_requires='>=3.6',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
