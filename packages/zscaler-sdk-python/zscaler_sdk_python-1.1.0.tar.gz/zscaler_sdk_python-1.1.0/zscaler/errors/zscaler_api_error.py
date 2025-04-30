0a93fe647d3e141a2a517129fecfad79874d9229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:19 +0000

    Add package zscaler-sdk-python/1.1.0/zscaler_sdk_python-1.1.0.tar.gz

---
+++ packages/zscaler-sdk-python/zscaler_sdk_python-1.1.0.tar.gz/zscaler_sdk_python-1.1.0/zscaler/errors/zscaler_api_error.py
@@ -0,0 +1,28 @@
+from zscaler.errors.error import Error
+
+
+class ZscalerAPIError(Error):
+    def __init__(self, url, response_details, response_body):
+        self.status_code = response_details.status_code
+        self.error_id = response_body.get("id", "")
+        self.reason = response_body.get("reason", "")
+        self.params = response_body.get("params", [])
+        self.error = response_body.get("error", "")
+        self.path = response_body.get("path", "")
+
+        # Constructing the message dynamically based on available fields
+        message_parts = [f"HTTP {self.status_code}"]
+
+        if self.error_id:
+            message_parts.append(self.error_id)
+        if self.reason:
+            message_parts.append(self.reason)
+        if self.params:
+            params_string = ", ".join(self.params)
+            message_parts.append(f"Parameters: {params_string}")
+
+        self.message = " ".join(message_parts)
+
+        self.url = url
+        self.headers = response_details.headers
+        self.stack = ""
