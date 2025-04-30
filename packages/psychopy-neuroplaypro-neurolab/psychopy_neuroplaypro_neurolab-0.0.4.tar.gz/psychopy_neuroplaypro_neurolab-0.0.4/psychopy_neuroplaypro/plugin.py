e3ace57dd0dd075f352c479e13ab88dfb838b8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 15:06:22 +0000

    Add package psychopy-neuroplaypro-neurolab/0.0.4/psychopy_neuroplaypro_neurolab-0.0.4.tar.gz

---
+++ packages/psychopy-neuroplaypro-neurolab/psychopy_neuroplaypro_neurolab-0.0.4.tar.gz/psychopy_neuroplaypro_neurolab-0.0.4/psychopy_neuroplaypro/plugin.py
@@ -0,0 +1,8 @@
+from psychopy.plugins import Plugin
+from psychopy.experiment.components import registerComponent
+from psychopy_neuroplaypro.components.neuroplay import NeuroPlayComponent
+
+class NeuroPlayPlugin(Plugin):
+    def onLoad(self):
+        registerComponent("NeuroPlay", NeuroPlayComponent)
+        print("✅ NeuroPlay plugin loaded")
