8068fd625a6b4ca4291f0745201cef243ad7434c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:05:13 +0000

    Add package kawapythonserver/0.31.0b1/kawapythonserver-0.31.0b1.tar.gz

---
+++ packages/kawapythonserver/kawapythonserver-0.31.0b1.tar.gz/kawapythonserver-0.31.0b1/src/kawapythonserver/scripts/kawa_loader_callback.py
@@ -0,0 +1,15 @@
+import typing
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+
+class LoaderCallback(ABC):
+
+    @abstractmethod
+    def load(self, df: pd.DataFrame):
+        pass
+
+    @abstractmethod
+    def retrieve_data(self) -> pd.DataFrame:
+        pass
