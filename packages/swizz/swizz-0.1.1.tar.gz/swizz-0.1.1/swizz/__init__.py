2edf9d12213a7fa6cb8c62b718aa9aebaeab4c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 17:05:35 +0000

    Add package swizz/0.1.1/swizz-0.1.1.tar.gz

---
+++ packages/swizz/swizz-0.1.1.tar.gz/swizz-0.1.1/swizz/__init__.py
@@ -0,0 +1,14 @@
+from swizz.plots.base import set_style
+from swizz.plot import plot, available_plots
+from swizz.table import table, available_tables
+from swizz.layout import layout, available_layouts
+
+# Set the default style to latex
+set_style("latex")
+
+__version__ = "0.1.1"
+__author__ = "Lars Quaedvlieg"
+__email__ = "larsquaedvlieg@outlook.com"
+
+__all__ = ["set_style", "table", "plot", "layout", "available_layouts", "available_tables", "available_plots",
+           "__version__", "__author__", "__email__"]
