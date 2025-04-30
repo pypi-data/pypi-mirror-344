38d96d2bae1f83bca6d1bee3322d73af52a4755a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 16:06:09 +0000

    Add package kuzu/0.9.1.dev30/kuzu-0.9.1.dev30.tar.gz

---
+++ packages/kuzu/kuzu-0.9.1.dev30.tar.gz/sdist/kuzu-source/src/include/function/gds/gds_object_manager.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include <atomic>
+
+#include "storage/buffer_manager/memory_manager.h"
+
+namespace kuzu {
+namespace function {
+
+// ObjectBlock represents a pre-allocated amount of memory that can hold up to maxElements objects
+// ObjectBlock should be accessed by a single thread.
+template<typename T>
+class ObjectBlock {
+public:
+    ObjectBlock(std::unique_ptr<storage::MemoryBuffer> block, uint64_t sizeInBytes)
+        : block{std::move(block)} {
+        maxElements.store(sizeInBytes / (sizeof(T)), std::memory_order_relaxed);
+        nextPosToWrite.store(0, std::memory_order_relaxed);
+    }
+
+    T* reserveNext() { return getData() + nextPosToWrite.fetch_add(1, std::memory_order_relaxed); }
+    void revertLast() { nextPosToWrite.fetch_sub(1, std::memory_order_relaxed); }
+
+    bool hasSpace() const {
+        return nextPosToWrite.load(std::memory_order_relaxed) <
+               maxElements.load(std::memory_order_relaxed);
+    }
+
+private:
+    T* getData() const { return reinterpret_cast<T*>(block->getData()); }
+
+private:
+    std::unique_ptr<storage::MemoryBuffer> block;
+    std::atomic<uint64_t> maxElements;
+    std::atomic<uint64_t> nextPosToWrite;
+};
+
+// Pre-allocated array of objects.
+template<typename T>
+class ObjectArray {
+public:
+    ObjectArray(const common::offset_t size, storage::MemoryManager* mm,
+        bool initializeToZero = false)
+        : allocation{mm->allocateBuffer(initializeToZero, size * sizeof(T))} {
+        data = std::span<T>(reinterpret_cast<T*>(allocation->getData()), size);
+    }
+
+    void set(const common::offset_t pos, const T value) {
+        KU_ASSERT(pos < data.size());
+        data[pos] = value;
+    }
+
+    T get(const common::offset_t pos) {
+        KU_ASSERT(pos < data.size());
+        return data[pos];
+    }
+
+private:
+    template<typename U>
+    friend class AtomicObjectArray;
+    std::span<T> data;
+    std::unique_ptr<storage::MemoryBuffer> allocation;
+};
+
+// Pre-allocated array of atomic objects.
+template<typename T>
+class AtomicObjectArray {
+public:
+    AtomicObjectArray(const common::offset_t size, storage::MemoryManager* mm,
+        bool initializeToZero = false)
+        : array{ObjectArray<std::atomic<T>>(size, mm, initializeToZero)} {}
+
+    void setRelaxed(common::offset_t pos, const T& value) {
+        KU_ASSERT(pos < array.data.size());
+        array.data[pos].store(value, std::memory_order_relaxed);
+    }
+
+    T getRelaxed(const common::offset_t pos) {
+        KU_ASSERT(pos < array.data.size());
+        return array.data[pos].load(std::memory_order_relaxed);
+    }
+
+    bool compare_exchange_strong_max(const common::offset_t src, const common::offset_t dest) {
+        auto srcValue = getRelaxed(src);
+        auto dstValue = getRelaxed(dest);
+        while (dstValue < srcValue) {
+            if (array.data[dest].compare_exchange_strong(dstValue, srcValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    ObjectArray<std::atomic<T>> array;
+};
+
+// ObjectArraysMap represents a pre-allocated amount of object per tableID.
+template<typename T>
+class GDSDenseObjectManager {
+public:
+    void allocate(common::table_id_t tableID, common::offset_t maxOffset,
+        storage::MemoryManager* mm) {
+        auto buffer = mm->allocateBuffer(false, maxOffset * sizeof(T));
+        bufferPerTable.insert({tableID, std::move(buffer)});
+    }
+
+    T* getData(common::table_id_t tableID) const {
+        KU_ASSERT(bufferPerTable.contains(tableID));
+        return reinterpret_cast<T*>(bufferPerTable.at(tableID)->getData());
+    }
+
+private:
+    common::table_id_map_t<std::unique_ptr<storage::MemoryBuffer>> bufferPerTable;
+};
+
+template<typename T>
+class GDSSpareObjectManager {
+public:
+    explicit GDSSpareObjectManager(
+        const common::table_id_map_t<common::offset_t>& nodeMaxOffsetMap) {
+        for (auto& [tableID, _] : nodeMaxOffsetMap) {
+            allocate(tableID);
+        }
+    }
+
+    void allocate(common::table_id_t tableID) {
+        KU_ASSERT(!mapPerTable.contains(tableID));
+        mapPerTable.insert({tableID, {}});
+    }
+
+    const common::table_id_map_t<std::unordered_map<common::offset_t, T>>& getData() {
+        return mapPerTable;
+    }
+
+    std::unordered_map<common::offset_t, T>* getMap(common::table_id_t tableID) {
+        KU_ASSERT(mapPerTable.contains(tableID));
+        return &mapPerTable.at(tableID);
+    }
+
+    std::unordered_map<common::offset_t, T>* getData(common::table_id_t tableID) {
+        if (!mapPerTable.contains(tableID)) {
+            mapPerTable.insert({tableID, {}});
+        }
+        KU_ASSERT(mapPerTable.contains(tableID));
+        return &mapPerTable.at(tableID);
+    }
+
+    uint64_t size() const {
+        uint64_t result = 0;
+        for (auto [_, map] : mapPerTable) {
+            result += map.size();
+        }
+        return result;
+    }
+
+private:
+    common::table_id_map_t<std::unordered_map<common::offset_t, T>> mapPerTable;
+};
+
+} // namespace function
+} // namespace kuzu
