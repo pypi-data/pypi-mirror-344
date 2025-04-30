441de6875b68f94852ff287b55bdcbeca0967f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 16:05:33 +0000

    Add package labelu/1.2.10/labelu-1.2.10.tar.gz

---
+++ packages/labelu/labelu-1.2.10.tar.gz/labelu-1.2.10/labelu/internal/statics/backend_version.js
@@ -0,0 +1,10 @@
+
+                  (function () {
+                    window.__backend = {
+  "version": "1.2.10",
+  "name": "labelu",
+  "build_time": "2025-04-30T08:13:33.250Z",
+  "commit": "bf567fccac510ef8fdca9ca9e979642b8d1e67f9"
+};
+                  })();
+                
---
+++ packages/labelu/labelu-1.2.10.tar.gz/labelu-1.2.10/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,16 @@
+(function () {
+  window.__frontend = {
+    version: '5.8.9',
+    deps: {
+      '@labelu/audio-annotator-react': '1.8.4',
+      '@labelu/components-react': '1.7.10',
+      '@labelu/formatter': '1.0.2',
+      '@labelu/i18n': '1.0.6',
+      '@labelu/image': '1.4.0',
+      '@labelu/image-annotator-react': '2.4.4',
+      '@labelu/interface': '1.3.1',
+      '@labelu/video-annotator-react': '1.4.10',
+      '@labelu/video-react': '1.5.2',
+    },
+  };
+})();
