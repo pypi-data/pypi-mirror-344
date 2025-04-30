8af16c6d646b31866050095f894f051c7eb0aa9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:30 +0000

    Add package gtec-gds/1.0.1/gtec_gds-1.0.1-cp310-cp310-win_amd64.whl

---
+++ packages/gtec-gds/gtec_gds-1.0.1-cp310-cp310-win_amd64.whl/gtec_gds/__init__.py
@@ -0,0 +1,8 @@
+# compatibility
+from __future__ import absolute_import, division, print_function
+
+# get version
+from .__version__ import __version__
+
+# allow lazy loading
+from .g_nautilus import GNautilus
