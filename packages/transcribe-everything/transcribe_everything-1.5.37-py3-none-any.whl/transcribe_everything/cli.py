0ec5b50599087ff492958733378f1e072eddde02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:06 +0000

    Add package transcribe-everything/1.5.37/transcribe_everything-1.5.37-py3-none-any.whl

---
+++ packages/transcribe-everything/transcribe_everything-1.5.37-py3-none-any.whl/transcribe_everything/cli.py
@@ -0,0 +1,39 @@
+"""
+Main entry point.
+"""
+
+import logging
+import sys
+import warnings
+
+from transcribe_everything.args import Args
+from transcribe_everything.run import run
+
+# Initialize logging for info on this package
+
+logger = logging.getLogger("transcribe_everything")
+logger.setLevel(logging.INFO)
+
+
+def main() -> int:
+    """Main entry point for the template_python_cmd package."""
+    args = Args.parse_args()
+    err_count = 0
+    while True:
+        count, err = run(args)
+        err_count += 1 if err else 0
+        if err:
+            warnings.warn(str(err))
+            err_count += 1
+        if count == 0:
+            break
+
+    return 0 if not err_count else 1
+
+
+if __name__ == "__main__":
+    src = "dst:TorrentBooks/podcast/dialogueworks01"
+    sys.argv.append(src)
+    sys.argv.append("--batch-size")
+    sys.argv.append("20")
+    sys.exit(main())
