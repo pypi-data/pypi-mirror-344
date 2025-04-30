a5a9b6250144d66d6cef37d0c3bc5c4ef4933827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:46 +0000

    Add package djpress/0.17.1/djpress-0.17.1.tar.gz

---
+++ packages/djpress/djpress-0.17.1.tar.gz/djpress-0.17.1/noxfile.py
@@ -0,0 +1,12 @@
+"""Nox file."""
+
+import nox
+
+
+@nox.session(venv_backend="uv", python=["3.10", "3.11", "3.12", "3.13"])
+@nox.parametrize("django_ver", ["~=4.2.0", "~=5.0.0", "~=5.1.0", "~=5.2.0"])
+def test(session: nox.Session, django_ver: str) -> None:
+    """Run the test suite."""
+    session.install("-e", ".", "--extra", "test", "-r", "pyproject.toml")
+    session.install(f"django{django_ver}")
+    session.run("pytest")
