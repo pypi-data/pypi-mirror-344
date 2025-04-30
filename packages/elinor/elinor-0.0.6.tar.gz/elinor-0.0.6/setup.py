9120d7bb9ef60bb51672c6bc08925afcd0a1b684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:52 +0000

    Add package elinor/0.0.6/elinor-0.0.6.tar.gz

---
+++ packages/elinor/elinor-0.0.6.tar.gz/elinor-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="elinor",
+    version="0.0.6",
+    author="Dashvvood",
+    author_email="mathismottis@gmail.com",
+    description="some util functions",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pillow",
+        "matplotlib",
+        "pybase64",
+        "pyyaml",
+        "pandas",
+        "dotenv",
+    ],
+)
