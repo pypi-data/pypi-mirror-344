b712bf7e9c41548c72fc896cae383c96a11b6ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:40 +0000

    Add package ns-parental-controls/1.0/ns_parental_controls-1.0.tar.gz

---
+++ packages/ns-parental-controls/ns_parental_controls-1.0.tar.gz/ns_parental_controls-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+packages = ['ns_parental_controls']
+
+setup(
+    name="ns_parental_controls",
+
+    version="1.0",
+
+    packages=packages,
+    install_requires=[
+        'requests',
+    ],
+
+    author="Grant miller",
+    author_email="grant@grant-miller.com",
+    description="A simple interface to enable/disable your NS",
+    long_description=long_description,
+    license="PSF",
+    keywords="grant miller ns parental controls",
+    url="https://github.com/GrantGMiller/ns_parental_controls",  # project home page, if any
+    project_urls={
+        "Source Code": "https://github.com/GrantGMiller/ns_parental_controls",
+    }
+
+)
