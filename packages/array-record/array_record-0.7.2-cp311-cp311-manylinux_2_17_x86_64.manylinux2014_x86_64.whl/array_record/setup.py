0ea41e073681a01a6f7c528916e30911607dd2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:05:13 +0000

    Add package array-record/0.7.2/array_record-0.7.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/array-record/array_record-0.7.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/array_record/setup.py
@@ -0,0 +1,49 @@
+"""Setup.py file for array_record."""
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.dist import Distribution
+
+REQUIRED_PACKAGES = [
+    'absl-py',
+    'etils[epath]',
+]
+
+BEAM_EXTRAS = [
+    'apache-beam[gcp]>=2.50.0',
+    'google-cloud-storage>=2.11.0',
+    'tensorflow>=2.14.0'
+]
+
+
+class BinaryDistribution(Distribution):
+  """This class makes 'bdist_wheel' include an ABI tag on the wheel."""
+
+  def has_ext_modules(self):
+    return True
+
+
+setup(
+    name='array_record',
+    version='0.7.2',
+    description='A file format that achieves a new frontier of IO efficiency',
+    author='ArrayRecord team',
+    author_email='no-reply@google.com',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['*.so']},
+    python_requires='>=3.10',
+    install_requires=REQUIRED_PACKAGES,
+    extras_require={
+        'beam': BEAM_EXTRAS
+    },
+    url='https://github.com/google/array_record',
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    zip_safe=False,
+    distclass=BinaryDistribution,
+)
