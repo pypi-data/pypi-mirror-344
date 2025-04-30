fe79e70f54f24dbe5bdcfe7dfb8980fd780336fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:05:00 +0000

    Add package llama-api-client/0.1.0/llama_api_client-0.1.0-py3-none-any.whl

---
+++ packages/llama-api-client/llama_api_client-0.1.0-py3-none-any.whl/llama_api_client/types/create_chat_completion_response.py
@@ -0,0 +1,23 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+
+from .._models import BaseModel
+from .completion_message import CompletionMessage
+
+__all__ = ["CreateChatCompletionResponse", "Metric"]
+
+
+class Metric(BaseModel):
+    metric: str
+
+    value: float
+
+    unit: Optional[str] = None
+
+
+class CreateChatCompletionResponse(BaseModel):
+    completion_message: CompletionMessage
+    """The complete response message"""
+
+    metrics: Optional[List[Metric]] = None
