afdc2f4e8baad24a5cbcda40739287c2bb4cf292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:29 +0000

    Add package teapotai/2.0.7/teapotai-2.0.7.tar.gz

---
+++ packages/teapotai/teapotai-2.0.7.tar.gz/teapotai-2.0.7/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='teapotai',
+    author='Zakery Clarke',
+    author_email='zakerytclarke@gmail.com',
+    description='Library for Teapot AI',
+    keywords='llm, teapot',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zakerytclarke/teapotai_lib/teapotai',
+    project_urls={
+        'Documentation': 'https://teapotai.com/docs',
+        'Bug Reports': 'https://github.com/zakerytclarke/teapot/issues',
+        'Source Code': 'https://github.com/zakerytclarke/teapot',
+    },
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    package_data={
+        'teapotai': ['teapot_refusal_classifier.joblib'],  # Make sure this points to your joblib file
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'torch',
+        'tqdm',             # for progress bars
+        'transformers',     # for huggingface transformer models
+        'numpy',            # for numerical operations
+        'scikit-learn',     # for sklearn (cosine_similarity)
+        'pydantic',         # for data validation (BaseModel)
+        'regex',            # to handle regex (re is often a wrapper for regex module in modern Python)
+        'langsmith'
+    ],
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],  # add test dependencies here
+    },
+)
