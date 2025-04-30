dc114dbef2aa2f1ce0888d34661fcdb125b3dc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:34 +0000

    Add package sysstra/0.1.3.1.2/sysstra-0.1.3.1.2-py3-none-any.whl

---
+++ packages/sysstra/sysstra-0.1.3.1.2-py3-none-any.whl/sysstra/config.py
@@ -0,0 +1,9 @@
+# api_key = None
+# orders_url = "https://api.orders.sysstra.com/"
+# data_url = "https://api.data.sysstra.com/"
+
+config = {
+    "api_key": None,
+    "orders_url": "https://api.orders.sysstra.com/",
+    "data_url": "https://api.data.sysstra.com/"
+    }
