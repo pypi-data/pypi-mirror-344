1a9ecd3eb3d499eede9289964730cdadac49a2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:28 +0000

    Add package mypri/0.1.0/mypri-0.1.0.tar.gz

---
+++ packages/mypri/mypri-0.1.0.tar.gz/mypri-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypri",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Custom print alias 'pri'",
+    author="Bladesl1W",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    install_requires=[],
+)
