620db66f6ba909ad39d580cbc16c314b1279bf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 15:06:13 +0000

    Add package frametree/0.14.2/frametree-0.14.2-py3-none-any.whl

---
+++ packages/frametree/frametree-0.14.2-py3-none-any.whl/frametree/core/tests/test_data_column.py
@@ -0,0 +1,28 @@
+from operator import mul
+from functools import reduce
+from fileformats.core import FileSet
+from frametree.core.frameset.base import FrameSet
+
+
+def test_column_api_access(dataset: FrameSet):
+
+    bp = dataset.__annotations__["blueprint"]
+
+    for fileset_bp in bp.entries:
+
+        dataset.add_source(fileset_bp.path, fileset_bp.datatype)
+
+        col = dataset[fileset_bp.path]
+
+        # Check length of column
+        assert len(col) == reduce(mul, bp.dim_lengths)
+
+        # Access file-set via leaf IDs
+        with dataset.tree:
+            for id_ in col.ids:
+                item = col[id_]
+                assert isinstance(item, fileset_bp.datatype)
+                if issubclass(fileset_bp.datatype, FileSet):
+                    assert sorted(p.name for p in item.fspaths) == sorted(
+                        fileset_bp.filenames
+                    )
