c280b12d1af2db9cc4ce0b2e9e5559ca89f18f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 16:05:33 +0000

    Add package ckanext-blocksmith/0.2.2/ckanext_blocksmith-0.2.2.tar.gz

---
+++ packages/ckanext-blocksmith/ckanext_blocksmith-0.2.2.tar.gz/ckanext_blocksmith-0.2.2/ckanext/blocksmith/logic/auth.py
@@ -0,0 +1,39 @@
+import ckan.plugins.toolkit as tk
+from ckan.types import AuthResult, Context, DataDict
+
+import ckanext.blocksmith.model as model
+
+
+def blocksmith_create_page(context: Context, data_dict: DataDict) -> AuthResult:
+    return {"success": False}
+
+
+def blocksmith_edit_page(context: Context, data_dict: DataDict) -> AuthResult:
+    return {"success": False}
+
+
+def blocksmith_update_page(context: Context, data_dict: DataDict) -> AuthResult:
+    return {"success": False}
+
+
+def blocksmith_list_pages(context: Context, data_dict: DataDict) -> AuthResult:
+    return {"success": False}
+
+
+def blocksmith_delete_page(context: Context, data_dict: DataDict) -> AuthResult:
+    return {"success": False}
+
+
+@tk.auth_allow_anonymous_access
+def blocksmith_get_page(context: Context, data_dict: DataDict) -> AuthResult:
+    page_id = tk.get_or_bust(data_dict, "id")
+
+    page = model.PageModel.get_by_id(page_id)
+
+    if page and page.published:
+        return {"success": True}
+
+    if not tk.current_user.is_anonymous and tk.current_user.sysadmin:
+        return {"success": True}
+
+    return {"success": False}
