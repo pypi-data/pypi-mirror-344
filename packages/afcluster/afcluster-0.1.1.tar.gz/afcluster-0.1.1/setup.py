669a4599623c15376294ff8b126d49cd65a96a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:05:08 +0000

    Add package afcluster/0.1.1/afcluster-0.1.1.tar.gz

---
+++ packages/afcluster/afcluster-0.1.1.tar.gz/afcluster-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="afcluster",
+    version="0.1.1",
+    description="Cluster Multiple-Sequence Alignments (MSA) using DBSCAN",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Noah Kleinschmidt",
+    author_email="noah.kleinschmidt@unibe.ch",
+    url="https://github.com/NoahHenrikKleinschmidt/AFCluster-api",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "polyleven",
+        "scipy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
