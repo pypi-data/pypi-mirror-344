a6dba7e8b956fbbe73df6d41d2b6288a3e972a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:46 +0000

    Add package RonAOVTool/0.1.4/ronaovtool-0.1.4.tar.gz

---
+++ packages/RonAOVTool/ronaovtool-0.1.4.tar.gz/ronaovtool-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RonAOVTool',
+    version='0.1.4',
+    packages=find_packages(),
+    description='Một thư viện giúp chuyển đổi chuỗi sang hashcode hoặc thay đường dẫn của AOV',
+    long_description=open('README.md', encoding='utf-8').read(),  # <-- thêm dòng này
+    long_description_content_type='text/markdown',                # <-- và dòng này
+    author='Ron AOV',
+    author_email='ronlatoiday@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
