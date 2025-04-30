ebb97044ed96b418abefdbf8f2039cfa6b2fc84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:03 +0000

    Add package composio-client/0.1.0a7/composio_client-0.1.0a7.tar.gz

---
+++ packages/composio-client/composio_client-0.1.0a7.tar.gz/composio_client-0.1.0a7/src/composio_client/types/connected_account_create_response.py
@@ -0,0 +1,31 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from pydantic import Field as FieldInfo
+
+from .._models import BaseModel
+
+__all__ = ["ConnectedAccountCreateResponse", "Deprecated"]
+
+
+class Deprecated(BaseModel):
+    auth_config_uuid: str = FieldInfo(alias="authConfigUuid")
+    """The uuid of the auth config"""
+
+    uuid: str
+    """The uuid of the connected account"""
+
+
+class ConnectedAccountCreateResponse(BaseModel):
+    id: str
+    """The id of the connected account"""
+
+    deprecated: Deprecated
+
+    redirect_uri: Optional[str] = None
+    """The redirect uri of the app"""
+
+    status: Literal["ACTIVE", "INACTIVE", "DELETED", "INITIATED", "EXPIRED", "FAILED"]
+    """The status of the connected account"""
