ececb0f257424f1a74e2d470e75fc5a848a5e426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:27 +0000

    Add package appium-python-client-shadowstep/0.16.43/appium_python_client_shadowstep-0.16.43-py3-none-any.whl

---
+++ packages/appium-python-client-shadowstep/appium_python_client_shadowstep-0.16.43-py3-none-any.whl/shadowstep/element/should.py
@@ -0,0 +1,56 @@
+
+"""
+have:
+    attr
+    id
+    resource-id
+    index
+    package
+    class
+    text
+    bounds
+
+be:
+    checked
+    checkable
+    enabled
+    focusable
+    focused
+    long_clickable
+    password
+    scrollable
+    selected
+    displayed
+"""
+
+"""
+######
+https://github.com/appium/appium-uiautomator2-driver
+Element Attributes
+
+checkable
+checked
+class or className
+clickable
+content-desc or contentDescription
+enabled
+focusable
+focused
+long-clickable or longClickable
+package
+password
+resource-id or resourceId
+scrollable
+selection-start
+selection-end
+selected
+text or name
+hint
+bounds
+displayed
+contentSize
+extras
+"""
+
+
+
