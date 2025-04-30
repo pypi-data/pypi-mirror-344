2ccead74de2399f623ed16500e9d981d63448225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:33 +0000

    Add package testmyrenovatestrong/0.1.0/testmyrenovatestrong-0.1.0.tar.gz

---
+++ packages/testmyrenovatestrong/testmyrenovatestrong-0.1.0.tar.gz/testmyrenovatestrong-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testmyrenovatestrong",
+    version="0.1.0",
+    author="MichaelScott",
+    author_email="vokow66306@asaption.com",
+    description="A simple math operations package",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
