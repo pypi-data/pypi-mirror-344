89e60a07c5846d1187f5572f8d861fee926148bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 17:05:38 +0000

    Add package fwdi/0.0.92/fwdi-0.0.92.tar.gz

---
+++ packages/fwdi/fwdi-0.0.92.tar.gz/fwdi-0.0.92/fwdi/Utilites/ext_deserializable_generic.py
@@ -0,0 +1,18 @@
+import json
+from typing import TypeVar
+from pydantic import BaseModel
+
+T = TypeVar('T', bound='BaseModel')
+
+
+class ExtDeserializableGeneric():
+
+    @staticmethod
+    def json_to_obj(json_object:str, obj_type:type[T])->T:
+        try:
+            json_obj = json.loads(str(json_object, encoding='utf-8').replace( "'", '"'))
+            serializable_data = obj_type(**json_obj)
+
+            return serializable_data
+        except Exception as ex:
+            print(f"Error ExtDeserializableGeneric.json_to_obj converter: {ex}")
