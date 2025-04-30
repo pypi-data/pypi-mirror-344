7fabcd6e1dd2a1d79cfecdf99a61a3d3f85bf00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 16:06:39 +0000

    Add package hike/0.12.0/hike-0.12.0-py3-none-any.whl

---
+++ packages/hike/hike-0.12.0-py3-none-any.whl/hike/messages/history.py
@@ -0,0 +1,26 @@
+"""Messages to do with manipulating history."""
+
+##############################################################################
+# Python imports.
+from dataclasses import dataclass
+
+##############################################################################
+# Textual imports.
+from textual.message import Message
+
+
+##############################################################################
+@dataclass
+class RemoveHistoryEntry(Message):
+    """Remove an item from history."""
+
+    location: int
+    """The location in the history to remove."""
+
+
+##############################################################################
+class ClearHistory(Message):
+    """Clear all items from history."""
+
+
+### history.py ends here
