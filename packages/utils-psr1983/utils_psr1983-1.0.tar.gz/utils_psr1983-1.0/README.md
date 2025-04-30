81909dcc9bde1d7fe0e0f0c1b597760624a541da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:05 +0000

    Add package utils-psr1983/1.0/utils_psr1983-1.0.tar.gz

---
+++ packages/utils-psr1983/utils_psr1983-1.0.tar.gz/utils_psr1983-1.0/README.md
@@ -0,0 +1,17 @@
+
+# Utils
+
+- Contains the following utils
+  - Git-Utils
+  - Cloud-Utils
+
+## Git-Utils
+
+- Supports the following operations
+  - Download File
+  - Upload file
+
+## Cloud-Utils
+
+- Supports the following operations
+- Listing Cloud Providers
---
+++ packages/utils-psr1983/utils_psr1983-1.0.tar.gz/utils_psr1983-1.0/src/utils_psr1983/git_utils.py
@@ -0,0 +1,6 @@
+def download_file(repo_name, path, file_name):
+    print(f"Downloaded the file {file_name} from the repo {repo_name} and from the path {path}")
+
+
+def upload_file(repo_name, path, file_name):
+    print(f"Uploaded the file {file_name} to the repo {repo_name} and onto the path {path}")
