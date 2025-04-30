f16f550d515f5221794c0e09d3d88e115a4fc5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 08:04:55 +0000

    Add package laboneq/2.51.0/laboneq-2.51.0-cp39-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl

---
+++ packages/laboneq/laboneq-2.51.0-cp39-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/laboneq/compiler/seqc/passes/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from .collect_measurement_info import collect_measurement_info
+from .fanout_awg import fanout_awgs
+from .section_inliner import inline_sections_in_branch, inline_sections
+from .oscillator_parameters import calculate_oscillator_parameters
+from .intervals import collect_empty_intervals
+from .pulse_parameters import PulseParams, detach_pulse_params
+
+__all__ = [
+    "fanout_awgs",
+    "inline_sections",
+    "inline_sections_in_branch",
+    "section_inliner",
+    "calculate_oscillator_parameters",
+    "collect_empty_intervals",
+    "collect_measurement_info",
+    "detach_pulse_params",
+    "PulseParams",
+]
---
+++ packages/laboneq/laboneq-2.51.0-cp39-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/laboneq/controller/devices/awg_pipeliner.py
@@ -0,0 +1,76 @@
+# Copyright 2023 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from typing import Any
+
+from laboneq.controller.devices.device_utils import NodeCollector
+
+
+class AwgPipeliner:
+    def __init__(self, node_base: str, unit: str):
+        self._node_base = node_base
+        self._unit = unit
+        self._pipeliner_slot_tracker = 0
+
+    def prepare_for_upload(self) -> NodeCollector:
+        self._pipeliner_slot_tracker = 0
+        nc = NodeCollector(base=f"{self._node_base}/")
+        nc.add("pipeliner/mode", 1)
+        nc.add("pipeliner/reset", 1, cache=False)
+        nc.add("synchronization/enable", 1)
+        nc.barrier()
+        return nc
+
+    def commit(self) -> NodeCollector:
+        self._pipeliner_slot_tracker += 1
+        nc = NodeCollector(base=f"{self._node_base}/")
+        nc.barrier()
+        nc.add("pipeliner/commit", 1, cache=False)
+        return nc
+
+    def ready_conditions(self) -> dict[str, Any]:
+        max_slots = 1024  # TODO(2K): read on connect from pipeliner/maxslots
+        avail_slots = max_slots - self._pipeliner_slot_tracker
+        return {f"{self._node_base}/pipeliner/availableslots": avail_slots}
+
+    def collect_execution_nodes(self) -> NodeCollector:
+        nc = NodeCollector(base=f"{self._node_base}/")
+        nc.add("pipeliner/enable", 1, cache=False)
+        return nc
+
+    def conditions_for_execution_ready(
+        self, is_standalone: bool = False
+    ) -> dict[str, tuple[Any, str]]:
+        if is_standalone:
+            return {
+                f"{self._node_base}/pipeliner/status": (
+                    [1, 0],  # exec -> idle
+                    f"Pipeliner for {self._unit} failed to transition to exec and back to stop.",
+                )
+            }
+        return {
+            f"{self._node_base}/pipeliner/status": (
+                1,  # exec
+                f"Pipeliner for {self._unit} didn't start.",
+            )
+        }
+
+    def conditions_for_execution_done(
+        self, is_standalone: bool = False
+    ) -> dict[str, tuple[Any, str]]:
+        if is_standalone:
+            return {}
+        return {
+            f"{self._node_base}/pipeliner/status": (
+                0,  # idle
+                f"Pipeliner for {self._unit} didn't stop. Missing start trigger? Check HW synchronization participants.",
+            )
+        }
+
+    def reset_nodes(self) -> NodeCollector:
+        nc = NodeCollector(base=f"{self._node_base}/")
+        nc.add("pipeliner/mode", 0, cache=False)  # off
+        nc.add("synchronization/enable", 0, cache=False)
+        return nc
