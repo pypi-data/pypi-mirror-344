b1be4d665ac280a1a3c9ceae59c4d95acebc437e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:29 +0000

    Add package scikit-plots/0.4.0rc4/scikit_plots-0.4.0rc4-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/scikit-plots/scikit_plots-0.4.0rc4-cp311-cp311-musllinux_1_2_aarch64.whl/scikitplot/_utils/sp_export.py
@@ -0,0 +1,15 @@
+SKPLT_API_NAME = "scikitplot"
+
+# class ExportType(Protocol):
+
+#   def __call__(
+#       self,
+#       *v2: str,
+#       v1: Optional[Sequence[str]] = None,
+#   ) -> api_export:
+#     ...
+
+
+# sp_export: ExportType = functools.partial(
+#     api_export, api_name=SKPLT_API_NAME
+# )
