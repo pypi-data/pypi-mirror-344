653b9212a152c5f0ee82d3fb9741522cb3526146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 06:04:43 +0000

    Add package django-admin-collaborator/0.4.0/django_admin_collaborator-0.4.0.tar.gz

---
+++ packages/django-admin-collaborator/django_admin_collaborator-0.4.0.tar.gz/django_admin_collaborator-0.4.0/django_admin_collaborator/defaults.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+
+
+DEFAULT_ADMIN_COLLABORATOR_OPTIONS = {
+    "editor_mode_text": "You are in editor mode.",
+    "viewer_mode_text": "This page is being edited by {editor_name}. You cannot make changes until they leave.",
+    "claiming_editor_text": "The editor has left. The page will refresh shortly to allow editing.",
+    "avatar_field": None,
+    "notification_request_interval": 15,
+    "notification_message": "User {username} is requesting the editors attention.",
+    'notification_button_text': 'Request Editor Attention',
+    "notification_request_sent_text": "Request sent.",
+    # Chat settings
+    "enable_chat": True,  # Enable/disable the chat feature
+    "chat_user_list_title": "Online Users",  # Title for the user list panel
+    "chat_empty_state_text": "No other users online",  # Text when no users are online
+    "chat_start_conversation_text": "No messages yet. Start the conversation!",  # Text for empty chat
+    "chat_input_placeholder": "Type a message...",  # Placeholder text for chat input field
+    "chat_online_status_text": "Online",  # Text for online status indicator
+}
+ADMIN_COLLABORATOR_ADMIN_URL = "admin"
+ADMIN_COLLABORATOR_WEBSOCKET_CONNECTION_PREFIX_URL = "admin/collaboration"
+
+def get_admin_collaborator_websocket_connection_prefix_url():
+    if hasattr(settings, "ADMIN_COLLABORATOR_WEBSOCKET_CONNECTION_PREFIX_URL"):
+        return settings.ADMIN_COLLABORATOR_WEBSOCKET_CONNECTION_PREFIX_URL
+    return ADMIN_COLLABORATOR_WEBSOCKET_CONNECTION_PREFIX_URL
