1d20592d0d10fa41cf7d5653bc5a642da9d4cc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 08:05:03 +0000

    Add package comps-sif-constructor/0.0.11/comps_sif_constructor-0.0.11-py3-none-any.whl

---
+++ packages/comps-sif-constructor/comps_sif_constructor-0.0.11-py3-none-any.whl/comps_sif_constructor/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ["__version__", "__versiondate__", "__license__"]
+
+__version__ = "0.0.11"
+__versiondate__ = '2025-04-29'
+__license__ = "Apache-2.0"
