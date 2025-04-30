0297df82b7d532adc3a1c7457e99e2501627a740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:54 +0000

    Add package game-bot/0.1.2/game_bot-0.1.2.tar.gz

---
+++ packages/game-bot/game_bot-0.1.2.tar.gz/game_bot-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='game_bot',
+    version='0.1.2',
+    packages=find_packages(where='src/python'),
+    package_dir={'': 'src/python'},
+    install_requires=[
+        'pywin32>=305',
+        'opencv-python>=4.8.0',
+        'pytesseract>=0.3.10',
+        'numpy>=1.24.0',
+        'mss>=10.0.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'picker=game_bot.picker:main'
+        ]
+    },
+    python_requires='>=3.6',
+    classifiers=[
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+    ],
+    platforms=['Windows'],
+)
