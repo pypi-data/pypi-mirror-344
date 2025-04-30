eed403273e3b125ef492b70050c8b432be125c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:33 +0000

    Add package kitconc/3.0.5/kitconc-3.0.5.tar.gz

---
+++ packages/kitconc/kitconc-3.0.5.tar.gz/kitconc-3.0.5/README.txt
@@ -0,0 +1,55 @@
+**Kitconc**
+===========
+
+Kitconc is a package for Corpus Linguistics and text analysis with Python. 
+It contains, among other things, tools for creating:
+
+* Corpora;
+* Frequency wordlists;
+* Keywords;
+* Concordance lines;
+* Collocates; 
+* N-gram lists;
+* Dispersion plots;
+* Excel data files. 
+
+The package is built on top of platforms and packages for scientific research: numpy, pandas, NLTK,  XlsxWrite and matplotlib. 
+All in Anaconda Platform.
+
+Requirements
+============
+
+Kitconc requires a Python 3.6 (or later) installation along with:
+
+numpy>=1.24.0,<2.0.0
+pandas>=2.2.0,<3.0.0
+matplotlib>=3.7.0,<4.0.0
+xlsxwriter>=3.2.3,<4.0.0
+ttkbootstrap>=1.12.0,<2.0.0
+pillow>=11.2.0,<12.0.0
+requests>=2.31.0,<3.0.0
+nltk>=3.9.1,<4.0.0
+chardet>=5.2.0,<6.0.0
+
+It is suggested that users install Anaconda Platform as an easy option. 
+
+Installation
+============
+(Make sure you have Python 3.6 (or later) and the required packages.)
+
+pip install kitconc
+
+
+Language resources
+==================
+
+Kitconc comes with some language resources for portuguese and english corpora.
+It also has functions for adding your own language resources.
+
+Usage example
+=============
+
+See how easy it is to use Kitconc:
+
+`https://ilexis.net.br/kitconc <https://ilexis.net.br/kitconc>`_
+
---
+++ packages/kitconc/kitconc-3.0.5.tar.gz/kitconc-3.0.5/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Author: jlopes@alumni.usp.br
+
+import sys
+import platform
+from setuptools import setup, find_packages
+import textwrap
+import pkg_resources
+
+def is_installed(requirement):
+    try:
+        pkg_resources.require(requirement)
+    except pkg_resources.ResolutionError:
+        return False
+    else:
+        return True
+
+if not is_installed('numpy>=1.14.0'):
+    print(textwrap.dedent("""
+        Error: numpy needs to be installed first. You can install it via:
+
+        $ pip install numpy
+    """), file=sys.stderr)
+    sys.exit(1)
+
+install_req = [
+    "numpy>=1.24.0,<2.0.0",
+    "pandas>=2.2.0,<3.0.0",
+    "matplotlib>=3.7.0,<4.0.0",
+    "xlsxwriter>=3.2.3,<4.0.0",
+    "ttkbootstrap>=1.12.0,<2.0.0",
+    "pillow>=11.2.0,<12.0.0",
+    "requests>=2.31.0,<3.0.0",
+    "nltk>=3.9.1,<4.0.0",
+    "chardet>=5.2.0,<6.0.0",
+    "setuptools>=65.5.0,<70.0.0"
+]
+
+
+
+setup(
+    name='kitconc',
+    version='3.0.5',
+    author='José Lopes Moreira Filho',
+    author_email='jlopes@alumni.usp.br',
+    packages=find_packages(),
+    package_data={
+        'kitconc': ['data/*.*', 'data/images/*.*', 'data/scripts/*.*']
+    },
+    url='http://pypi.python.org/pypi/Kitconc/',
+    license='MIT',
+    license_files=['LICENSE.txt'],
+    description='A toolkit for Corpus Linguistics Analysis',
+    long_description=open('README.txt', encoding='utf-8').read(),
+    long_description_content_type='text/x-rst',
+    install_requires=install_req,
+)
