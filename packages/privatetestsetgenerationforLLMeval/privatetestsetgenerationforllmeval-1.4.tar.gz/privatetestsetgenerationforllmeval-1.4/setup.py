e2e974e545fe802e387f2a20de0c0147fcb3dbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 15:05:50 +0000

    Add package privatetestsetgenerationforLLMeval/1.4/privatetestsetgenerationforllmeval-1.4.tar.gz

---
+++ packages/privatetestsetgenerationforLLMeval/privatetestsetgenerationforllmeval-1.4.tar.gz/privatetestsetgenerationforllmeval-1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name='privatetestsetgenerationforLLMeval',
+    version='1.4',
+    author='Ilias',
+    author_email='ilias.driouich@amadeus.com',
+    description='A package for generating evaluation set for LLM-based chatbots in a diverse and private manner',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AmadeusITGroup/privatetestsetgenerationforLLMeval',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pandas>=1.1.0',
+        'numpy>=1.19.0',
+    ],
+    tests_require=[
+        'pytest>=6.0.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'privatetestsetgeneration=run.run:main',
+    ],
+    },
+    python_requires='>=3.6',
+)
