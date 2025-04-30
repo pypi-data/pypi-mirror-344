ddf4c93f7524b56e4993f91615a277152f0b72b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:46 +0000

    Add package storylinez/0.1.0b1/storylinez-0.1.0b1.tar.gz

---
+++ packages/storylinez/storylinez-0.1.0b1.tar.gz/storylinez-0.1.0b1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Get the long description from the README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name='storylinez',
+    version='0.1.0b1',  # Changed to beta version
+    license="MIT",
+    author='Sayanti Chatterjee, Ranit Bhowmick',
+    author_email='sayantichatterjee28@gmail.com, bhowmickranitking@gmail.com',
+    description='Storylinez: A modular library for narrative generation and story manipulation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages('src', include=["storylinez", "storylinez.*"]),
+    package_dir={'': 'src'},
+    url='https://github.com/Kawai-Senpai/Storylinez-SDK',
+    install_requires=[
+    ],
+    python_requires='>=3.6',
+)
