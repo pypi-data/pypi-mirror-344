66c154bf863e0f41c48b16bb1a7e7b5d134bd14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:41 +0000

    Add package ros-hirac-camera-topic/1.0.2/ros_hirac_camera_topic-1.0.2-py3-none-any.whl

---
+++ packages/ros-hirac-camera-topic/ros_hirac_camera_topic-1.0.2-py3-none-any.whl/ros_hirac_camera_topic/script.py
@@ -0,0 +1,74 @@
+import subprocess
+import logging
+import logging.config
+import time
+
+def get_image_topic():
+    while True:
+        result = subprocess.run(
+            ["ros2", "topic", "list"], stdout=subprocess.PIPE, text=True
+        )
+        topics = result.stdout.splitlines()
+
+        for topic_name in topics:
+            if "/camera/color/image_raw" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            elif "/right/image_rect" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            elif "/oak/rgb/image_raw" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+        
+        logging.info("No matching topic found. Retrying in 1 second...")
+        time.sleep(1)
+
+def get_pointcloud_topic():
+    while True: 
+        result = subprocess.run(
+            ["ros2", "topic", "list"], stdout=subprocess.PIPE, text=True
+        )
+        topics = result.stdout.splitlines()
+
+        for topic_name in topics:
+
+            if "/camera/camera/depth/color/points" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            elif "/stereo/points" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            elif "/oak/points" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            
+        logging.info("No matching pointcloud topic found. Retrying in 1 second...")
+        time.sleep(1)
+
+def get_camera_info_topic():
+    while True:
+        result = subprocess.run(
+            ["ros2", "topic", "list"], stdout=subprocess.PIPE, text=True
+        )
+        topics = result.stdout.splitlines()
+
+        for topic_name in topics: 
+
+            if "/camera/depth/camera_info" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            elif "/stereo/camera_info" in topic_name:
+                logging.info("Subscribed Topic is: ")
+                logging.info(topic_name)
+                return topic_name
+            
+        logging.info("No matching camera info topic found. Retrying in 1 second...")
+        time.sleep(1)
