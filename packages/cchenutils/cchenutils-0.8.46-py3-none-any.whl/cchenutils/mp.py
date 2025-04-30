bb48a70bceb05064861b5f6a5093c5e7e14bfb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 15:06:15 +0000

    Add package cchenutils/0.8.46/cchenutils-0.8.46-py3-none-any.whl

---
+++ packages/cchenutils/cchenutils-0.8.46-py3-none-any.whl/cchenutils/mp.py
@@ -0,0 +1,53 @@
+import random
+import time
+import traceback
+
+from requests.exceptions import ProxyError, ConnectTimeout, ReadTimeout, Timeout
+
+from .files import write
+
+
+def writer(queue):
+    """
+    Listens to `queue` for file writing tasks. Expects a tuple:
+
+    - For CSV:
+        (fp, data, headers, [optional scrape_time])
+    - For JSON/JSONL:
+        (fp, data)
+
+    Use 'STOP' to end the loop.
+    """
+    while True:
+        args = queue.get()
+        if args == 'STOP':
+            break
+        if len(args) < 2:
+            print(f'[writer] Invalid input: {args}')
+            continue
+        write(*args)
+
+
+def scraper(func, *func_args, proxy_list=None, **kwargs):
+    retries = kwargs.pop('retries', 3)
+    debug = kwargs.pop('debug', None)
+    stderr = kwargs.pop('stderr', None)
+    if proxy_list is None:
+        retries = min([1, retries])
+    for _ in range(retries):
+        proxy = random.choice(proxy_list) if proxy_list else None
+        try:
+            return func(*func_args, **kwargs, proxy=proxy)
+        except (ProxyError, ConnectTimeout, ReadTimeout, Timeout):
+            try:
+                proxy_list.remove(proxy)
+                if stderr is not None:
+                    write(*stderr)
+            except TypeError:
+                pass
+        except Exception as e:
+            print(proxy, *func_args)
+            if debug:
+                print(traceback.print_exc())
+                time.sleep(300)
+    return None
