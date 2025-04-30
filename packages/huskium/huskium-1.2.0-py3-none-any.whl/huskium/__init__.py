dbfe62ebfa699d6fea88779a6c67e121207fe55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 15:06:16 +0000

    Add package huskium/1.2.0/huskium-1.2.0-py3-none-any.whl

---
+++ packages/huskium/huskium-1.2.0-py3-none-any.whl/huskium/__init__.py
@@ -0,0 +1,12 @@
+# Author: Johnny Chou
+# Email: johnny071531@gmail.com
+# PyPI: https://pypi.org/project/huskium/
+# GitHub: https://github.com/uujohnnyuu/huskium
+
+
+__version__ = "1.2.0"
+
+
+from .logging import LogConfig, PrefixFilter, FuncPrefixFilter, FilePrefixFilter
+from .common import Area, Offset
+from .dynamic import dynamic
---
+++ packages/huskium/huskium-1.2.0-py3-none-any.whl/huskium/appium/__init__.py
@@ -0,0 +1,14 @@
+# Author: Johnny Chou
+# Email: johnny071531@gmail.com
+# PyPI: https://pypi.org/project/huskium/
+# GitHub: https://github.com/uujohnnyuu/huskium
+
+
+from appium.webdriver.webdriver import WebDriver
+from appium.webdriver.webelement import WebElement
+
+from .by import By
+from .ecex import ECEX
+from .element import Element
+from .elements import Elements
+from .page import Page
