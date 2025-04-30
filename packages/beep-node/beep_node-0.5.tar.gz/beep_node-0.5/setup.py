929c5049e9258f9a66a8eeb5aa0111484425146f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:04:43 +0000

    Add package beep-node/0.5/beep_node-0.5.tar.gz

---
+++ packages/beep-node/beep_node-0.5.tar.gz/beep_node-0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='beep_node',
+    version='0.5',
+    author='John Doe',
+    description='Library of converted notebooks',
+    packages=find_packages(),
+    install_requires=[],
+)
