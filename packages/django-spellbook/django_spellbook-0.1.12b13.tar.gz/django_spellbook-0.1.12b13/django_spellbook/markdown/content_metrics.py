2f71c4e52bab5ddafd0875e453397b8cb86b62b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:05:34 +0000

    Add package django-spellbook/0.1.12b13/django_spellbook-0.1.12b13.tar.gz

---
+++ packages/django-spellbook/django_spellbook-0.1.12b13.tar.gz/django_spellbook-0.1.12b13/django_spellbook/markdown/content_metrics.py
@@ -0,0 +1,10 @@
+# django_spellbook/markdown/content_metrics.py
+
+def get_word_count(content: str) -> int:
+    """Get word count of content"""
+    return len(content.split())
+
+
+def get_reading_time_minutes(content: str) -> int:
+    """Get reading time in minutes of content"""
+    return len(content.split()) / 215
