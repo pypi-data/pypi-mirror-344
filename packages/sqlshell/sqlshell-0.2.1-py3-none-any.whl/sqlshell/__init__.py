5eb3ac4a81c97051f8ed75b1ae1a0ae1b3dbae9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 07:05:35 +0000

    Add package sqlshell/0.2.1/sqlshell-0.2.1-py3-none-any.whl

---
+++ packages/sqlshell/sqlshell-0.2.1-py3-none-any.whl/sqlshell/__init__.py
@@ -0,0 +1,10 @@
+"""
+SQLShell - A powerful SQL shell with GUI interface for data analysis
+"""
+
+__version__ = "0.2.1"
+__author__ = "SQLShell Team"
+
+from sqlshell.main import main as start
+
+# SQLShell package initialization 
