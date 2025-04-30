7316d9e37c4bd1ae0373d2d57d141d31eecdbf3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:31 +0000

    Add package jbqt/0.1.1/jbqt-0.1.1.tar.gz

---
+++ packages/jbqt/jbqt-0.1.1.tar.gz/jbqt-0.1.1/jbqt/common/__init__.py
@@ -0,0 +1,6 @@
+"""Const exports"""
+
+from jbqt.common.consts import COLORS, STYLES, SIZES, GlobalRefs, ICONS
+from jbqt.common.qt_utils import register_app
+
+__all__ = ["COLORS", "STYLES", "SIZES", "GlobalRefs", "ICONS", "register_app"]
