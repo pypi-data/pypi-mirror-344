067b983369cfcb361032c38658bf1a50c6d9f81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:36 +0000

    Add package doctr-labeler/0.2.0/doctr_labeler-0.2.0.tar.gz

---
+++ packages/doctr-labeler/doctr_labeler-0.2.0.tar.gz/doctr_labeler-0.2.0/README.md
@@ -0,0 +1,145 @@
+<p align="center">
+  <img src="https://github.com/text2knowledge/docTR-Labeler/raw/main/docs/images/logo.png" width="40%">
+</p>
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](LICENSE)
+![Build Status](https://github.com/text2knowledge/docTR-Labeler/workflows/builds/badge.svg)
+[![codecov](https://codecov.io/gh/text2knowledge/docTR-Labeler/graph/badge.svg?token=vrRnHbweMg)](https://codecov.io/gh/text2knowledge/docTR-Labeler)
+[![CodeFactor](https://www.codefactor.io/repository/github/text2knowledge/doctr-labeler/badge)](https://www.codefactor.io/repository/github/text2knowledge/doctr-labeler)
+[![Pypi](https://img.shields.io/badge/pypi-v0.2.0-blue.svg)](https://pypi.org/project/docTR-Labeler/)
+
+docTR Labeler is a tool to label OCR data for the [docTR](https://github.com/mindee/doctr) and [OnnxTR](https://github.com/felixdittrich92/OnnxTR) projects.
+
+**Attention**: This project is still in development - and currently a pre-release version - please report any issues you encounter.
+
+What you can expect from this repository:
+
+- Efficient way to label OCR data
+- Features like auto-annotation using [OnnxTR](https://github.com/felixdittrich92/OnnxTR) and auto polygon adjustment
+- Easy to use frontend with keybindings
+- CLI and programmatic usage
+- No Login required
+
+<p align="center">
+  <img src="https://github.com/text2knowledge/docTR-Labeler/raw/main/docs/images/ui_screen.png" width="90%">
+</p>
+
+## Installation
+
+### Prerequisites
+
+Python 3.10 (or higher) and [pip](https://pip.pypa.io/en/stable/) are required to install docTR-Labeler.
+
+### Latest release
+
+You can then install the latest release of the package using [pypi](https://pypi.org/project/OnnxTR/) as follows:
+
+```bash
+pip3 install doctr-labeler
+```
+
+## Keybindings
+
+- `Ctrl + a` : Select all polygons
+- `Esc` : Deselect all selected polygons
+- `Ctrl + t` : Auto adjust the selected polygons
+- `Ctrl + r` : Reset last auto adjustment
+- `Ctrl + s` : Save the current progress / image annotation
+- `Ctrl + d` : Delete the selected polygon
+- `Ctrl + f` : Draw a new polygon
+- `Ctrl + c` : Undo while drawing a polygon
+
+- `Ctrl + +` : Zoom in (up to 150% by default) - Can be changed by setting a environment variable `DOCTR_LABELER_MAX_ZOOM` to a value between 1.1 and 2.0
+- `Ctrl + -` : Zoom out (down to 50% by default) - Can be changed by setting a environment variable `DOCTR_LABELER_MIN_ZOOM` to a value between 0.1 and 0.9
+
+## Configuration
+
+You can set the following environment variables to configure the tool:
+
+- `DOCTR_LABELER_MAX_ZOOM` : Maximum zoom level (default: 1.5)
+- `DOCTR_LABELER_MIN_ZOOM` : Minimum zoom level (default: 0.5)
+- `RECOGNITION_ARCH` : The recognition architecture to use (default: `Felix92/onnxtr-parseq-multilingual-v1`)
+- `DETECTION_ARCH` : The detector architecture to use (default: `fast_base`)
+- `OBJECTNESS_THRESHOLD` : The objectness threshold for the detector (default: 0.5)
+
+## Usage CLI
+
+After installation you can use the CLI to start the tool:
+
+For this open a terminal and run:
+
+```bash
+doctr-labeler
+```
+
+## Usage Programmatic
+
+You can also use the tool programmatic:
+
+```python
+from labeler.views import GUI
+from labeler.utils import prepare_data_folder, hf_upload_dataset
+
+# (Optional)
+# Prepare the data folder you can pass a path to a folder containing images and PDFs
+# The function will create a new folder 'images' with the prepared data
+prepared_data_path = prepare_data_folder("path/to/folder")
+
+# Start the GUI
+gui = GUI(image_folder=prepared_data_path)
+gui.start_gui()
+
+# or if you want to annotate also for KIE
+types = ["Total", "Date", "Invoice Number", "VAT Number", "Address", "Company Name"]
+gui = GUI(image_folder=prepared_data_path, text_types=types)
+gui.start_gui()
+
+# (Optional) Upload the prepared data to the Hugging Face dataset hub
+# The path to the folder should contain an 'images' folder and it's corresponding 'labels.json' file or the 'tmp
+hf_upload_dataset(prepared_data_path)
+```
+
+## Credits
+
+- This project is based on the [Form-Labeller](https://github.com/devarshi16/Form-Labeller) project by Devarshi Aggarwal.
+
+## Citation
+
+If you wish to cite please refer to the base project citation, feel free to use this [BibTeX](http://www.bibtex.org/) references:
+
+```bibtex
+@misc{docTR-Labeler,
+    title={docTR Labeler: docTR OCR Annotation Tool},
+    author={{Dittrich, Felix}, {List, Ian}},
+    year={2024},
+    publisher = {GitHub},
+    howpublished = {\url{https://github.com/text2knowledge/docTR-Labeler}}
+}
+```
+
+```bibtex
+@misc{Form-Labeller,
+  author = {Aggarwal, Devarshi},
+  title = {{Form Labeller}},
+  howpublished = {\url{https://github.com/devarshi16/Form-Labeller}},
+  year = {2020},
+  note = {Online; accessed 01-March-2020}
+}
+```
+
+## Contributing
+
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create.
+
+Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Add your Changes
+4. Run the tests and quality checks (`make test` and `make style` and `make quality`)
+5. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+6. Push to the Branch (`git push origin feature/AmazingFeature`)
+
+## License
+
+Distributed under the Apache 2.0 License. See [`LICENSE`](https://github.com/felixdittrich92/OnnxTR?tab=Apache-2.0-1-ov-file#readme) for more information.
---
+++ packages/doctr-labeler/doctr_labeler-0.2.0.tar.gz/doctr_labeler-0.2.0/labeler/automation/auto_labeler.py
@@ -0,0 +1,150 @@
+# Copyright (C) 2024-2025, Felix Dittrich | Ian List | Devarshi Aggarwal.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
+
+import os
+
+import cv2
+import numpy as np
+import onnxruntime as ort
+from onnxtr.io import DocumentFile
+from onnxtr.models import from_hub, ocr_predictor
+
+ort.set_default_logger_severity(os.getenv("ORT_LOG_SEVERITY_LEVEL", 4))
+
+from ..logger import logger
+
+__all__ = ["AutoLabeler"]
+
+
+class AutoLabeler:
+    """
+    AutoLabeler class to predict text from an image and return the polygons and texts.
+    """
+
+    def __init__(self):
+        self.predictor = ocr_predictor(
+            det_arch=self._load_arch(os.environ.get("DETECTION_ARCH", "fast_base")),
+            reco_arch=self._load_arch(os.environ.get("RECOGNITION_ARCH", "Felix92/onnxtr-parseq-multilingual-v1")),
+            det_bs=1,
+            reco_bs=256,
+            assume_straight_pages=False,
+            straighten_pages=False,
+            export_as_straight_boxes=False,
+            preserve_aspect_ratio=True,
+            symmetric_pad=True,
+            detect_orientation=False,
+            detect_language=False,
+            disable_crop_orientation=True,
+            disable_page_orientation=True,
+        )
+        self.objectness_threshold = float(os.environ.get("OBJECTNESS_THRESHOLD", 0.5))
+        logger.info(f"AutoLabeler objectness threshold set to: {self.objectness_threshold}")
+
+    def _load_arch(self, arch: str) -> str:
+        # For HF hub models
+        if arch.count("/") == 1:
+            logger.info(f"Loading model from HF hub: {arch}")
+            return from_hub(arch)
+        # Otherwise OnnxTR downloaded models
+        logger.info(f"Loading model from OnnxTR: {arch}")
+        return arch
+
+    def _to_absolute(self, geom: tuple[tuple[float, float]], img_shape: tuple[int, int]) -> list[list[int]]:
+        """
+        Convert relative coordinates to absolute coordinates.
+
+        Args:
+            geom: tuple[tuple[float, float]]: List of points in relative coordinates
+            img_shape: tuple[int, int]: Shape of the image
+
+        Returns:
+            list[list[int]]: List of points in absolute coordinates
+        """
+        h, w = img_shape
+        if len(geom) == 2:  # Assume straight pages = True -> [[xmin, ymin], [xmax, ymax]]
+            (xmin, ymin), (xmax, ymax) = geom
+            xmin, xmax = int(round(w * xmin)), int(round(w * xmax))
+            ymin, ymax = int(round(h * ymin)), int(round(h * ymax))
+            return [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
+        # Assume straight pages = False -> [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+        else:  # For polygons, convert each point to absolute coordinates
+            return [[int(point[0] * w), int(point[1] * h)] for point in geom]
+
+    def predict(self, image_path: str) -> dict[str, list[list[list[int]]] | list[str]]:
+        """
+        Predict text from an image and return the polygons and texts.
+
+        Args:
+            image_path: str: Path to the image
+
+        Returns:
+            dict[str, list[list[list[int]]] | list[str]]: Dictionary containing the polygons and texts
+        """
+        polygons = []
+        texts = []
+        doc = DocumentFile.from_images(image_path)
+        res = self.predictor(doc)
+        json_res = res.export()
+        for page in json_res["pages"]:
+            shape = page["dimensions"]
+            for block in page["blocks"]:
+                for line in block["lines"]:
+                    for word in line["words"]:
+                        if word["objectness_score"] > self.objectness_threshold:
+                            polygons.append(self._to_absolute(word["geometry"], shape))
+                            texts.append(word["value"])
+
+        return {"polygons": polygons, "texts": texts}
+
+    def _extract_as_straight_box(self, image: np.ndarray, coords: list[list[int]]) -> np.ndarray:
+        """
+        Extract a 4-point polygon from the image and warp it into a straight rectangular crop.
+
+        Args:
+            image (np.ndarray): The input image.
+            coords (list[list[int]]): List of 4 points [[x1, y1], [x2, y2], [x3, y3], [x4, y4]].
+
+
+        Returns:
+            np.ndarray: The straightened rectangular crop.
+        """
+        src_points = np.array(coords, dtype="float32")
+
+        # Determine the bounding box dimensions (width and height of the rectangle)
+        width = max(  # type: ignore[call-overload]
+            np.linalg.norm(src_points[0] - src_points[1]),
+            np.linalg.norm(src_points[2] - src_points[3]),
+        )
+        height = max(  # type: ignore[call-overload]
+            np.linalg.norm(src_points[0] - src_points[3]),
+            np.linalg.norm(src_points[1] - src_points[2]),
+        )
+
+        # Define the destination points for the rectangle
+        dst_points = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype="float32")
+
+        # Compute the perspective transform matrix & warp the image
+        transform_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+        return cv2.warpPerspective(image, transform_matrix, (int(width), int(height)))
+
+    def predict_label(self, image_path: str, coords: list[list[int]]) -> str:
+        """
+        Predict text from an image and return the text for the given coordinates.
+
+        Args:
+            image_path: str: Path to the image
+            coords: list[list[int]]: List of 4 points [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+
+        Returns:
+            str: The predicted text
+        """
+        doc = DocumentFile.from_images(image_path)
+        try:
+            crop = self._extract_as_straight_box(doc[0], coords)
+        except Exception:  # pragma: no cover
+            # Return empty string if the crop is not valid
+            return ""
+        res = self.predictor.reco_predictor([crop])
+        return res[0][0].strip()
