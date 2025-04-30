9bc6f2710d485f71a9a6da761fa109d6925e9c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 15:05:48 +0000

    Add package mdkits/0.1.7/mdkits-0.1.7-py3-none-any.whl

---
+++ packages/mdkits/mdkits-0.1.7-py3-none-any.whl/mdkits/util/out_err.py
@@ -0,0 +1,8 @@
+"""
+output and error for cli
+"""
+
+
+def cell_output(cell: list):
+    print(f"system cell: x = {cell[0]}, y = {cell[1]}, z = {cell[2]}, a = {cell[3]}\u00B0, b = {cell[4]}\u00B0, c = {cell[5]}\u00B0")
+
