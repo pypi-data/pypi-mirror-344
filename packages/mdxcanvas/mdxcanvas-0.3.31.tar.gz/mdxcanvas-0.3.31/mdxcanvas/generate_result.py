4c15b05caf8cd80b13eceb4313525563ea836faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 05:04:59 +0000

    Add package mdxcanvas/0.3.31/mdxcanvas-0.3.31.tar.gz

---
+++ packages/mdxcanvas/mdxcanvas-0.3.31.tar.gz/mdxcanvas-0.3.31/mdxcanvas/generate_result.py
@@ -0,0 +1,29 @@
+import json
+
+class MDXCanvasResult:
+    def __init__(self, output_file: str):
+        self.output_file = output_file
+        self.json = {
+            "deployed_content": {},
+            "content_to_review": [],
+            "error": ""
+        }
+
+    def add_deployed_content(self, rtype: str, content_name: str):
+        if rtype not in self.json["deployed_content"]:
+            self.json["deployed_content"][rtype] = []
+        self.json["deployed_content"][rtype].append(content_name)
+
+    def add_content_to_review(self, quiz_name: str, link_to_quiz: str):
+        self.json["content_to_review"].append([quiz_name, link_to_quiz])
+
+    def get_content_to_review(self):
+        return self.json["content_to_review"]
+
+    def add_error(self, error: str):
+        self.json["error"] = error
+
+    def output(self):
+        if self.output_file:
+            with open(self.output_file, 'w') as f:
+                f.write(json.dumps(self.json, indent=4))
