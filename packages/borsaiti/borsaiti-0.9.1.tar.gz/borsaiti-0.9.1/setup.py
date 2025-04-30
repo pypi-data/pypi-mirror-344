0d8618109c33342a47da00e8fdab3707d82c9192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:37 +0000

    Add package borsaiti/0.9.1/borsaiti-0.9.1.tar.gz

---
+++ packages/borsaiti/borsaiti-0.9.1.tar.gz/borsaiti-0.9.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='borsaiti',
+    version='0.9.1',
+    description='Kick yayinlari icin AI destekli asistan',
+    author='Aytunc',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'mtranslate',
+        'ollama',
+        'selenium',
+        'soundcard',
+        'soundfile',
+        'speechrecognition',
+        'undetected-chromedriver',
+        'certifi'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
