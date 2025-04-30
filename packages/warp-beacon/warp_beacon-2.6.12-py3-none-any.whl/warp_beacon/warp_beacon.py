6a0812efdcb6a09e134b0e8eb4b1e20850ec2549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 06:04:35 +0000

    Add package warp-beacon/2.6.12/warp_beacon-2.6.12-py3-none-any.whl

---
+++ packages/warp-beacon/warp_beacon-2.6.12-py3-none-any.whl/warp_beacon/warp_beacon.py
@@ -0,0 +1,18 @@
+import os
+
+from warp_beacon.telegram.bot import Bot
+
+#import logging
+
+def main() -> None:
+	if __name__ == "__main__":
+		run()
+
+def run() -> None:
+	bot = Bot(
+		tg_bot_name=os.environ.get("TG_BOT_NAME", default=None),
+		tg_token=os.environ.get("TG_TOKEN", default=None),
+		tg_api_id=os.environ.get("TG_API_ID", default=None),
+		tg_api_hash=os.environ.get("TG_API_HASH", default=None)
+	)
+	bot.start()
