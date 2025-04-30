683a8461c9683192aeaa8e521dc3b8633988cfa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 14:05:28 +0000

    Add package vector2dggs/0.6.2/vector2dggs-0.6.2-py3-none-any.whl

---
+++ packages/vector2dggs/vector2dggs-0.6.2-py3-none-any.whl/vector2dggs/cli.py
@@ -0,0 +1,27 @@
+import click
+
+from vector2dggs import __version__
+from vector2dggs.h3 import h3
+from vector2dggs.rHP import rhp
+
+#   If the program does terminal interaction, make it output a short
+# notice like this when it starts in an interactive mode:
+
+#     <program>  Copyright (C) <year>  <name of author>
+#     This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+#     This is free software, and you are welcome to redistribute it
+#     under certain conditions; type `show c' for details.
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    pass
+
+
+cli.add_command(h3)
+cli.add_command(rhp)
+
+
+def main():
+    cli()
