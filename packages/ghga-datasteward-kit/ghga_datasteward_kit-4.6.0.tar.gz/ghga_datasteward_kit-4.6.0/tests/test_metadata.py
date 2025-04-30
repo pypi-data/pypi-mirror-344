e52a9a360e58884b19702f0bcb79c414936e79c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:31 +0000

    Add package ghga-datasteward-kit/4.6.0/ghga_datasteward_kit-4.6.0.tar.gz

---
+++ packages/ghga-datasteward-kit/ghga_datasteward_kit-4.6.0.tar.gz/ghga_datasteward_kit-4.6.0/tests/test_metadata.py
@@ -0,0 +1,80 @@
+# Copyright 2021 - 2025 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Metadata related tests"""
+
+import os
+import shutil
+from collections.abc import Generator
+from pathlib import Path
+
+import pytest
+
+from ghga_datasteward_kit.cli.metadata import submit, transform
+from tests.fixtures.metadata import (
+    ARCHIVE_METADATA_CONFIG_PATH,
+    ARCHIVE_ORIGINAL_METADATA_PATH,
+    ARCHIVE_ORIGINAL_MODEL_PATH,
+    METADATA_CONFIG_PATH,
+    ORIGINAL_METADATA_PATH,
+    ORIGINAL_MODEL_PATH,
+)
+
+
+def workdir_factory(original_model_path: Path):
+    """Prepare a work directory for the provided model."""
+
+    @pytest.fixture
+    def workdir(tmp_path: Path) -> Generator[Path, None, None]:
+        """Prepare a work directory for"""
+        tmp_path.joinpath("event_store").mkdir()
+        tmp_path.joinpath("submission_store").mkdir()
+        tmp_path.joinpath("accession_store").touch()
+        tmp_path.joinpath("artifact_model").touch()
+        shutil.copyfile(original_model_path, tmp_path / "original_model.yaml")
+        cwd = os.getcwd()
+        os.chdir(tmp_path)
+        yield tmp_path
+        os.chdir(cwd)
+
+    return workdir
+
+
+example_workdir = workdir_factory(ORIGINAL_MODEL_PATH)
+archive_workdir = workdir_factory(ARCHIVE_ORIGINAL_MODEL_PATH)
+
+
+def test_happy(example_workdir: Path):
+    """Test the 'happy' test case that is expected to run through without errors"""
+    submit(
+        submission_title="Test Title",
+        submission_description="Test Description",
+        metadata_path=ORIGINAL_METADATA_PATH,
+        config_path=METADATA_CONFIG_PATH,
+    )
+
+    transform(config_path=METADATA_CONFIG_PATH)
+
+
+def test_archive_happy(archive_workdir: Path):
+    """Test the 'happy' test case that is expected to run through without errors"""
+    submit(
+        submission_title="Test Title",
+        submission_description="Test Description",
+        metadata_path=ARCHIVE_ORIGINAL_METADATA_PATH,
+        config_path=ARCHIVE_METADATA_CONFIG_PATH,
+    )
+
+    transform(config_path=ARCHIVE_METADATA_CONFIG_PATH)
