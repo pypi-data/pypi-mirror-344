8048e3ad72b5775057293a8c85f83d41a20d1ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 03:19:24 +0000

    Add package tko/4.0.2/tko-4.0.2-py3-none-any.whl

---
+++ packages/tko/tko-4.0.2-py3-none-any.whl/tko/play/language_setter.py
@@ -0,0 +1,51 @@
+from tko.play.flags import FlagsMan
+from tko.play.floating import Floating, FloatingInput, FloatingInputData
+from tko.play.floating_manager import FloatingManager
+from tko.util.text import Text
+from typing import List
+from tko.settings.repository import Repository, available_languages
+
+
+class LanguageSetter:
+    def __init__(self, rep: Repository, flagsman: FlagsMan, fman: FloatingManager):
+        self.flagsman = flagsman
+        self.fman = fman
+        self.rep = rep
+
+
+    def set_language(self):
+        options: List[FloatingInputData] = []
+        for lang in available_languages:
+            options.append(FloatingInputData(TextFunctor(lang), SetLangFunctor(self.rep, self.fman, lang)))
+
+        self.fman.add_input(
+            FloatingInput("^")
+            .set_header(" Escolha a extensão default para os rascunhos ")
+            .set_options(options)
+            .set_default_index(available_languages.index(self.rep.get_lang()))
+            .set_footer(" Escolha e reinicie o tko para aplicar!!!!! ")
+        )
+
+class TextFunctor:
+    def __init__(self, data: str):
+        self.value = data
+    
+    def __call__(self):
+        return Text().add(self.value)
+
+class SetLangFunctor:
+    def __init__(self, rep: Repository, fman: FloatingManager, lang: str):
+        self.rep = rep
+        self.fman = fman
+        self.value = lang
+
+    def __call__(self):
+        self.rep.set_lang(self.value.strip())
+        self.rep.save_config()
+        self.fman.add_input(
+            Floating()
+            .put_text("")
+            .put_text("Linguagem alterada para " + self.value)
+            .put_text("")
+            .warning()
+        )
