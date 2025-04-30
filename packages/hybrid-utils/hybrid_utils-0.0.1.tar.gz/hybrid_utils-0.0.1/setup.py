9a0d32a0b2506fbec948b275763b0a484ad38df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-30 04:04:34 +0000

    Add package hybrid-utils/0.0.1/hybrid_utils-0.0.1.tar.gz

---
+++ packages/hybrid-utils/hybrid_utils-0.0.1.tar.gz/hybrid_utils-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hybrid-utils',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
