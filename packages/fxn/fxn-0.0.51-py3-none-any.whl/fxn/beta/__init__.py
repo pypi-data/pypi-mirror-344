45313b43beab9e4a65fa3cc30e84c52200979ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:51 +0000

    Add package fxn/0.0.51/fxn-0.0.51-py3-none-any.whl

---
+++ packages/fxn/fxn-0.0.51-py3-none-any.whl/fxn/beta/__init__.py
@@ -0,0 +1,10 @@
+# 
+#   Function
+#   Copyright © 2025 NatML Inc. All Rights Reserved.
+#
+
+from .metadata import (
+    CoreMLInferenceMetadata, LiteRTInferenceMetadata, LlamaCppInferenceMetadata,
+    ONNXInferenceMetadata, ONNXRuntimeInferenceSessionMetadata
+)
+from .remote import RemoteAcceleration
