5e52f4041d2c72d5d502abfb41e3f8a2b52a3978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:26:06 +0000

    Add package ir-sim/2.4.3/ir_sim-2.4.3-py3-none-any.whl

---
+++ packages/ir-sim/ir_sim-2.4.3-py3-none-any.whl/irsim/world/map/obstacle_map.py
@@ -0,0 +1,19 @@
+from irsim.world import ObjectBase
+
+
+class ObstacleMap(ObjectBase):
+
+    def __init__(
+        self,
+        shape={"name": "map", "reso": "0.1", "points": None},
+        color="k",
+        static=True,
+        **kwargs,
+    ):
+        super(ObstacleMap, self).__init__(
+            shape=shape,
+            role="obstacle",
+            color=color,
+            static=static,
+            **kwargs,
+        )
