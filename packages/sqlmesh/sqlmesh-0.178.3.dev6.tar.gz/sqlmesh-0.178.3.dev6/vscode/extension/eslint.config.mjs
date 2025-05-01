6343de52b9fb795d08a5e74b77836e203fe3ceae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:38 +0000

    Add package sqlmesh/0.178.3.dev6/sqlmesh-0.178.3.dev6.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.178.3.dev6.tar.gz/sqlmesh-0.178.3.dev6/vscode/extension/eslint.config.mjs
@@ -0,0 +1,29 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [{
+    files: ["**/*.ts"],
+}, {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module",
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "import",
+            format: ["camelCase", "PascalCase"],
+        }],
+
+        curly: "error",
+        eqeqeq: "error",
+        "no-throw-literal": "error",
+        semi: ["error", "never"],
+        "no-shadow": "error",
+    },
+}];
