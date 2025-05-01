c464e233e9c0a24f63b7da61f9b2e30a92a6442f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:04 +0000

    Add package eric-ide/25.5/eric_ide-25.5-py3-none-any.whl

---
+++ packages/eric-ide/eric_ide-25.5-py3-none-any.whl/eric7/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 - 2025 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module containing some eric-ide version information.
+"""
+
+Version = "25.5 (rev. 6826ed3e7772)"
+VersionOnly = "25.5"
