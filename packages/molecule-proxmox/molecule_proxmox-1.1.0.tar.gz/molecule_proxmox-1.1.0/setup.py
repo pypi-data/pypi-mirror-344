5888dc431756245106c969ff5390cc02251dab0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 06:07:13 +0000

    Add package molecule-proxmox/1.1.0/molecule_proxmox-1.1.0.tar.gz

---
+++ packages/molecule-proxmox/molecule_proxmox-1.1.0.tar.gz/molecule_proxmox-1.1.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import re
+
+name = 'molecule-proxmox'
+description='Proxmox Molecule Plugin :: run molecule tests using proxmox'
+
+def find_version():
+    text = open('src/%s/__init__.py' % name.replace('-', '_')).read()
+    return re.search(r"__version__\s*=\s*'(.*)'", text).group(1)
+
+setuptools.setup(
+    name=name,
+    version=find_version(),
+    author='Michael Meffie',
+    author_email='mmeffie@sinenomine.net',
+    description=description,
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/meffie/molecule-proxmox',
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    entry_points={
+        'molecule.driver': [
+            'proxmox = molecule_proxmox.driver:Proxmox',
+        ],
+    },
+    install_requires=[
+        # molecule plugins are not allowed to mention Ansible as a direct dependency
+        'molecule>=6.0.0,<=25.4.0',
+        'PyYAML',
+        'proxmoxer>=1.3.1',
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
---
+++ packages/molecule-proxmox/molecule_proxmox-1.1.0.tar.gz/molecule_proxmox-1.1.0/tests/test_proxmox_driver.py
@@ -0,0 +1,48 @@
+#
+# Proxmox driver tests.
+#
+
+import contextlib
+import os
+import pathlib
+import subprocess
+import pytest
+
+
+@contextlib.contextmanager
+def chdir(path):
+    prev = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev)
+
+
+def molecule(command, *args):
+    args = ['molecule', command] + list(args)
+    proc = subprocess.Popen(args)
+    rc = proc.wait()
+    assert rc == 0
+
+
+def test_molecule_init_scenario(tmpdir):
+    print('')
+    with chdir(tmpdir):
+        molecule('init', 'scenario', '--driver-name', 'molecule-proxmox')
+        assert pathlib.Path('molecule/default/converge.yml').exists()
+        assert pathlib.Path('molecule/default/create.yml').exists()
+        assert pathlib.Path('molecule/default/destroy.yml').exists()
+        assert pathlib.Path('molecule/default/molecule.yml').exists()
+
+
+@pytest.mark.parametrize('scenario', [
+                           'default', 'by-name', 'by-vmid', 'cloud-init',
+                           'secrets-file', 'secrets-script', 'linked-clone'])
+def test_molecule_test(scenario):
+    print('')
+    testdir = pathlib.Path(__file__).resolve().parent
+    projectdir = testdir / 'proxmox_driver'
+    with chdir(projectdir):
+        molecule('test', '--scenario-name', scenario)
+        molecule('reset')
