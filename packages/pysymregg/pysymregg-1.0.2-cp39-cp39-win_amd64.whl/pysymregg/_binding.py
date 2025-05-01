07d6434b054663d18da303bfbb4929cdc2edcf8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 08:06:00 +0000

    Add package pysymregg/1.0.2/pysymregg-1.0.2-cp39-cp39-win_amd64.whl

---
+++ packages/pysymregg/pysymregg-1.0.2-cp39-cp39-win_amd64.whl/pysymregg/_binding.py
@@ -0,0 +1,20 @@
+def __bootstrap__():
+   global __bootstrap__, __file__, __loader__
+   import sys, os, pkg_resources, importlib.util
+   __file__ = pkg_resources.resource_filename(__name__,'_binding.cp39-win_amd64.pyd')
+   del __bootstrap__
+   if '__loader__' in globals():
+       del __loader__
+
+   old_dir = os.getcwd()
+   try:
+     os.chdir(os.path.dirname(__file__))
+
+     spec = importlib.util.spec_from_file_location(
+                __name__, __file__)
+     mod = importlib.util.module_from_spec(spec)
+     spec.loader.exec_module(mod)
+   finally:
+
+     os.chdir(old_dir)
+__bootstrap__()
