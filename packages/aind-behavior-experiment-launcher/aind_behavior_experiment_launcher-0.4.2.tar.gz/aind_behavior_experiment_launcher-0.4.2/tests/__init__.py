1e860eec1ca83a2380148dcd878e3f960d48e147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 14:07:28 +0000

    Add package aind-behavior-experiment-launcher/0.4.2/aind_behavior_experiment_launcher-0.4.2.tar.gz

---
+++ packages/aind-behavior-experiment-launcher/aind_behavior_experiment_launcher-0.4.2.tar.gz/aind_behavior_experiment_launcher-0.4.2/tests/__init__.py
@@ -0,0 +1,40 @@
+import contextlib
+import importlib.util
+import io
+import logging
+import os
+import sys
+from pathlib import Path
+from types import ModuleType
+
+REPO_ROOT = Path(__file__).parents[1]
+
+TESTS_ASSETS = REPO_ROOT / "tests" / "assets"
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logging.disable(logging.CRITICAL)
+os.environ["WATCHDOG_EXE"] = "watchdog.exe"
+os.environ["WATCHDOG_CONFIG"] = "watchdog_config.yml"
+
+
+def build_example(script_path: str) -> ModuleType:
+    module_name = Path(script_path).stem
+    spec = importlib.util.spec_from_file_location(module_name, script_path)
+    if spec is None:
+        raise ImportError(f"Can't find {script_path}")
+    module = importlib.util.module_from_spec(spec)
+    if spec.loader is None:
+        raise ImportError(f"Can't load {script_path}")
+    spec.loader.exec_module(module)
+    return module
+
+
+@contextlib.contextmanager
+def suppress_stdout():
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    try:
+        yield
+    finally:
+        sys.stdout = original_stdout
