07d381aec6ba567f6aa284dbe66197fc00d3d7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 06:05:37 +0000

    Add package sversion/2.2.0/sversion-2.2.0.tar.gz

---
+++ packages/sversion/sversion-2.2.0.tar.gz/sversion-2.2.0/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "kostiantyn.chomakov@gmail.com"
+
+from . import version_file_based, error_handling, contracts
+
+__all__ = ['version_file_based',
+           'pyproject_toml_based',
+           'error_handling',
+           'contracts']
