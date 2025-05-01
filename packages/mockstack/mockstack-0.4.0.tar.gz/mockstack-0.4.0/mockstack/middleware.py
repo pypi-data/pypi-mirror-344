c21333849292ec117cdb5e64ac60d7eb2467801f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 15:06:23 +0000

    Add package mockstack/0.4.0/mockstack-0.4.0.tar.gz

---
+++ packages/mockstack/mockstack-0.4.0.tar.gz/mockstack-0.4.0/mockstack/middleware.py
@@ -0,0 +1,19 @@
+"""Middleware definitionsfor the mockstack app."""
+
+import time
+
+from fastapi import FastAPI, Request
+
+from mockstack.config import Settings
+
+
+def middleware_provider(app: FastAPI, settings: Settings) -> None:
+    """Instrument the middlewares to the mockstack app."""
+
+    @app.middleware("http")
+    async def add_process_time_header(request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        return response
---
+++ packages/mockstack/mockstack-0.4.0.tar.gz/mockstack-0.4.0/mockstack/telemetry.py
@@ -0,0 +1,44 @@
+"""OpenTelemetry integration."""
+
+from importlib import metadata
+
+from fastapi import FastAPI, Request
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from mockstack.config import Settings
+
+
+def span_name_for(request: Request) -> str:
+    """Get the span name for a request."""
+    return f"{request.method.upper()} {request.url.path}"
+
+
+def opentelemetry_provider(app: FastAPI, settings: Settings) -> None:
+    """Initialize OpenTelemetry for the mockstack app."""
+    if not settings.opentelemetry.enabled:
+        return
+
+    # Initialize OpenTelemetry
+    distribution = metadata.distribution("mockstack")
+    resource = Resource(
+        attributes={
+            "service.name": distribution.name,
+            "service.version": distribution.version,
+        }
+    )
+
+    tracer_provider = TracerProvider(resource=resource)
+    trace.set_tracer_provider(tracer_provider)
+
+    # Set up OTLP exporter
+    otlp_exporter = OTLPSpanExporter(endpoint=settings.opentelemetry.endpoint)
+    span_processor = BatchSpanProcessor(otlp_exporter)
+    tracer_provider.add_span_processor(span_processor)
+
+    # Nb. we do not actually use the default FastAPIInstrumentor here
+    # because we use custom tracing in various places.
+    # FastAPIInstrumentor.instrument_app(app)
