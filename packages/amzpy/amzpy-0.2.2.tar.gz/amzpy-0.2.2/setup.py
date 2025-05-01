1c2b49fa6498bce76e77187cc310cf5d2f28323d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:17 +0000

    Add package amzpy/0.2.2/amzpy-0.2.2.tar.gz

---
+++ packages/amzpy/amzpy-0.2.2.tar.gz/amzpy-0.2.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="amzpy",
+    version="0.2.2",
+    description="A lightweight Amazon scraper library.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Anil Sardiwal",
+    author_email="theonlyanil@gmail.com",
+    url="https://github.com/theonlyanil/amzpy",
+    packages=find_packages(),
+    license_files = ('LICENSE'),
+    install_requires=[
+        "curl_cffi>=0.5.7",
+        "beautifulsoup4>=4.11.0",
+        "lxml>=4.9.0",
+        "fake_useragent>=1.1.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    python_requires=">=3.6",
+    keywords="amazon, scraper, web-scraping, product-data, e-commerce, curl_cffi, anti-bot",
+)
