75e2cea122a8fa808f498e5607a047328e2486df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:27 +0000

    Add package snowflow/0.0.14/snowflow-0.0.14.tar.gz

---
+++ packages/snowflow/snowflow-0.0.14.tar.gz/snowflow-0.0.14/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="snowflow",
+    version="0.0.14",
+    author="Thomas Garcia, Aryan Singh",
+    author_email="tgarcia@svam.com, aryan.singh@svam.com",
+    description="SnowFlow - A Snowflake Deployment Tool",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'snowflow': ['*.sql', 'snowflow/*.sql']},
+    install_requires=[
+        "snowflake-connector-python",
+        "snowflake-snowpark-python",
+        "networkx",
+        "pyyaml",
+        "toml",
+    ],
+    entry_points={
+        'console_scripts': [
+            'snowflow = snowflow.handler:main',  
+            #'snowflow = snowflow:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    license="BSD-3-Clause",
+    python_requires='>=3.6',
+)
