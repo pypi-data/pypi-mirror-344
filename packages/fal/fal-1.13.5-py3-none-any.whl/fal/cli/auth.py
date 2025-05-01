d25a0423126379394edad018050e6e87e74d548c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 07:05:17 +0000

    Add package fal/1.13.5/fal-1.13.5-py3-none-any.whl

---
+++ packages/fal/fal-1.13.5-py3-none-any.whl/fal/cli/auth.py
@@ -0,0 +1,172 @@
+def _login(args):
+    from fal.auth import login
+    from fal.config import Config
+    from fal.console.icons import CHECK_ICON, CROSS_ICON
+    from fal.exceptions import FalServerlessException
+
+    try:
+        login(args.console)
+        args.console.print(f"{CHECK_ICON} Authenticated successfully, welcome!")
+    except FalServerlessException as e:
+        args.console.print(f"{CROSS_ICON} {e}")
+        return
+
+    with Config().edit() as config:
+        config.unset("team")
+
+    _set_account(args)
+
+
+def _logout(args):
+    from fal.auth import logout
+    from fal.config import Config
+    from fal.console.icons import CHECK_ICON, CROSS_ICON
+    from fal.exceptions import FalServerlessException
+
+    try:
+        logout(args.console)
+        args.console.print(f"{CHECK_ICON} Logged out of [cyan bold]fal[/]. Bye!")
+    except FalServerlessException as e:
+        args.console.print(f"{CROSS_ICON} {e}")
+        return
+
+    with Config().edit() as config:
+        config.unset("team")
+
+
+def _list_accounts(args):
+    from rich.style import Style
+    from rich.table import Table
+
+    from fal.auth import UserAccess
+    from fal.config import Config
+
+    user_access = UserAccess()
+    config = Config()
+    current_account = config.get("team") or user_access.info["nickname"]
+
+    table = Table(border_style=Style(frame=False), show_header=False)
+    table.add_column("#")
+    table.add_column("Nickname")
+    table.add_column("Type")
+
+    for idx, account in enumerate(user_access.accounts):
+        selected = account["nickname"] == current_account
+        color = "bold yellow" if selected else None
+
+        table.add_row(
+            f"* {idx + 1}" if selected else f"  {idx + 1}",
+            account["nickname"],
+            "Personal" if account["is_personal"] else "Team",
+            style=color,
+        )
+
+    args.console.print(table)
+
+
+def _set_account(args):
+    from rich.prompt import Prompt
+
+    from fal.auth import UserAccess
+    from fal.config import Config
+
+    user_access = UserAccess()
+
+    if hasattr(args, "account") and args.account:
+        if args.account.isdigit():
+            acc_index = int(args.account) - 1
+            account = user_access.accounts[acc_index]
+        else:
+            account = user_access.get_account(args.account)
+    else:
+        _list_accounts(args)
+        indices = list(map(str, range(1, len(user_access.accounts) + 1)))
+        team_names = [account["nickname"] for account in user_access.accounts]
+        acc_choice = Prompt.ask(
+            "Select an account by number",
+            choices=indices + team_names,
+            show_choices=False,
+        )
+        if acc_choice in indices:
+            acc_index = int(acc_choice) - 1
+            account = user_access.accounts[acc_index]
+        else:
+            account = user_access.get_account(acc_choice)
+
+    if account["is_personal"]:
+        args.console.print(
+            f"Using personal account [cyan]{account['nickname']}[/]. "
+            "You can change this later with [bold]fal team set[/]"
+        )
+    else:
+        args.console.print(
+            f"Using team account [cyan]{account['nickname']}[/]. "
+            "You can change this later with [bold]fal team set[/]"
+        )
+
+    with Config().edit() as config:
+        config.set("team", account["nickname"])
+
+
+def _whoami(args):
+    from fal.auth import UserAccess
+    from fal.config import Config
+
+    user_access = UserAccess()
+    config = Config()
+
+    team = config.get("team")
+    if team:
+        account = user_access.get_account(team)
+    else:
+        account = user_access.get_account(user_access.info["nickname"])
+
+    nickname = account["nickname"]
+    full_name = account["full_name"]
+    user_id = account["user_id"]
+
+    args.console.print(f"Hello, {full_name}: {nickname!r} - {user_id!r}")
+
+
+def add_parser(main_subparsers, parents):
+    auth_help = "Authenticate with fal."
+    parser = main_subparsers.add_parser(
+        "auth",
+        description=auth_help,
+        help=auth_help,
+        parents=parents,
+    )
+
+    subparsers = parser.add_subparsers(
+        title="Commands",
+        metavar="command",
+        dest="cmd",
+        required=True,
+    )
+
+    login_help = "Log in a user."
+    login_parser = subparsers.add_parser(
+        "login",
+        description=login_help,
+        help=login_help,
+        parents=parents,
+    )
+    login_parser.set_defaults(func=_login)
+
+    logout_help = "Log out the currently logged-in user."
+    logout_parser = subparsers.add_parser(
+        "logout",
+        description=logout_help,
+        help=logout_help,
+        parents=parents,
+    )
+    logout_parser.set_defaults(func=_logout)
+
+    whoami_help = "Show the currently authenticated user."
+    whoami_parser = subparsers.add_parser(
+        "whoami",
+        description=whoami_help,
+        help=whoami_help,
+        parents=parents,
+    )
+    whoami_parser.set_defaults(func=_whoami)
---
+++ packages/fal/fal-1.13.5-py3-none-any.whl/fal/cli/teams.py
@@ -0,0 +1,54 @@
+from fal.cli.auth import _list_accounts, _set_account
+
+
+def _unset(args):
+    from fal.config import Config
+
+    with Config().edit() as config:
+        config.unset("team")
+
+
+def add_parser(main_subparsers, parents):
+    teams_help = "Manage teams."
+    parser = main_subparsers.add_parser(
+        "teams",
+        aliases=["team"],
+        description=teams_help,
+        help=teams_help,
+        parents=parents,
+    )
+
+    subparsers = parser.add_subparsers(
+        title="Commands",
+        metavar="command",
+        dest="cmd",
+        required=True,
+    )
+
+    list_help = "List teams."
+    list_parser = subparsers.add_parser(
+        "list",
+        description=list_help,
+        help=list_help,
+        parents=parents,
+    )
+    list_parser.set_defaults(func=_list_accounts)
+
+    set_help = "Set the current team."
+    set_parser = subparsers.add_parser(
+        "set",
+        description=set_help,
+        help=set_help,
+        parents=parents,
+    )
+    set_parser.add_argument("account", help="The team to set.")
+    set_parser.set_defaults(func=_set_account)
+
+    unset_help = "Unset the current team."
+    unset_parser = subparsers.add_parser(
+        "unset",
+        description=unset_help,
+        help=unset_help,
+        parents=parents,
+    )
+    unset_parser.set_defaults(func=_unset)
