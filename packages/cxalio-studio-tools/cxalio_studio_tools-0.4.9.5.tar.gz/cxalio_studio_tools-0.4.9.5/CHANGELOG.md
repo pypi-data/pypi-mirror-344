96507f567a03bf3bd9fa691b4680cd4e167aaa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:39 +0000

    Add package cxalio-studio-tools/0.4.9.5/cxalio_studio_tools-0.4.9.5.tar.gz

---
+++ packages/cxalio-studio-tools/cxalio_studio_tools-0.4.9.5.tar.gz/cxalio_studio_tools-0.4.9.5/CHANGELOG.md
@@ -0,0 +1,17 @@
+# Change Log of Cxalio Studio Tools
+
+### v0.4.9.5
+- 全面使用 pydantic 和 Box 替代原生和 cx-studio 的实现
+
+### v0.4.9.4
+
+- hot fix for a packaging bug
+
+### v0.4.9.3
+
+- 修复了 Media Killer 提示信息的 bug
+- 增加了 FFpretty 工具
+
+### v0.4.9.1
+
+- Media Scout 现在可以选择输出转义模式了
---
+++ packages/cxalio-studio-tools/cxalio_studio_tools-0.4.9.5.tar.gz/cxalio_studio_tools-0.4.9.5/media_killer/components/mission_arranger.py
@@ -0,0 +1,44 @@
+from operator import attrgetter
+from typing import Generator, Literal
+
+from cx_wealth.wealth_label import WealthLabel
+from media_killer.appenv import appenv
+from .mission import Mission
+
+
+class MissionArranger:
+    def __init__(
+        self,
+        missions: list[Mission],
+        sort_mode: Literal["source", "target", "preset", "x"] = "x",
+    ):
+        self.missions = missions
+        self.sort_mode = sort_mode
+
+        self._sorters = {
+            "source": self.__sort_by_source,
+            "target": self.__sort_by_target,
+            "preset": self.__sort_by_preset,
+            "x": self.__no_sort,
+        }
+
+    def __no_sort(self) -> Generator[Mission]:
+        yield from self.missions
+
+    def __sort_by_source(self) -> Generator[Mission]:
+        yield from sorted(self.missions, key=attrgetter("source"))
+
+    def __sort_by_target(self) -> Generator[Mission]:
+        yield from sorted(self.missions, key=attrgetter("standard_target"))
+
+    def __sort_by_preset(self) -> Generator[Mission]:
+        yield from sorted(self.missions, key=lambda x: x.preset_id)
+
+    def __iter__(self) -> Generator[Mission]:
+        cache = set()
+        for m in self._sorters[self.sort_mode]():
+            if m in cache:
+                appenv.say(WealthLabel(m), "是重复任务，已自动排除。")
+                continue
+            cache.add(m)
+            yield m
