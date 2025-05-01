806617e236001e328e2e9680bc9b597862569053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:06:50 +0000

    Add package whisperx/3.3.3/whisperx-3.3.3-py3-none-any.whl

---
+++ packages/whisperx/whisperx-3.3.3-py3-none-any.whl/whisperx/__init__.py
@@ -0,0 +1,7 @@
+from whisperx.alignment import load_align_model as load_align_model, align as align
+from whisperx.asr import load_model as load_model
+from whisperx.audio import load_audio as load_audio
+from whisperx.diarize import (
+    assign_word_speakers as assign_word_speakers,
+    DiarizationPipeline as DiarizationPipeline,
+)
