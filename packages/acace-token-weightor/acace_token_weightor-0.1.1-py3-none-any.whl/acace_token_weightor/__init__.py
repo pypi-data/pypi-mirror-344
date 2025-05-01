4a0b0ac0f1d1260cd2b158ea7da53a78e2b7072a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 07:05:08 +0000

    Add package acace-token-weightor/0.1.1/acace_token_weightor-0.1.1-py3-none-any.whl

---
+++ packages/acace-token-weightor/acace_token_weightor-0.1.1-py3-none-any.whl/acace_token_weightor/__init__.py
@@ -0,0 +1,9 @@
+"""
+acace_token_weightor - Assigns importance weights to tokens using NLP heuristics for compression prioritization.
+"""
+
+__version__ = "0.1.1"
+
+def get_version():
+    """Return the package version."""
+    return __version__
