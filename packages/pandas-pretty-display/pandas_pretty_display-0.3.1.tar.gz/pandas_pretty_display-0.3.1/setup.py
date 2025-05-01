0358c8b2345507cbea6e03f8c793d3468871d3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:32 +0000

    Add package pandas-pretty-display/0.3.1/pandas_pretty_display-0.3.1.tar.gz

---
+++ packages/pandas-pretty-display/pandas_pretty_display-0.3.1.tar.gz/pandas_pretty_display-0.3.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pandas_pretty_display",
+    version="0.3.1",
+    author="Balaji",
+    author_email="bala@python4u.in",
+    description="A package to make pandas DataFrames display beautifully in Jupyter notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bala-srm/pandas-pretty-display-.git",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "ipython>=7.0.0",
+        "pandas>=1.0.0"
+    ],
+)
