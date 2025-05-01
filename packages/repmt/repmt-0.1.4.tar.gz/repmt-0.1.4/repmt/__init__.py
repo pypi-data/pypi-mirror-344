ebbd2ddd0c3bd7165e1e61d4ae7c3f9ac5b8438f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 07:05:09 +0000

    Add package repmt/0.1.4/repmt-0.1.4.tar.gz

---
+++ packages/repmt/repmt-0.1.4.tar.gz/repmt-0.1.4/repmt/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 RePromptsQuest
+# Licensed under the MIT License
+
+# reprompt package initialization.
+from .backend import backend, prompt_generator
+ # if you have plugin-related code, otherwise you can remove this line.
