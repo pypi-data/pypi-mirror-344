1d257cba065fdb3551e4e0773749033ecf36ca7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:04 +0000

    Add package aiomexc/0.2.1/aiomexc-0.2.1-py3-none-any.whl

---
+++ packages/aiomexc/aiomexc-0.2.1-py3-none-any.whl/aiomexc/ws/messages/__init__.py
@@ -0,0 +1,17 @@
+from .subscription import subscription, unsubscribe, PING, PONG
+from .listen_key import ListenKeyExtendedMessage
+from .public_aggre_deals import PublicAggreDealsMessage, PublicAggreDealItemMessage
+from .private_order import PrivateOrdersMessage
+from .base import BaseMessage
+
+__all__ = [
+    "subscription",
+    "unsubscribe",
+    "PING",
+    "PONG",
+    "ListenKeyExtendedMessage",
+    "PublicAggreDealsMessage",
+    "PublicAggreDealItemMessage",
+    "BaseMessage",
+    "PrivateOrdersMessage",
+]
