5997b2749df67181e225227dfcc8e71385bc9c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:20 +0000

    Add package mynotification/0.7.0/mynotification-0.7.0.tar.gz

---
+++ packages/mynotification/mynotification-0.7.0.tar.gz/mynotification-0.7.0/src/mynotification/__init__.py
@@ -0,0 +1,16 @@
+class Notification:
+    def __init__(self, title: str, message: str, platform: str = "desktop"):
+        self.title = title
+        self.message = message
+        self.platform = platform
+
+    def send_mynotification(self):
+        desktop = ["linux", "windows", "macos"]
+        if self.platform == "android":
+            from src.mynotification.notification_android import send_notification
+
+            send_notification(title=self.title, message=self.message)
+        if self.platform in desktop:
+            from src.mynotification.notification_desktop import send_notification
+
+            send_notification(title=self.title, message=self.message)
