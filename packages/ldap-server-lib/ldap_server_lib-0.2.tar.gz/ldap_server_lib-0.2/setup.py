e8389ac30ce753c23e5db32592b085f52ff1d514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:28 +0000

    Add package ldap-server-lib/0.2/ldap_server_lib-0.2.tar.gz

---
+++ packages/ldap-server-lib/ldap_server_lib-0.2.tar.gz/ldap_server_lib-0.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='ldap_server_lib',
+    version='0.2',
+    description="well uh, not for commercial use i guess????",
+    packages=['ldap_server_lib']
+)
