55015c5994d025dae3f1c21dd71727f8637fb882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 15:06:17 +0000

    Add package BusServoControl/0.1.1/BusServoControl-0.1.1.tar.gz

---
+++ packages/BusServoControl/BusServoControl-0.1.1.tar.gz/BusServoControl-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BusServoControl',  # 包名
+    version='0.1.1',  # 版本号
+    packages=find_packages(),  # 自动查找包
+    install_requires=[  # 依赖项
+        "pyserial"
+    ],
+    author='Ma Fukang',  # 作者
+    author_email='956787367@qq.com',  # 作者邮箱
+    description='A module for controlling servo motors',  # 描述
+    long_description=open('README.md').read(),  # 长描述
+    long_description_content_type='text/markdown',  # 长描述格式
+    url='https://github.com/LeurDeLis/BusServoControl',  # 项目地址
+    license='MIT',  # 许可证
+)
