572a09af340ff763b33d0adab044cca05ffdaf1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:37 +0000

    Add package labelu/1.2.12/labelu-1.2.12-py3-none-any.whl

---
+++ packages/labelu/labelu-1.2.12-py3-none-any.whl/labelu/internal/statics/backend_version.js
@@ -0,0 +1,10 @@
+
+                  (function () {
+                    window.__backend = {
+  "version": "1.2.12",
+  "name": "labelu",
+  "build_time": "2025-04-30T11:52:53.572Z",
+  "commit": "932a8ac5624be7fd5b2f87d0ae0c9ed845ed622a"
+};
+                  })();
+                
