a5e3539c38ad25bf683cf96160be54a9e50a6a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:06:44 +0000

    Add package kajihs_utils/0.9.0/kajihs_utils-0.9.0-py3-none-any.whl

---
+++ packages/kajihs_utils/kajihs_utils-0.9.0-py3-none-any.whl/kajihs_utils/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Kajihs Utils."""
+
+from kajihs_utils.core import batch, bisect_predicate, get_first, is_sorted
+
+__all__ = ["batch", "bisect_predicate", "get_first", "is_sorted"]
