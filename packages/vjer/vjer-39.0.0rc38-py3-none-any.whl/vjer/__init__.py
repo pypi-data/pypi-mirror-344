ad06a19be48bc72b92b58bf4e46d1803fd44fbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 08:05:56 +0000

    Add package vjer/39.0.0rc38/vjer-39.0.0rc38-py3-none-any.whl

---
+++ packages/vjer/vjer-39.0.0rc38-py3-none-any.whl/vjer/__init__.py
@@ -0,0 +1,24 @@
+"""Vjer CI/CD module.
+
+This module provides utilities for making it easier to write CI/CD automations.
+
+"""
+
+__all__ = ('__title__', '__summary__', '__uri__',
+           '__version__', '__build_name__', '__build_date__',
+           '__author__', '__email__',
+           '__license__', '__copyright__')
+
+__title__ = 'Vjer'
+__summary__ = 'CI/CD Toolkit'
+__uri__ = 'https://github.com/arisilon/vjer/'
+
+__version__ = '39.0.0rc38'
+__build_name__ = 'vjer_39.0.0rc38-14765021693'
+__build_date__ = '2025-04-30 21:51:12.002620'
+
+__author__ = 'Jeffery G. Smith'
+__email__ = 'web@pobox.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2025 Jeffery G. Smith'
