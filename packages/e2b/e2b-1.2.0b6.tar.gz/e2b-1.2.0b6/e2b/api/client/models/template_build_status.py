5175bfb3dc7cf621f149966a0cdb9e78df16bc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:47 +0000

    Add package e2b/1.2.0b6/e2b-1.2.0b6.tar.gz

---
+++ packages/e2b/e2b-1.2.0b6.tar.gz/e2b-1.2.0b6/e2b/api/client/models/template_build_status.py
@@ -0,0 +1,16 @@
+from typing import Literal, cast
+
+TemplateBuildStatus = Literal["building", "error", "ready", "waiting"]
+
+TEMPLATE_BUILD_STATUS_VALUES: set[TemplateBuildStatus] = {
+    "building",
+    "error",
+    "ready",
+    "waiting",
+}
+
+
+def check_template_build_status(value: str) -> TemplateBuildStatus:
+    if value in TEMPLATE_BUILD_STATUS_VALUES:
+        return cast(TemplateBuildStatus, value)
+    raise TypeError(f"Unexpected value {value!r}. Expected one of {TEMPLATE_BUILD_STATUS_VALUES!r}")
