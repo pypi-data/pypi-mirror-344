580571bd48c864c9ccb2e1c67bffb455784253c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 08:05:46 +0000

    Add package runrl/0.1.1/runrl-0.1.1.tar.gz

---
+++ packages/runrl/runrl-0.1.1.tar.gz/runrl-0.1.1/runrl/__init__.py
@@ -0,0 +1,11 @@
+def grpo(model_name: str) -> str:
+    """
+    Interface function for RunRL's GRPO implementation.
+    
+    Args:
+        model_name (str): Name of the model to use
+        
+    Returns:
+        str: Information about the web interface
+    """
+    return "Check out our web interface at https://runrl.com" 
