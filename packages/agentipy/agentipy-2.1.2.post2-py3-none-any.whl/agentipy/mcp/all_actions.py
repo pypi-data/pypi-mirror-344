e52859519b3f1e167787f87961fc7d9356107746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:39 +0000

    Add package agentipy/2.1.2.post2/agentipy-2.1.2.post2-py3-none-any.whl

---
+++ packages/agentipy/agentipy-2.1.2.post2-py3-none-any.whl/agentipy/mcp/all_actions.py
@@ -0,0 +1,19 @@
+from agentipy.mcp.allora import ALLORA_ACTIONS
+from agentipy.mcp.coingecko import COINGECKO_ACTIONS
+from agentipy.mcp.core import CORE_ACTIONS
+from agentipy.mcp.debridge import DEBRIDGE_ACTIONS
+from agentipy.mcp.jito import JITO_ACTIONS
+from agentipy.mcp.jupiter import JUPITER_ACTIONS
+from agentipy.mcp.lulo import LULO_ACTIONS
+from agentipy.mcp.pyth import PYTH_ACTIONS
+
+ALL_ACTIONS = {
+    **CORE_ACTIONS,
+    **ALLORA_ACTIONS,
+    **JUPITER_ACTIONS,
+    **COINGECKO_ACTIONS,
+    **PYTH_ACTIONS,
+    **DEBRIDGE_ACTIONS,
+    **LULO_ACTIONS,
+    **JITO_ACTIONS
+}
