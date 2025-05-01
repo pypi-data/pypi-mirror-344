5dbb14600a43d5cb70ff8b2d69a46f3e9751bcd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:32 +0000

    Add package volbatch/0.0.2/volbatch-0.0.2-py3-none-any.whl

---
+++ packages/volbatch/volbatch-0.0.2-py3-none-any.whl/volbatch/vol_params.py
@@ -0,0 +1,477 @@
+vol_params = {
+    'divs': False,
+    'skew_tenors': 24,
+    'save': False,
+    'interest_rate': 0.0454,
+    'timeout_seconds': 120,
+    'discount_type': 'smooth',
+    'tickerMap':{
+        'AAPL':
+            {
+            'ticker': 'AAPL',
+            'longName': 'Apple Inc.',
+            'divYield':	0.0042
+            },
+        'AI':
+            {
+            'ticker': 'AI',
+            'longName': 'C3.ai, Inc.',
+            'divYield':	0.0
+            },
+        'AMD':
+            {
+            'ticker': 'AMD',
+            'longName': 'Advanced Micro Devices, Inc.',
+            'divYield':	0.0
+            },
+        'AMZN':
+            {
+            'ticker': 'AMZN',
+            'longName': 'Amazon.com, Inc.',
+            'divYield':	0.0
+            },
+        'AVGO':
+            {
+            'ticker': 'AVGO',
+            'longName': 'Broadcom Inc.',
+            'divYield':	0.0139
+            },
+        'BA':
+            {
+            'ticker': 'BA',
+            'longName': 'The Boeing Company',
+            'divYield':	0.0
+            },
+        'BABA':
+            {
+            'ticker': 'BABA',
+            'longName': 'Alibaba Group Holding Limited',
+            'divYield':	0.0124
+            },
+        'BAC':
+            {
+            'ticker': 'BAC',
+            'longName': 'Bank of America Corporation',
+            'divYield':	0.0231
+            },
+         'BITO':
+           {
+             'ticker': 'BITO',
+             'longName': 'ProShares Bitcoin ETF',
+             'divYield':	0.6087
+           },
+        'C':
+            {
+            'ticker': 'C',
+            'longName': 'Citigroup Inc.',
+            'divYield':	0.0314
+            },
+        'CLSK':
+            {
+            'ticker': 'CLSK',
+            'longName': 'CleanSpark, Inc.',
+            'divYield':	0.0
+            },
+        'COIN':
+            {
+            'ticker': 'COIN',
+            'longName': 'Coinbase Global, Inc.',
+            'divYield':	0.0
+            },
+        'DIA':
+            {
+            'ticker': 'DIA',
+            'longName': 'SPDR Dow Jones Industrial Average ETF Trust',
+            'divYield':	0.0201
+            },
+        'DJT':
+            {
+            'ticker': 'DJT',
+            'longName': 'Trump Media & Technology Group Corp.',
+            'divYield':	0.0
+            },
+        'EEM':
+            {
+            'ticker': 'EEM',
+            'longName': 'iShares MSCI Emerging Markets ETF',
+            'divYield':	0.0248
+            },
+        'ET':
+            {
+            'ticker': 'ET',
+            'longName': 'Energy Transfer LP',
+            'divYield':	0.0665
+            },
+        'EWZ':
+            {
+            'ticker': 'EWZ',
+            'longName': 'iShares MSCI Brazil ETF',
+            'divYield':	0.0884
+            },
+        # 'F':
+        #     {
+        #     'ticker': 'F',
+        #     'longName': 'Ford Motor Company',
+        #     'divYield':	0.0859
+        #     },
+        'FXI':
+            {
+            'ticker': 'FXI',
+            'longName': 'iShares China Large-Cap ETF',
+            'divYield':	0.0187
+            },
+        'GDX':
+            {
+            'ticker': 'GDX',
+            'longName': 'VanEck Gold Miners ETF',
+            'divYield':	0.0112
+            },
+        'GLD':
+            {
+            'ticker': 'GLD',
+            'longName': 'SPDR Gold Shares',
+            'divYield':	0.0
+            },
+        'GME':
+            {
+            'ticker': 'GME',
+            'longName': 'GameStop Corp.',
+            'divYield':	0.0
+            },
+        'GOLD':
+            {
+            'ticker': 'GOLD',
+            'longName': 'Barrick Gold Corporation',
+            'divYield':	0.0226
+            },
+        'GOOGL':
+            {
+            'ticker': 'GOOGL',
+            'longName': 'Alphabet Inc.',
+            'divYield':	0.0042
+            },
+        'HOOD':
+            {
+            'ticker': 'HOOD',
+            'longName': 'Robinhood Markets, Inc.',
+            'divYield':	0.0
+            },
+        'HYG':
+            {
+            'ticker': 'HYG',
+            'longName': 'iShares iBoxx $ High Yield Corporate Bond ETF',
+            'divYield':	0.065
+            },
+        'INTC':
+            {
+            'ticker': 'INTC',
+            'longName': 'Intel Corporation',
+            'divYield':	0.0
+            },
+        'IWM':
+            {
+            'ticker': 'IWM',
+            'longName': 'iShares Russell 2000 ETF',
+            'divYield':	0.0117
+            },
+        'KRE':
+            {
+            'ticker': 'KRE',
+            'longName': 'SPDR S&P Regional Banking ETF',
+            'divYield':	0.0268
+            },
+        'MARA':
+            {
+            'ticker': 'MARA',
+            'longName': 'MARA Holdings, Inc.',
+            'divYield':	0.0
+            },
+        'META':
+            {
+            'ticker': 'META',
+            'longName': 'Meta Platforms, Inc.',
+            'divYield':	0.0032
+            },
+        'MSFT':
+            {
+            'ticker': 'MSFT',
+            'longName': 'Microsoft Corporation',
+            'divYield':	0.0079
+            },
+        'MSTR':
+            {
+            'ticker': 'MSTR',
+            'longName': 'MicroStrategy Incorporated',
+            'divYield':	0.0
+            },
+        'MU':
+            {
+            'ticker': 'MU',
+            'longName': 'Micron Technology, Inc.',
+            'divYield':	0.0071
+            },
+        # 'NDX':
+        #     {
+        #     'ticker': '^NDX',
+        #     'longName': 'Nasdaq 100',
+        #     'divYield':	0.0098
+        #     },
+        'NFLX':
+            {
+            'ticker': 'NFLX',
+            'longName': 'Netflix, Inc.',
+            'divYield':	0.0
+            },
+        'NIO':
+            {
+            'ticker': 'NIO',
+            'longName': 'NIO Inc.',
+            'divYield':	0.0
+            },
+        'NVDA':
+            {
+            'ticker': 'NVDA',
+            'longName': 'NVIDIA Corporation',
+            'divYield':	0.0003
+            },
+        'PFE':
+            {
+            'ticker': 'PFE',
+            'longName': 'Pfizer Inc.',
+            'divYield':	0.0644
+            },
+        'PLTR':
+            {
+            'ticker': 'PLTR',
+            'longName': 'Palantir Technologies Inc.',
+            'divYield':	0.0
+            },
+        'QQQ':
+            {
+            'ticker': 'QQQ',
+            'longName': 'Invesco QQQ Trust',
+            'divYield':	0.0056
+            },
+        'SLV':
+            {
+            'ticker': 'SLV',
+            'longName': 'iShares Silver Trust',
+            'divYield':	0.0
+            },
+        'SMCI':
+            {
+            'ticker': 'SMCI',
+            'longName': 'Super Micro Computer, Inc.',
+            'divYield':	0.0
+            },
+        'SNOW':
+            {
+            'ticker': 'SNOW',
+            'longName': 'Snowflake Inc.',
+            'divYield':	0.0
+            },
+        'SOFI':
+            {
+            'ticker': 'SOFI',
+            'longName': 'SoFi Technologies, Inc.',
+            'divYield':	0.0
+            },
+        'SOUN':
+            {
+            'ticker': 'SOUN',
+            'longName': 'SoundHound AI, Inc.',
+            'divYield':	0.0
+            },
+        'SOXL':
+            {
+            'ticker': 'SOXL',
+            'longName': 'Direxion Daily Semiconductor Bull 3x Shares',
+            'divYield':	0.0114
+            },
+        'SPX':
+            {
+            'ticker': '^SPX',
+            'longName': 'S&P 500 Index',
+            'divYield':	0.0122
+            },
+        'SPY':
+            {
+            'ticker': 'SPY',
+            'longName': 'SPDR S&P 500 ETF Trust',
+            'divYield':	0.0122
+            },
+        'TGT':
+            {
+            'ticker': 'TGT',
+            'longName': 'Target Corporation',
+            'divYield':	0.0316
+            },
+        'T':
+            {
+            'ticker': 'T',
+            'longName': 'AT&T Inc.',
+            'divYield':	0.0417
+            },
+        'TLT':
+            {
+            'ticker': 'TLT',
+            'longName': 'iShares 20+ Year Treasury Bond ETF',
+            'divYield':	0.0439
+            },
+        'TQQQ':
+            {
+            'ticker': 'TQQQ',
+            'longName': 'ProShares UltraPro QQQ',
+            'divYield':	0.0131
+            },
+        'TSLA':
+            {
+            'ticker': 'TSLA',
+            'longName': 'Tesla, Inc.',
+            'divYield':	0.0
+            },
+        'TSLL':
+            {
+            'ticker': 'TSLL',
+            'longName': 'Direxion Daily TSLA Bull 2X Shares',
+            'divYield':	0.0263
+            },
+        'UBER':
+            {
+            'ticker': 'UBER',
+            'longName': 'Uber Technologies, Inc.',
+            'divYield':	0.0
+            },
+        'UVXY':
+            {
+            'ticker': 'UVXY',
+            'longName': 'ProShares Ultra VIX Short-Term Futures ETF',
+            'divYield':	0.0
+            },
+        'VALE':
+            {
+            'ticker': 'VALE',
+            'longName': 'Vale S.A.',
+            'divYield':	0.1518
+            },
+        'VZ':
+            {
+            'ticker': 'VZ',
+            'longName': 'Verizon Communications Inc.',
+            'divYield':	0.063
+            },
+        'WMT':
+            {
+            'ticker': 'WMT',
+            'longName': 'Walmart Inc.',
+            'divYield':	0.0089
+            },
+        # 'WOLF':
+        #     {
+        #     'ticker': 'WOLF',
+        #     'longName': 'Wolfspeed, Inc.',
+        #     'divYield':	0.0
+        #     },
+        'XBI':
+            {
+            'ticker': 'XBI',
+            'longName': 'SPDR S&P BIOTECH ETF',
+            'divYield':	0.0015
+            },
+        'XLB':
+            {
+            'ticker': 'XLB',
+            'longName': 'Materials Select Sector SPDR Fund',
+            'divYield':	0.0194
+            },
+        'XLC':
+            {
+            'ticker': 'XLC',
+            'longName': 'Communication Services Select Sector SPDR Fund',
+            'divYield':	0.01
+            },
+        'XLE':
+            {
+            'ticker': 'XLE',
+            'longName': 'Energy Select Sector SPDR Fund',
+            'divYield':	0.0325
+            },
+        'XLF':
+            {
+            'ticker': 'XLF',
+            'longName': 'Financial Select Sector SPDR Fund',
+            'divYield':	0.0145
+            },
+        'XLI':
+            {
+            'ticker': 'XLI',
+            'longName': 'Industrial Select Sector SPDR Fund',
+            'divYield':	0.0145
+            },
+        'XLK':
+            {
+            'ticker': 'XLK',
+            'longName': 'Technology Select Sector SPDR Fund',
+            'divYield':	0.0067
+            },
+        'XLP':
+            {
+            'ticker': 'XLP',
+            'longName': 'Consumer Staples Select Sector SPDR Fund',
+            'divYield':	0.0285
+            },
+        'XLRE':
+            {
+            'ticker': 'XLRE',
+            'longName': 'Real Estate Select Sector SPDR Fund',
+            'divYield':	0.0356
+            },
+        'XLU':
+            {
+            'ticker': 'XLU',
+            'longName': 'Utilities Select Sector SPDR Fund',
+            'divYield':	0.0297
+            },
+        'XLV':
+            {
+            'ticker': 'XLV',
+            'longName': 'Health Care Select Sector SPDR Fund',
+            'divYield':	0.0164
+            },
+        'XLY':
+            {
+            'ticker': 'XLY',
+            'longName': 'Consumer Discretionary Select Sector SPDR Fund',
+            'divYield':	0.0073
+            },
+        'XOM':
+            {
+            'ticker': 'XOM',
+            'longName': 'Exxon Mobil Corporation',
+            'divYield':	0.038
+            }
+    },
+    'USER_AGENTS': [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 "
+        "Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 '
+        'Safari/537.36'
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 "
+        "Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 "
+        "Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+    ],
+    'request_headers': {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "origin": "https://finance.yahoo.com",
+        "referer": "https://finance.yahoo.com",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+    }
+}
