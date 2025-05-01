744deabeb724904b265b962090626fb330fb6008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:32 +0000

    Add package qase-python-commons/3.4.2/qase_python_commons-3.4.2.tar.gz

---
+++ packages/qase-python-commons/qase_python_commons-3.4.2.tar.gz/qase_python_commons-3.4.2/src/qase/commons/models/runtime.py
@@ -0,0 +1,48 @@
+from .step import Step
+from .attachment import Attachment
+
+
+class QaseRuntimeException(Exception):
+    pass
+
+
+class Runtime:
+    def __init__(self):
+        self.result = None
+        self.steps = {}
+        self.step_id = None
+
+    def add_step(self, step: Step):
+        try:
+            if self.step_id:
+                step.set_parent_id(self.step_id)
+
+            self.steps[step.id] = step
+            self.step_id = step.id
+        except Exception as e:
+            raise QaseRuntimeException(e)
+
+    def finish_step(self, id: str, status: str, data=None):
+        try:
+            self.steps[id].execution.set_status(status)
+            if data:
+                self.steps[id].set_data(data)
+
+            self.steps[id].execution.complete()
+            self.step_id = self.steps[id].parent_id
+        except Exception as e:
+            raise QaseRuntimeException(e)
+
+    def add_attachment(self, attachment: Attachment):
+        try:
+            if self.step_id:
+                self.steps[self.step_id].add_attachment(attachment)
+            else:
+                self.result.add_attachment(attachment)
+        except Exception as e:
+            raise QaseRuntimeException(e)
+
+    def clear(self):
+        self.result = None
+        self.steps = {}
+        self.step_id = None
