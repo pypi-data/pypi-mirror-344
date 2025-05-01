9d3932a48c5a2d2d2767ecd6ebc3cb43fdcae5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 14:06:53 +0000

    Add package bibliothekarios/0.0.2/bibliothekarios-0.0.2-py3-none-any.whl

---
+++ packages/bibliothekarios/bibliothekarios-0.0.2-py3-none-any.whl/bibliothekarios/entities.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+import json
+import datetime
+
+
+class Bibliothekarios():
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+
+if __name__ == "__main__":
+    print('you launched main')
