c2c12c1900b0a35c7dc73efbc1301e7ff5f4664b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:06 +0000

    Add package ryo/0.1.1/ryo-0.1.1.tar.gz

---
+++ packages/ryo/ryo-0.1.1.tar.gz/ryo-0.1.1/ryo/src/cli.py
@@ -0,0 +1,27 @@
+import yaml
+import os
+import argparse
+from ryo.src.utils import get_task
+from ryo.src.runner import run_steps
+
+def main():
+    parser = argparse.ArgumentParser(description="Um utilitário de linha de comando Ryo.")
+    subparsers = parser.add_subparsers(dest="comando", help="Subcomandos disponíveis")
+
+    task_parser = subparsers.add_parser("task", help="Acompanha a tarefa")
+    task_parser.add_argument("nome", help="O nome da task a ser implementada")
+
+    args = parser.parse_args()
+
+    if args.comando == "task":
+        task = get_task(args.nome)
+        if task:
+            run_steps(task, os.getcwd())
+        else:
+            print(f"Task '{args.nome}' não encontrada.")
+        
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
