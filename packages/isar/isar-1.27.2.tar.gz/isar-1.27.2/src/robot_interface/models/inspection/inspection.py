1808f7793650cb06bbab90befc1b42cbe8685430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:26:07 +0000

    Add package isar/1.27.2/isar-1.27.2.tar.gz

---
+++ packages/isar/isar-1.27.2.tar.gz/isar-1.27.2/src/robot_interface/models/inspection/inspection.py
@@ -0,0 +1,105 @@
+from abc import ABC
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional, Type
+
+from alitra import Pose
+from pydantic import BaseModel, Field
+
+
+@dataclass
+class InspectionMetadata(ABC):
+    start_time: datetime
+    pose: Pose
+    file_type: str
+    analysis_type: Optional[str] = field(default=None, init=False)
+    tag_id: Optional[str] = field(default=None, init=False)
+    additional: Optional[dict] = field(default_factory=dict, init=False)
+
+
+@dataclass
+class ImageMetadata(InspectionMetadata):
+    pass
+
+
+@dataclass
+class ThermalImageMetadata(InspectionMetadata):
+    pass
+
+
+@dataclass
+class VideoMetadata(InspectionMetadata):
+    duration: Optional[float] = field(default=None)
+
+
+@dataclass
+class ThermalVideoMetadata(InspectionMetadata):
+    duration: Optional[float] = field(default=None)
+
+
+@dataclass
+class AudioMetadata(InspectionMetadata):
+    duration: Optional[float] = field(default=None)
+
+
+@dataclass
+class GasMeasurementMetadata(InspectionMetadata):
+    pass
+
+
+class Inspection(BaseModel):
+    metadata: InspectionMetadata
+    id: str = Field(frozen=True)
+    data: Optional[bytes] = Field(default=None, frozen=True)
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return InspectionMetadata
+
+
+class Image(Inspection):
+    metadata: ImageMetadata
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return ImageMetadata
+
+
+class ThermalImage(Inspection):
+    metadata: ThermalImageMetadata
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return ThermalImageMetadata
+
+
+class Video(Inspection):
+    metadata: VideoMetadata
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return VideoMetadata
+
+
+class ThermalVideo(Inspection):
+    metadata: ThermalVideoMetadata
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return ThermalVideoMetadata
+
+
+class Audio(Inspection):
+    metadata: AudioMetadata
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return AudioMetadata
+
+
+class GasMeasurement(Inspection):
+    metadata: GasMeasurementMetadata
+
+    @staticmethod
+    def get_metadata_type() -> Type[InspectionMetadata]:
+        return GasMeasurementMetadata
