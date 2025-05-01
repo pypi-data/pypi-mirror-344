3c869b5497b3bb487ed4a79e7dce72fbf36fdb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:04:57 +0000

    Add package flexhub/0.1.0/flexhub-0.1.0-py3-none-any.whl

---
+++ packages/flexhub/flexhub-0.1.0-py3-none-any.whl/flexhub/__init__.py
@@ -0,0 +1,6 @@
+"""
+Flexhub is a tool for handling streaming to eventhub with minimal bottlenecks
+"""
+
+from flexhub import send_to_eventhub, BatchHandler  # noqa
+from flexhub._version import __version__  # noqa
