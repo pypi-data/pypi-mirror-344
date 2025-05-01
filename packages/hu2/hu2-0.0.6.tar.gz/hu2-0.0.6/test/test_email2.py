73056a7396505631187834937df1d6201b009e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:25 +0000

    Add package hu2/0.0.6/hu2-0.0.6.tar.gz

---
+++ packages/hu2/hu2-0.0.6.tar.gz/hu2-0.0.6/test/test_email2.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+from hu2.excel2 import ExcelTable, ExcelStyle, COLOR_GREEN, COLOR_DARKGREEN, COLOR_DARKBLUE
+
+
+class TestEmail(unittest.TestCase):
+    def test_send_email(self):
+
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
