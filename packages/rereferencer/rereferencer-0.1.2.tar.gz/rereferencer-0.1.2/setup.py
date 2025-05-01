bc6e45cf7c687e27583a163acc35912bf288d017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 08:05:51 +0000

    Add package rereferencer/0.1.2/rereferencer-0.1.2.tar.gz

---
+++ packages/rereferencer/rereferencer-0.1.2.tar.gz/rereferencer-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rereferencer",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "click>=8.0.0",
+        "biopython>=1.81",
+        "pandas>=2.0.0",
+        "rich>=13.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "rereferencer=rereferencer.cli:main",
+        ],
+    },
+    author="SemiQuant",
+    author_email="JasonLimberis@ucsf.edu",
+    description="A tool to extract amplified regions from reference sequences using primer pairs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SemiQuant/rereferencer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+) 
