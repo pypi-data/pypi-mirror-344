a12ddfc975fb4a0eac25030b4296467788c62da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:17 +0000

    Add package ncatbot/3.8.1.post1/ncatbot-3.8.1.post1.tar.gz

---
+++ packages/ncatbot/ncatbot-3.8.1.post1.tar.gz/ncatbot-3.8.1.post1/src/ncatbot/utils/__init__.py
@@ -0,0 +1,76 @@
+from ncatbot.utils.assets import *
+from ncatbot.utils.config import config
+from ncatbot.utils.file_io import (
+    UniversalLoader,
+    convert_uploadable_object,
+    read_file,
+    unzip_file,
+)
+from ncatbot.utils.function_enhance import (
+    add_sync_methods,
+    report,
+    run_func_async,
+    run_func_sync,
+    to_async,
+    to_sync,
+)
+from ncatbot.utils.logger import get_log
+from ncatbot.utils.network_io import download_file, get_proxy_url
+from ncatbot.utils.optional import *
+
+__all__ = [
+    "SetConfig",
+    "get_log",
+    "get_proxy_url",
+    "download_file",
+    "UniversalLoader",
+    "read_file",
+    "convert_uploadable_object",
+    "unzip_file",
+    "config",
+    "report",
+    "to_sync",
+    "to_async",
+    "run_func_sync",
+    "run_func_async",
+    "add_sync_methods",
+    # literals
+    "NAPCAT_WEBUI_SALT",
+    "WINDOWS_NAPCAT_DIR",
+    "LINUX_NAPCAT_DIR",
+    "INSTALL_SCRIPT_URL",
+    "NAPCAT_CLI_URL",
+    "PYPI_URL",
+    "NAPCAT_CLI_PATH",
+    "REQUEST_SUCCESS",
+    "OFFICIAL_GROUP_MESSAGE_EVENT",
+    "OFFICIAL_PRIVATE_MESSAGE_EVENT",
+    "OFFICIAL_REQUEST_EVENT",
+    "OFFICIAL_NOTICE_EVENT",
+    "OFFICIAL_STARTUP_EVENT",
+    "PLUGIN_BROKEN_MARK",
+    "STATUS_ONLINE",
+    "STATUS_Q_ME",
+    "STATUS_LEAVE",
+    "STATUS_BUSY",
+    "STATUS_DND",
+    "STATUS_HIDDEN",
+    "STATUS_LISTENING",
+    "STATUS_LOVE_YOU",
+    "STATUS_LEARNING",
+    "Status",
+    "PermissionGroup",
+    "DefaultPermission",
+    "EVENT_QUEUE_MAX_SIZE",
+    "PLUGINS_DIR",
+    "META_CONFIG_PATH",
+    "PERSISTENT_DIR",
+    # custom errors
+    "PluginCircularDependencyError",
+    "PluginNotFoundError",
+    "PluginLoadError",
+    "PluginDependencyError",
+    "PluginVersionError",
+    "PluginUnloadError",
+    "InvalidPluginStateError",
+]
---
+++ packages/ncatbot/ncatbot-3.8.1.post1.tar.gz/ncatbot-3.8.1.post1/src/ncatbot/utils/assets/__init__.py
@@ -0,0 +1,53 @@
+# 静态资源
+
+
+from ncatbot.utils.assets.color import Color
+from ncatbot.utils.assets.literals import *
+from ncatbot.utils.assets.plugin_custom_err import *
+
+__all__ = [
+    "PluginCircularDependencyError",
+    "PluginDependencyError",
+    "PluginNotFoundError",
+    "PluginVersionError",
+    "Color",
+    # literals
+    "NAPCAT_WEBUI_SALT",
+    "WINDOWS_NAPCAT_DIR",
+    "LINUX_NAPCAT_DIR",
+    "INSTALL_SCRIPT_URL",
+    "NAPCAT_CLI_URL",
+    "PYPI_URL",
+    "NAPCAT_CLI_PATH",
+    "REQUEST_SUCCESS",
+    "OFFICIAL_GROUP_MESSAGE_EVENT",
+    "OFFICIAL_PRIVATE_MESSAGE_EVENT",
+    "OFFICIAL_REQUEST_EVENT",
+    "OFFICIAL_NOTICE_EVENT",
+    "OFFICIAL_STARTUP_EVENT",
+    "PLUGIN_BROKEN_MARK",
+    "STATUS_ONLINE",
+    "STATUS_Q_ME",
+    "STATUS_LEAVE",
+    "STATUS_BUSY",
+    "STATUS_DND",
+    "STATUS_HIDDEN",
+    "STATUS_LISTENING",
+    "STATUS_LOVE_YOU",
+    "STATUS_LEARNING",
+    "Status",
+    "PermissionGroup",
+    "DefaultPermission",
+    "EVENT_QUEUE_MAX_SIZE",
+    "PLUGINS_DIR",
+    "META_CONFIG_PATH",
+    "PERSISTENT_DIR",
+    # custom errors
+    "PluginCircularDependencyError",
+    "PluginNotFoundError",
+    "PluginLoadError",
+    "PluginDependencyError",
+    "PluginVersionError",
+    "PluginUnloadError",
+    "InvalidPluginStateError",
+]
