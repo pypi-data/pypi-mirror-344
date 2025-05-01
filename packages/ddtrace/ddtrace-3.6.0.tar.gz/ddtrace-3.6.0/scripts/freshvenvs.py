8af789dfa8e94c4e59dae28887f36d9f7507b8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 06:06:53 +0000

    Add package ddtrace/3.6.0/ddtrace-3.6.0.tar.gz

---
+++ packages/ddtrace/ddtrace-3.6.0.tar.gz/ddtrace-3.6.0/scripts/freshvenvs.py
@@ -0,0 +1,313 @@
+import argparse
+from collections import defaultdict
+import datetime as dt
+from http.client import HTTPSConnection
+from io import StringIO
+import json
+from operator import itemgetter
+import os
+import pathlib
+import sys
+import typing
+from typing import Optional
+
+from packaging.version import Version
+from pip import _internal
+
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
+import riotfile  # noqa: E402
+
+
+CONTRIB_ROOT = pathlib.Path("ddtrace/contrib/internal")
+LATEST = ""
+
+excluded = {"coverage"}
+
+# map module => lockfile dependency
+module_dependency_mapping = {
+    "kafka": "confluent-kafka",
+    "consul": "python-consul",
+    "snowflake": "snowflake-connector-python",
+    "flask_cache": "flask-caching",
+    "graphql": "graphql-core",
+    "mysql": "mysql-connector-python",
+    "mysqldb": "mysqlclient",
+    "asyncio": "pytest-asyncio",
+    "sqlite3": "pysqlite3-binary",
+    "grpc": "grpcio",
+    "google_generativeai": "google-generativeai",
+    "psycopg2": "psycopg2-binary",
+    "cassandra": "cassandra-driver",
+    "rediscluster": "redis-py-cluster",
+    "dogpile_cache": "dogpile-cache",
+    "vertica": "vertica-python",
+    "aiohttp_jinja2": "aiohttp-jinja2",
+    "azure_functions": "azure-functions",
+    "pytest_bdd": "pytest-bdd",
+    "aws_lambda": "datadog-lambda",
+}
+
+dependency_module_mapping = {v: k for k, v in module_dependency_mapping.items()}
+
+supported_versions = []
+pinned_packages = set()
+
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        self._stderr = sys.stderr
+        sys.stdout = self._stringio = StringIO()
+        sys.stderr = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio  # free up some memory
+        sys.stdout = self._stdout
+        sys.stderr = self._stderr
+
+def parse_args():
+    """
+    usage: python scripts/freshvenvs.py <output> OR <generate>
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", choices=["output", "generate"], help="mode: output or generate")
+    return parser.parse_args()
+
+def _get_integrated_modules() -> typing.Set[str]:
+    """Get all modules that have contribs implemented for them"""
+    all_required_modules = set()
+    for item in CONTRIB_ROOT.iterdir():
+        if not os.path.isdir(item):
+            continue
+
+        patch_filepath = item / "patch.py"
+
+        if os.path.isfile(patch_filepath):
+            module_name = item.name
+            all_required_modules.add(module_name)
+
+
+    return all_required_modules
+
+
+def _get_riot_envs_including_any(modules: typing.Set[str]) -> typing.Set[str]:
+    """Return the set of riot env hashes where each env uses at least one of the given modules"""
+    envs = set()
+    for item in os.listdir(".riot/requirements"):
+        if item.endswith(".txt"):
+            with open(f".riot/requirements/{item}", "r") as lockfile:
+                lockfile_content = lockfile.read()
+                for module in modules:
+                    if module in lockfile_content or (
+                        module in module_dependency_mapping and module_dependency_mapping[module] in lockfile_content
+                    ):
+                        envs |= {item.split(".")[0]}
+                        break
+    return envs
+
+
+def _get_updatable_packages_implementing(modules: typing.Set[str]) -> typing.Set[str]:
+    """Return all packages have contribs implemented for them"""
+    all_venvs = riotfile.venv.venvs
+
+    for v in all_venvs:
+        package = v.name
+        if package not in modules:
+            continue
+        if not _venv_sets_latest_for_package(v, package):
+            pinned_packages.add(package)
+
+    packages = {m for m in modules if "." not in m and m not in pinned_packages}
+    return packages
+
+
+def _get_all_modules(modules: typing.Set[str]) -> typing.Set[str]:
+    """Return all packages have contribs implemented for them"""
+    contrib_modules = {m for m in modules if "." not in m}
+    return contrib_modules
+
+
+def _get_version_extremes(package_name: str) -> typing.Tuple[Optional[str], Optional[str]]:
+    """Return the (earliest, latest) supported versions of a given package"""
+    with Capturing() as output:
+        _internal.main(["index", "versions", package_name])
+    if not output:
+        return (None, None)
+    version_list = [a for a in output if "available versions" in a.lower()][0]
+    output_parts = version_list.split()
+    versions = [p.strip(",") for p in output_parts[2:]]
+    earliest_within_window = versions[-1]
+
+    conn = HTTPSConnection("pypi.org", 443)
+    conn.request("GET", f"pypi/{package_name}/json")
+    response = conn.getresponse()
+
+    if response.status != 200:
+        raise ValueError(f"Failed to connect to PyPI: HTTP {response.status}")
+
+    version_infos = json.loads(response.readlines()[0])["releases"]
+    for version in versions:
+        version_info = version_infos.get(version, [])
+        if not version_info:
+            continue
+
+        upload_timestamp = version_info[0].get("upload_time_iso_8601")
+        upload_time = dt.datetime.strptime(upload_timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+        upload_time = upload_time.replace(tzinfo=dt.timezone.utc)
+        current_time = dt.datetime.now(dt.timezone.utc)
+        version_age = current_time - upload_time
+        if version_age > dt.timedelta(days=365 * 2):
+            earliest_within_window = version
+            break
+    return earliest_within_window, versions[0]
+
+
+def _get_package_versions_from(env: str, packages: typing.Set[str]) -> typing.List[typing.Tuple[str, str]]:
+    """Return the list of package versions that are tested, related to the modules"""
+    lockfile_content = pathlib.Path(f".riot/requirements/{env}.txt").read_text().splitlines()
+    lock_packages = []
+    for line in lockfile_content:
+        package, _, versions = line.partition("==")
+        if package in packages:
+            lock_packages.append((package, versions))
+        elif package in dependency_module_mapping and dependency_module_mapping[package] in packages:
+            lock_packages.append((dependency_module_mapping[package], versions))
+
+    return lock_packages
+
+
+def _is_module_autoinstrumented(module: str) -> bool:
+    import importlib
+
+    _monkey = importlib.import_module("ddtrace._monkey")
+    PATCH_MODULES = getattr(_monkey, "PATCH_MODULES")
+
+    return module in PATCH_MODULES and PATCH_MODULES[module]
+
+def _versions_fully_cover_bounds(bounds: typing.Tuple[str, str], versions: typing.List[str]) -> bool:
+    """Return whether the tested versions cover the upper bound range of supported versions"""
+    if not versions:
+        return False
+    _, upper_bound = bounds
+    return versions[0] >= Version(upper_bound)
+
+
+def _venv_sets_latest_for_package(venv: riotfile.Venv, suite_name: str) -> bool:
+    """
+    Returns whether the Venv for the package uses `latest` or not.
+    DFS traverse through the Venv, as it may have nested Venvs.
+
+    If the module name is in module_dependency_mapping, remap it.
+    """
+    package = module_dependency_mapping.get(suite_name, suite_name)
+
+    if package in venv.pkgs:
+        if LATEST in venv.pkgs[package]:
+            return True
+
+    if venv.venvs:
+        for child_venv in venv.venvs:
+            if _venv_sets_latest_for_package(child_venv, package):
+                return True
+
+    return False
+
+
+def _get_all_used_versions(envs, packages) -> dict:
+    """
+    Returns dict(module, set(versions)) for a venv, as defined from riot lockfiles.
+    """
+    all_used_versions = defaultdict(set)
+    for env in envs:
+        versions_used = _get_package_versions_from(env, packages)
+        for package, version in versions_used:
+            all_used_versions[package].add(version)
+    return all_used_versions
+
+
+def _get_version_bounds(packages) -> dict:
+    """
+    Return dict(module: (earliest, latest)) of the module from PyPI
+    """
+    bounds = dict()
+    for package in packages:
+        earliest, latest = _get_version_extremes(package)
+        bounds[package] = (earliest, latest)
+    return bounds
+
+def output_outdated_packages(all_required_packages, envs, bounds):
+    """
+    Output a list of package names that can be updated.
+    """
+    outdated_packages = []
+
+    for package in all_required_packages:
+        earliest, latest = _get_version_extremes(package)
+        bounds[package] = (earliest, latest)
+
+    all_used_versions = defaultdict(set)
+    for env in envs:
+        versions_used = _get_package_versions_from(env, all_required_packages)
+        for pkg, version in versions_used:
+            all_used_versions[pkg].add(version)
+
+
+    for package in all_required_packages:
+        ordered = sorted([Version(v) for v in all_used_versions[package]], reverse=True)
+        if not ordered:
+            continue
+        if package not in bounds or bounds[package] == (None, None):
+            continue
+        if not _versions_fully_cover_bounds(bounds[package], ordered):
+            outdated_packages.append(package)
+
+    print(" ".join(outdated_packages))
+
+
+def generate_supported_versions(contrib_packages, all_used_versions):
+    """
+    Generate supported versions JSON
+    """
+    patched = {}
+    for package in contrib_packages:
+        ordered = sorted([Version(v) for v in all_used_versions[package]], reverse=True)
+        if not ordered:
+            continue
+        json_format = {
+            "integration": package,
+            "minimum_tracer_supported": str(ordered[-1]),
+            "max_tracer_supported": str(ordered[0]),
+        }
+
+        if package in pinned_packages:
+            json_format["pinned"] = "true"
+
+        if package not in patched:
+            patched[package] = _is_module_autoinstrumented(package)
+        json_format["auto-instrumented"] = patched[package]
+        supported_versions.append(json_format)
+
+    supported_versions_output = sorted(supported_versions, key=itemgetter("integration"))
+    with open("supported_versions_output.json", "w") as file:
+        json.dump(supported_versions_output, file, indent=4)
+
+def main():
+    args = parse_args()
+    all_required_modules = _get_integrated_modules()
+    all_required_packages = _get_updatable_packages_implementing(all_required_modules)  # MODULE names
+    envs = _get_riot_envs_including_any(all_required_modules)
+    contrib_packages = _get_all_modules(all_required_modules)
+
+    if args.mode == "output":
+        bounds = _get_version_bounds(contrib_packages)
+        output_outdated_packages(all_required_packages, envs, bounds)
+    elif args.mode == "generate":
+        all_used_versions = _get_all_used_versions(envs, contrib_packages)
+        generate_supported_versions(contrib_packages, all_used_versions)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ddtrace/ddtrace-3.6.0.tar.gz/ddtrace-3.6.0/tests/appsec/integrations/pygoat_tests/test_pygoat.py
@@ -0,0 +1,155 @@
+import json
+import os
+import time
+
+import pytest
+import requests
+
+from tests.appsec.iast.conftest import iast_context_defaults
+
+
+span_defaults = iast_context_defaults  # So ruff does not remove it
+
+
+# Note: these tests require the testagent and pygoat images to be up from the docker-compose file
+# dc up -d pygoat testagent
+
+IMAGE_NAME = "pygoat:2.0.1"
+PYGOAT_URL = "http://0.0.0.0:8321"
+TESTAGENT_URL = os.getenv("DD_TRACE_AGENT_URL", "http://localhost:9126")
+TESTAGENT_TOKEN = "pygoat_test"
+TESTAGENT_HEADERS = {"X-Datadog-Test-Session-Token": TESTAGENT_TOKEN}
+TESTAGENT_TOKEN_PARAM = "?test_session_token=" + TESTAGENT_TOKEN
+
+
+@pytest.fixture(autouse=False)
+def client():
+    agent_client = requests.session()
+    url = TESTAGENT_URL + "/test/session/start" + TESTAGENT_TOKEN_PARAM
+    reply = agent_client.get(url, headers=TESTAGENT_HEADERS)
+
+    assert reply.status_code == 200, f"Status code: {reply.status_code}: {reply.text}"
+    pygoat_client, token, session_id = login_to_pygoat()
+
+    class RetClient:
+        agent_session = agent_client
+        pygoat_session = pygoat_client
+        sessionid = session_id
+        csrftoken = token
+
+    return RetClient
+
+
+def login_to_pygoat():
+    client = requests.session()
+    LOGIN_URL = PYGOAT_URL + "/login/"
+
+    client.get(LOGIN_URL)
+    csrftoken = client.cookies["csrftoken"]
+
+    login_data = {"username": "admin", "password": "adminpassword", "csrfmiddlewaretoken": csrftoken}
+    reply = client.post(LOGIN_URL, data=login_data, headers=TESTAGENT_HEADERS)
+
+    assert reply.status_code == 200
+    csrftoken = client.cookies["csrftoken"]
+    sessionid = client.cookies["sessionid"]
+    return client, csrftoken, sessionid
+
+
+def get_traces(agent_client: requests.Session) -> requests.Response:
+    return agent_client.get(TESTAGENT_URL + "/test/session/traces" + TESTAGENT_TOKEN_PARAM, headers=TESTAGENT_HEADERS)
+
+
+def vulnerability_in_traces(vuln_type: str, agent_client: requests.Session) -> bool:
+    time.sleep(5)
+    traces = get_traces(agent_client)
+    assert traces.status_code == 200, traces.text
+    traces_list = json.loads(traces.text)
+
+    class InnerBreakException(Exception):
+        pass
+
+    try:
+        for trace in traces_list:
+            for trace_dict in trace:
+                if "meta" not in trace_dict:
+                    continue
+                iast_data = trace_dict["meta"].get("_dd.iast.json")
+                if not iast_data:
+                    continue
+
+                vulnerabilities = json.loads(iast_data).get("vulnerabilities")
+                if not vulnerabilities:
+                    continue
+
+                for vuln in vulnerabilities:
+                    if vuln["type"] == vuln_type:
+                        raise InnerBreakException()
+    except InnerBreakException:
+        return True
+
+    return False
+
+
+def test_insecure_cookie(client):
+    payload = {"name": "My Name", "username": "user1", "pass": "testuser1", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(PYGOAT_URL + "/auth_lab/signup", data=payload, headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("INSECURE_COOKIE", client.agent_session)
+
+
+def test_nohttponly_cookie(client):
+    payload = {"name": "My Name2", "username": "user2", "pass": "testuser2", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(PYGOAT_URL + "/auth_lab/signup", data=payload, headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("NO_HTTPONLY_COOKIE", client.agent_session)
+
+
+def test_weak_random(client):
+    reply = client.pygoat_session.get(PYGOAT_URL + "/otp?email=test%40test.com", headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("WEAK_RANDOMNESS", client.agent_session)
+
+
+def test_weak_hash(client):
+    payload = {"username": "admin", "password": "adminpassword", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(
+        PYGOAT_URL + "/cryptographic_failure/lab", data=payload, headers=TESTAGENT_HEADERS
+    )
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("WEAK_HASH", client.agent_session)
+
+
+def test_cmdi(client):
+    payload = {"domain": "google.com && ls", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(PYGOAT_URL + "/cmd_lab", data=payload, headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("COMMAND_INJECTION", client.agent_session)
+
+
+def test_sqli(client):
+    payload = {"name": "admin", "pass": "anything' OR '1' ='1", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(PYGOAT_URL + "/sql_lab", data=payload, headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("SQL_INJECTION", client.agent_session)
+
+
+@pytest.mark.skip("TODO: SSRF is not implemented for open()")
+def test_ssrf1(client, iast_context_defaults):
+    payload = {"blog": "templates/Lab/ssrf/blogs/blog2.txt", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(PYGOAT_URL + "/ssrf_lab", data=payload, headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("SSRF", client.agent_session)
+
+
+def test_ssrf2(client, iast_context_defaults):
+    payload = {"url": "http://example.com", "csrfmiddlewaretoken": client.csrftoken}
+    reply = client.pygoat_session.post(PYGOAT_URL + "/ssrf_lab2", data=payload, headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("SSRF", client.agent_session)
+
+
+def test_xss(client):
+    reply = client.pygoat_session.get(PYGOAT_URL + '/xssL?q=<script>alert("XSS")</script>', headers=TESTAGENT_HEADERS)
+    assert reply.status_code == 200
+    assert vulnerability_in_traces("XSS", client.agent_session)
