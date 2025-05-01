22a7cd68df666b1f27a35f8bb666e55c83198e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:39 +0000

    Add package uiform/0.0.31/uiform-0.0.31-py3-none-any.whl

---
+++ packages/uiform/uiform-0.0.31-py3-none-any.whl/uiform/types/jobs/finetune.py
@@ -0,0 +1,6 @@
+# from pydantic import BaseModel
+# from .batch_annotation import AnnotationProps
+
+# class FineTuningInputData(BaseModel):
+#     dataset_id: str
+#     finetuning_props : FinetuningProps
