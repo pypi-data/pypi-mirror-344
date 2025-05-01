337a76727ea67e154da46bbe7bba7a8563544a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 14:06:44 +0000

    Add package graphfaker/0.1.1/graphfaker-0.1.1.tar.gz

---
+++ packages/graphfaker/graphfaker-0.1.1.tar.gz/graphfaker-0.1.1/graphfaker/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for graphfaker."""
+
+__author__ = """Dennis Irorere"""
+__email__ = 'denironyx@gmail.com'
+__version__ = '0.1.0'
+
+from .core import GraphFaker
+__all__ = ['GraphFaker']
