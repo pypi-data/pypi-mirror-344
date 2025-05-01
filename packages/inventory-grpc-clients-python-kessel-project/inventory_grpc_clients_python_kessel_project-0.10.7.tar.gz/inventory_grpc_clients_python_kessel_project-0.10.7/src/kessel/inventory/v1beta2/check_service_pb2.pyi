ce5ad519a5f5d1cb7ef19d970a29f1cea7ecb0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:10:32 +0000

    Add package inventory-grpc-clients-python-kessel-project/0.10.7/inventory_grpc_clients_python_kessel_project-0.10.7.tar.gz

---
+++ packages/inventory-grpc-clients-python-kessel-project/inventory_grpc_clients_python_kessel_project-0.10.7.tar.gz/inventory_grpc_clients_python_kessel_project-0.10.7/src/kessel/inventory/v1beta2/check_service_pb2.pyi
@@ -0,0 +1,9 @@
+from google.api import annotations_pb2 as _annotations_pb2
+from kessel.inventory.v1beta2 import check_request_pb2 as _check_request_pb2
+from kessel.inventory.v1beta2 import check_response_pb2 as _check_response_pb2
+from kessel.inventory.v1beta2 import check_for_update_request_pb2 as _check_for_update_request_pb2
+from kessel.inventory.v1beta2 import check_for_update_response_pb2 as _check_for_update_response_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
