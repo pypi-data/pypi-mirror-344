79c08b67b6fbf0699a84370ec14fb4cafeed0863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:12 +0000

    Add package secondbrain/0.0.50/secondbrain-0.0.50-py3-none-any.whl

---
+++ packages/secondbrain/secondbrain-0.0.50-py3-none-any.whl/secondbrain/reference/mcp.py
@@ -0,0 +1,31 @@
+import os
+import importlib
+from .lib.lib import ModuleManager, wrapped_func
+from secondbrain import utils
+import json
+
+
+params = utils.params
+
+
+def ref_mcp(item_id, config=None, workspace_path=None):
+    if workspace_path is None:
+        workspace_path = params["workspacePath"]
+    tool_base_path = os.path.join(workspace_path, "User/Local/Workflow")
+    code_folder = os.path.join(tool_base_path, item_id)
+    code_folder = os.path.normpath(os.path.abspath(code_folder))
+    with open(code_folder + "/info.json", "r", encoding="utf-8") as f:
+        name = json.load(f)["name"]
+    
+    if not os.path.exists(code_folder):
+        print(f"Mcp {item_id} not found")
+        return None
+    try:
+        with ModuleManager(code_folder) as manager:
+            
+
+            return tool
+    except Exception:
+        import traceback
+        print(f"Error loading mcp {name}({item_id}): \n{traceback.format_exc()}")
+        return None
