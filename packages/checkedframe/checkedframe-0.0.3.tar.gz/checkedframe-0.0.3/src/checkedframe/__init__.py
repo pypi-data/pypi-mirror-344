e3fdaa48a4485ed98402c1c3403c218054b6be07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 14:07:08 +0000

    Add package checkedframe/0.0.3/checkedframe-0.0.3.tar.gz

---
+++ packages/checkedframe/checkedframe-0.0.3.tar.gz/checkedframe-0.0.3/src/checkedframe/__init__.py
@@ -0,0 +1,30 @@
+# ruff: noqa: F401
+from checkedframe import exceptions
+
+from ._core import Check, Column, Schema
+from ._dtypes import (
+    Array,
+    Binary,
+    Boolean,
+    Date,
+    Datetime,
+    Decimal,
+    Duration,
+    Float32,
+    Float64,
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    Int128,
+    List,
+    Object,
+    String,
+    Struct,
+    UInt8,
+    UInt16,
+    UInt32,
+    UInt64,
+    UInt128,
+    Unknown,
+)
