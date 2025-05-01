766ee0e86b053c88ce830e2b440703820402e59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 07:05:14 +0000

    Add package frequenz-lib-notebooks/0.5.1/frequenz_lib_notebooks-0.5.1-py3-none-any.whl

---
+++ packages/frequenz-lib-notebooks/frequenz_lib_notebooks-0.5.1-py3-none-any.whl/frequenz/data/microgrid/__init__.py
@@ -0,0 +1,10 @@
+# License: MIT
+# Copyright © 2025 Frequenz Energy-as-a-Service GmbH
+
+"""Initialize the microgrid data module."""
+
+from .config import MicrogridConfig
+
+__all__ = [
+    "MicrogridConfig",
+]
