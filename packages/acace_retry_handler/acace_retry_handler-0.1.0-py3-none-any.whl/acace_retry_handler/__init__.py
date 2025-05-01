3be5b0bb08f53b03166db8c3d12e4e6e3d2ea3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:16 +0000

    Add package acace_retry_handler/0.1.0/acace_retry_handler-0.1.0-py3-none-any.whl

---
+++ packages/acace_retry_handler/acace_retry_handler-0.1.0-py3-none-any.whl/acace_retry_handler/__init__.py
@@ -0,0 +1,9 @@
+"""
+acace_retry_handler - Component of the Adaptive Context-Aware Content Engine
+"""
+
+__version__ = "0.1.0"
+
+def get_version():
+    """Return the package version."""
+    return __version__
