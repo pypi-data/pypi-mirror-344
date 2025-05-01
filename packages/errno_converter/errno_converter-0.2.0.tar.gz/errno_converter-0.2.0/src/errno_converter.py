0702e867a6e328e8797f87187697cd7366ef9b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 08:05:41 +0000

    Add package errno_converter/0.2.0/errno_converter-0.2.0.tar.gz

---
+++ packages/errno_converter/errno_converter-0.2.0.tar.gz/errno_converter-0.2.0/src/errno_converter.py
@@ -0,0 +1,30 @@
+import os
+import errno
+
+from lib.converter import Converter
+from lib.parser import Parser
+
+
+class ErrnoConverter(Converter):
+    NAME: str = "errno"
+    NUMBER_RANGE: range = range(1, 132)
+
+    @staticmethod
+    def number2code(number: int) -> str:
+        return errno.errorcode[int(number)]
+
+    @staticmethod
+    def code2number(code: str) -> int:
+        return getattr(errno, code)
+
+    @staticmethod
+    def get_candidates() -> list[str]:
+        return dir(errno)
+
+    @staticmethod
+    def number2description(number: int) -> str:
+        return os.strerror(number)
+
+
+def main() -> None:
+    return Parser.parse(ErrnoConverter)
