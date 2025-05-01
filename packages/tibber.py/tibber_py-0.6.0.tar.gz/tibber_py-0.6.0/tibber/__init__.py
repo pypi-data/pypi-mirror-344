f00a4f67a523c645e2427c0ee45a0b41cf4dda84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:54 +0000

    Add package tibber.py/0.6.0/tibber_py-0.6.0.tar.gz

---
+++ packages/tibber.py/tibber_py-0.6.0.tar.gz/tibber_py-0.6.0/tibber/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.6.0"
+DEMO_TOKEN = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
+API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
+
+import asyncio
+import os
+
+# The event loop type causes problems on windows systems when exiting.
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+# Import modules after defining constants to avoid circular import error.
+from .account import Account
+from .types.home import NonDecoratedTibberHome, TibberHome
+
+__all__ = [
+    "__version__",
+    "DEMO_TOKEN",
+    "API_ENDPOINT",
+    "Account",
+    "NonDecoratedTibberHome",
+    "TibberHome",
+]
