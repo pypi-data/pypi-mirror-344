ee27b3b5c16f7f7f1ff5c3e1226814cfbfca7231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:10:10 +0000

    Add package fspacker/0.8.1/fspacker-0.8.1-py3-none-any.whl

---
+++ packages/fspacker/fspacker-0.8.1-py3-none-any.whl/fspacker/parsers/package.py
@@ -0,0 +1,57 @@
+import email.message
+import pathlib
+import tarfile
+import typing
+import zipfile
+from pathlib import Path
+
+from packaging.requirements import Requirement
+
+from fspacker.utils.requirement import RequirementParser
+
+__all__ = ["analyze_package_deps"]
+
+
+class PackageFileDependencyAnalyzer:
+    @staticmethod
+    def extract_metadata(package_path: Path) -> typing.Optional[email.message.Message]:
+        """从包文件中提取元数据"""
+        if package_path.suffix == ".whl":
+            with zipfile.ZipFile(package_path) as z:
+                for name in z.namelist():
+                    if name.endswith(".dist-info/METADATA"):
+                        metadata = z.read(name).decode("utf-8")
+                        return email.message_from_string(metadata)
+
+        elif package_path.suffix in (".tar.gz", ".zip"):
+            opener = tarfile.open if package_path.suffix == ".gz" else zipfile.ZipFile
+            with opener(package_path) as archive:
+                for member in archive.getmembers():
+                    if member.name.endswith(("PKG-INFO", "METADATA")):
+                        fileobj = archive.extractfile(member)
+                        metadata = fileobj.read().decode("utf-8")
+                        return email.message_from_string(metadata)
+        return None
+
+    @classmethod
+    def analyze_dependencies(cls, package_path: Path) -> typing.List[Requirement]:
+        metadata = cls.extract_metadata(package_path)
+        if not metadata:
+            return []
+
+        requirements = []
+        for field in ["Requires-Dist", "Requires"]:
+            for req_str in metadata.get_all(field, []):
+                req = RequirementParser.parse_requirement(req_str)
+                if req:
+                    requirements.append(req)
+        return requirements
+
+
+__analyzer = PackageFileDependencyAnalyzer()
+
+
+def analyze_package_deps(package_file_path: pathlib.Path):
+    global __analyzer
+
+    return __analyzer.analyze_dependencies(package_file_path)
