a4d1682fdd219693fc811cd8422f90c61d669958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 07:05:47 +0000

    Add package togmito/1.0.2/togmito-1.0.2.tar.gz

---
+++ packages/togmito/togmito-1.0.2.tar.gz/togmito-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='togmito',
+    version='1.0.2',  # her yüklemede farklı olmalı
+    description='Kick ve Instagram için Telegram kontrollü AI bot',
+    author='Aytunç',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'mtranslate',
+        'ollama',
+        'selenium',
+        'soundcard',
+        'soundfile',
+        'speechrecognition',
+        'undetected-chromedriver',
+        'certifi',
+        'python-telegram-bot==20.7'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
