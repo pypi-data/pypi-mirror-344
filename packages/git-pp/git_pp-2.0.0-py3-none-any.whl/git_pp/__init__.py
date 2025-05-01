76271c527b541b6e087567b926cea4ed939da665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 14:06:40 +0000

    Add package git-pp/2.0.0/git_pp-2.0.0-py3-none-any.whl

---
+++ packages/git-pp/git_pp-2.0.0-py3-none-any.whl/git_pp/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '2.0.0'
+__app_name__ = 'git pp'
+
+try:
+    from logging_utils_tddschn import get_logger  # type: ignore
+    logger, _ = get_logger(__app_name__)
+except:
+    import logging
+    from logging import NullHandler
+    logger = logging.getLogger(__name__)
+    logger.addHandler(NullHandler())
