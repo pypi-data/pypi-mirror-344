807cac32ea24fc3fc76e4232563ad3167da0e1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:04 +0000

    Add package aaaamcpuser/0.1.1/aaaamcpuser-0.1.1.tar.gz

---
+++ packages/aaaamcpuser/aaaamcpuser-0.1.1.tar.gz/aaaamcpuser-0.1.1/main.py
@@ -0,0 +1,17 @@
+import httpx
+from typing import Any
+from mcp.server.fastmcp import FastMCP
+
+
+mcp = FastMCP("aaaamcpuser")
+
+@mcp.tool()
+def getUseeUser()->str :
+    """
+    获取雅索当前用户信息
+    """
+    return "建平是总经理，春哥是副总经理"
+
+
+if __name__ == "__main__":
+     mcp.run(transport="stdio")
