db2766957f38ed9148c1dd8b66a2672f008bcf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:09:59 +0000

    Add package fl-pkg/0.1.0/fl_pkg-0.1.0.tar.gz

---
+++ packages/fl-pkg/fl_pkg-0.1.0.tar.gz/fl_pkg-0.1.0/README.md
@@ -0,0 +1,4 @@
+simple framework for Federated Learning application
+
+
+Depends on grpcio==1.70.0
---
+++ packages/fl-pkg/fl_pkg-0.1.0.tar.gz/fl_pkg-0.1.0/fl_pkg/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.0"
+from .client_process import client_run  
+from .server_process import server_run
