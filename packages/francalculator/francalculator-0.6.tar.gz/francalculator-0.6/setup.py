1a5637a51ef472baff95cd89245c97232c5ae7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 15:06:27 +0000

    Add package francalculator/0.6/francalculator-0.6.tar.gz

---
+++ packages/francalculator/francalculator-0.6.tar.gz/francalculator-0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='francalculator',  # Nome do seu pacote
+    version='0.6',
+    description='Biblioteca para cálculos matemáticos e engenharia',  # Descrição do seu pacote
+    author='Lukydnomo',  # Seu nome
+    packages=find_packages(),  # Encontra automaticamente os pacotes
+    install_requires=[  # Dependências, se houver
+        "requests", "pathlib", "colorama"
+    ],
+    classifiers=[  # Classificadores para ajudar na busca
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Versão mínima do Python
+)
