08466049631f32f98bdc5fc11823fa57091a6317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 15:06:26 +0000

    Add package francalculator/0.4/francalculator-0.4.tar.gz

---
+++ packages/francalculator/francalculator-0.4.tar.gz/francalculator-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='francalculator',  # Nome do seu pacote
+    version='0.4',
+    description='Biblioteca para cálculos matemáticos e engenharia',  # Descrição do seu pacote
+    author='Lukydnomo',  # Seu nome
+    packages=find_packages(),  # Encontra automaticamente os pacotes
+    install_requires=[  # Dependências, se houver
+        "requests", "pathlib", "math", "sys", "random", "datetime", "ast", "operator", "logging", "traceback", "colorama", "shutil"
+    ],
+    classifiers=[  # Classificadores para ajudar na busca
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Versão mínima do Python
+)
