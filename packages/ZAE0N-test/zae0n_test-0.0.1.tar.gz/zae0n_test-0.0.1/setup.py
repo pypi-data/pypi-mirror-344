835e553524060ae1fdbbd88a793b4cb6a75209be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:01 +0000

    Add package ZAE0N-test/0.0.1/zae0n_test-0.0.1.tar.gz

---
+++ packages/ZAE0N-test/zae0n_test-0.0.1.tar.gz/zae0n_test-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ZAE0N_test',
+    version='0.0.1',
+    description='test file',
+    author='ZAE0N',
+    author_email='jeon020526@gmail.com',
+    url='',
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=['test', 'ZAE0N', 'python workout 8.2', 'python tutorial', 'pypi'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
