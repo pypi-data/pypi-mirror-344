b7e8a98b884953094687e714f2516af3768a804c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:06:59 +0000

    Add package danish05/0.0.1/danish05-0.0.1.tar.gz

---
+++ packages/danish05/danish05-0.0.1.tar.gz/danish05-0.0.1/danish05/__init__.py
@@ -0,0 +1,15 @@
+"""
+# My Social media
+youtubed05 = "https://www.youtube.com/@ImNotDanish05"
+tiktokd05 = "https://www.tiktok.com/@imnotdanish05" 
+githubd05 = "https://github.com/ImNotDanish05"
+gmaild05 = "mailto:imnotdanish05bussiness@gmail.com"
+"""
+
+from .info import beautifullline
+from .info import info
+from .info import onOpen
+from .info import bgrem
+from .info import mbgrem
+
+__version__ = "0.0.1"
