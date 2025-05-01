dbe4e9247b96e0f8e3454cf6c47521998e6d5ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:00 +0000

    Add package async-stream-processing/1.0.0/async_stream_processing-1.0.0.tar.gz

---
+++ packages/async-stream-processing/async_stream_processing-1.0.0.tar.gz/async_stream_processing-1.0.0/examples/common.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+import async_stream_processing as asp
+from async_stream_processing.testing import create_async_generator, timestamps  # noqa: F401
+
+NAMES = ["Jane", "John", "Sarah", "Paul", "Jane"]
+
+
+def format(event_time: datetime):
+    return f"{event_time:%H:%M:%S.%f}"
+
+
+def log(event_time: datetime, msg: str):
+    print(f"{format(datetime.now())} {format(asp.now())} {format(event_time)} {msg}")
+
+
+class Greeter:
+    def __init__(self):
+        self.greeted = set()
+
+    def greet(self, event_time: datetime, name: str):
+        if name not in self.greeted:
+            log(event_time, f"Hello {name}.")
+            self.greeted.add(name)
+        else:
+            log(event_time, f"Hello again {name}!")
+
+    def greet_later(self, event_time: datetime, name):
+        log(event_time, f"{name} arrived.")
+        asp.call_later(event_time + timedelta(seconds=1), self.greet, name)
+
+    async def sleep_and_greet(self, event_time: datetime, name):
+        log(event_time, f"{name} arrived.")
+        delay = timedelta(seconds=5)
+        await asp.sleep(delay)
+        self.greet(event_time + delay, name)
---
+++ packages/async-stream-processing/async_stream_processing-1.0.0.tar.gz/async_stream_processing-1.0.0/tests/test_past.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+from typing import Any
+
+import pytest
+
+import async_stream_processing as asp
+from async_stream_processing.testing import timestamps
+
+TIMESTAMP_TOLERANCE = 0.001
+
+
+class Client:
+    def __init__(self, start_time):
+        self.start_time = start_time
+        self.greeted = []
+
+    def greet(self, _event_time: datetime, value):
+        self.greeted.append(((asp.now() - self.start_time).total_seconds(), value))
+
+    async def sleep_and_greet(self, _event_time: datetime, value):
+        delay = timedelta(seconds=1)
+        await asp.sleep(delay)
+        self.greet(_event_time + delay, value)
+
+    def greet_later(self, _event_time: datetime, value: Any):
+        asp.call_later(1, self.greet, value)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("method,lags", [("greet", [0] * 10), ("sleep_and_greet", [1] * 10), ("greet_later", [1] * 10)])
+async def test_fast_forward(method, lags):
+    """
+    - past events are all passed at roughly the right virtual time.
+    """
+    start_time = datetime.now() - timedelta(seconds=60)
+    client = Client(start_time)
+    values = list(range(10))
+    past_values = list(zip(timestamps(start_time, delay=timedelta(seconds=1)), values))
+    await asp.run([asp.process_stream(callback=getattr(client, method), past=past_values)], start_time=start_time)
+    assert len(client.greeted) == len(values)
+    for (event_time, value), (expected_timestamp, expected_value), lag in zip(client.greeted, past_values, lags):
+        assert abs(event_time - lag - (expected_timestamp - start_time).total_seconds()) < TIMESTAMP_TOLERANCE
+        assert value == expected_value
