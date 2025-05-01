00f7a001882a5d9ec2bc4584db433b025df3621a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:00 +0000

    Add package async-stream-processing/1.0.0/async_stream_processing-1.0.0-py3-none-any.whl

---
+++ packages/async-stream-processing/async_stream_processing-1.0.0-py3-none-any.whl/async_stream_processing/testing.py
@@ -0,0 +1,33 @@
+import asyncio
+from datetime import datetime, timedelta
+from typing import Any, Dict, Iterable, Iterator, Tuple
+
+import async_stream_processing as asp
+
+
+async def create_async_generator(values, delay=1):
+    """ "
+    Yield values in the future with a delay.
+    """
+    for value in values:
+        yield datetime.now(), value
+        await asyncio.sleep(delay)
+
+
+def timestamps(start: datetime, delay: timedelta):
+    current_time = start
+    while True:
+        yield current_time
+        current_time += delay
+
+
+def merge_timeseries(
+    timeseries: Dict[str, Iterable[Tuple[datetime, Any]]],
+) -> Iterator[Tuple[datetime, Dict[str, Any]]]:
+    for values in zip(*(iter(ts) for ts in timeseries.values())):
+        yield values[0][0], dict(zip(timeseries, [v for _, v in values]))
+
+
+def log(*args: Any):
+    """Log a message with the current time."""
+    print(asp.now().isoformat(sep=" ", timespec="milliseconds"), *args)
