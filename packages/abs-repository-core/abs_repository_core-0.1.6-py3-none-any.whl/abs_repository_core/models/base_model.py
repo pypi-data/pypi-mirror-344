969c971fcdf8df30fd3bea98e105f5b28da6aa09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 03:25:32 +0000

    Add package abs-repository-core/0.1.6/abs_repository_core-0.1.6-py3-none-any.whl

---
+++ packages/abs-repository-core/abs_repository_core-0.1.6-py3-none-any.whl/abs_repository_core/models/base_model.py
@@ -0,0 +1,24 @@
+import uuid
+from datetime import UTC, datetime
+
+from sqlalchemy import Column, DateTime, Integer, String,Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import inspect
+
+Base = declarative_base()
+
+
+class BaseModel(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    uuid = Column(String(36), unique=True, default=lambda: str(uuid.uuid4()))
+    created_at = Column(DateTime, default=lambda: datetime.now(UTC))
+    updated_at = Column(
+        DateTime, default=lambda: datetime.now(UTC), onupdate=lambda: datetime.now(UTC)
+    )
+
+    class Config:
+        orm_mode = True
+
+class BaseDraftModel(BaseModel):
+    is_draft = Column(Boolean, default=False)
