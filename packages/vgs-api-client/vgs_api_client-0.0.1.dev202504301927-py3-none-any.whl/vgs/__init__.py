e60cd551ae7c735993be4af154a154d425c28bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 07:05:27 +0000

    Add package vgs-api-client/0.0.1.dev202504301927/vgs_api_client-0.0.1.dev202504301927-py3-none-any.whl

---
+++ packages/vgs-api-client/vgs_api_client-0.0.1.dev202504301927-py3-none-any.whl/vgs/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.1.dev202504301927"
+
+# import Aliases
+from vgs.aliases_api import Aliases
+from vgs.configuration import VGSConfiguration, config
+from vgs.exceptions import *
+from vgs.functions_api import Functions
