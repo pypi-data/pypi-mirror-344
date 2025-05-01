de3b905826738d36af57260677286d5e3f6e51cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 15:06:27 +0000

    Add package safe-transformer-fixed/0.2.0/safe-transformer-fixed-0.2.0.tar.gz

---
+++ packages/safe-transformer-fixed/safe-transformer-fixed-0.2.0.tar.gz/safe-transformer-fixed-0.2.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="safe-transformer-fixed",
+    version="0.2.0",
+    author="Aleksandra Gacek, Piotr Luboń",
+    author_email="lubonp@student.mini.pw.edu.pl, gaceka@student.mini.pw.edu.pl",
+    maintainer="Dilshan Sonnadara",
+    maintainer_email="dilshan.sonnadara13@gmail.com",
+    description="Build explainable ML models using surrogate models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olagacek/SAFE",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+          'ruptures',
+          'scikit-learn',
+          'pandas',
+          'scipy',
+          'kneed'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
