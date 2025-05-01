96c8bc081eb548d2a791f004f9b0833e5ad21cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 08:05:57 +0000

    Add package auto-editor/27.1.1/auto_editor-27.1.1-py3-none-any.whl

---
+++ packages/auto-editor/auto_editor-27.1.1-py3-none-any.whl/auto_editor/cmds/desc.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import sys
+from dataclasses import dataclass, field
+
+from auto_editor.ffwrapper import FileInfo
+from auto_editor.utils.log import Log
+from auto_editor.vanparse import ArgumentParser
+
+
+@dataclass(slots=True)
+class DescArgs:
+    help: bool = False
+    input: list[str] = field(default_factory=list)
+
+
+def desc_options(parser: ArgumentParser) -> ArgumentParser:
+    parser.add_required("input", nargs="*")
+    return parser
+
+
+def main(sys_args: list[str] = sys.argv[1:]) -> None:
+    args = desc_options(ArgumentParser("desc")).parse_args(DescArgs, sys_args)
+    for path in args.input:
+        src = FileInfo.init(path, Log())
+        if src.description is not None:
+            sys.stdout.write(f"\n{src.description}\n\n")
+        else:
+            sys.stdout.write("\nNo description.\n\n")
+
+
+if __name__ == "__main__":
+    main()
