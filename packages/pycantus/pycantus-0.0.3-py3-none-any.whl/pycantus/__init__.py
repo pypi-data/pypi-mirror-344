90994bd72f883ce5b203997ed19724c921f5f39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 16:07:06 +0000

    Add package pycantus/0.0.3/pycantus-0.0.3-py3-none-any.whl

---
+++ packages/pycantus/pycantus-0.0.3-py3-none-any.whl/pycantus/__init__.py
@@ -0,0 +1,20 @@
+
+#from .models.corpus import Corpus
+#from .models.chant import Chant
+#from .models.source import Source
+#from .dataloaders.loader import CSVLoader
+#import data
+
+def hello_pycantus():
+    message = """
+    *********************************************
+    *                                           *
+    *           Welcome to PyCantus!            *
+    *                                           *
+    *    A Python library designed to enhance   *
+    *    accessibility of Gregorian chants for  *
+    *    both coders and non-coders.            *
+    *                                           *
+    *********************************************
+    """
+    print(message)
