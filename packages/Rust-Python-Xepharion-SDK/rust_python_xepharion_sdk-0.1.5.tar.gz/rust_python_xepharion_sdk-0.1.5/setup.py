66577a6bf80ff0319d07c40d8dcdcc01daa20704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:15 +0000

    Add package Rust-Python-Xepharion-SDK/0.1.5/rust_python_xepharion_sdk-0.1.5.tar.gz

---
+++ packages/Rust-Python-Xepharion-SDK/rust_python_xepharion_sdk-0.1.5.tar.gz/rust_python_xepharion_sdk-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Rust_Python_Xepharion_SDK',
+    version='0.1.5',
+    author='Gustavo Alejandro Paredes Orozco',
+    author_email='gustavoparedes1978@gmail.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
