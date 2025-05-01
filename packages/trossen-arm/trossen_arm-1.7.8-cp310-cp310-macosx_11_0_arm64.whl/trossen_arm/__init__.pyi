8ba6ad11568474b566558c43bd0615aa6430c630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 05:05:14 +0000

    Add package trossen-arm/1.7.8/trossen_arm-1.7.8-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/trossen-arm/trossen_arm-1.7.8-cp310-cp310-macosx_11_0_arm64.whl/trossen_arm/__init__.pyi
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from trossen_arm.trossen_arm import EndEffectorProperties
+from trossen_arm.trossen_arm import IPMethod
+from trossen_arm.trossen_arm import JointCharacteristic
+from trossen_arm.trossen_arm import LinkProperties
+from trossen_arm.trossen_arm import Mode
+from trossen_arm.trossen_arm import Model
+from trossen_arm.trossen_arm import StandardEndEffector
+from trossen_arm.trossen_arm import TrossenArmDriver
+from . import trossen_arm
+__all__ = ['EndEffectorProperties', 'IPMethod', 'JointCharacteristic', 'LinkProperties', 'Mode', 'Model', 'StandardEndEffector', 'TrossenArmDriver', 'trossen_arm']
