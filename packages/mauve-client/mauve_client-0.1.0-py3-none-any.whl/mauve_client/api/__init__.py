d8291a991e25f6b43b3fadaf9a1eff584b66923f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-01 04:10:38 +0000

    Add package mauve-client/0.1.0/mauve_client-0.1.0-py3-none-any.whl

---
+++ packages/mauve-client/mauve_client-0.1.0-py3-none-any.whl/mauve_client/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mauve_client.api.categories_api import CategoriesApi
+from mauve_client.api.customers_api import CustomersApi
+from mauve_client.api.documents_api import DocumentsApi
+from mauve_client.api.orders_api import OrdersApi
+from mauve_client.api.others_api import OthersApi
+from mauve_client.api.portal__categories_api import PortalCategoriesApi
+from mauve_client.api.portal__documents_api import PortalDocumentsApi
+from mauve_client.api.portal__orders_api import PortalOrdersApi
+from mauve_client.api.portal__products_api import PortalProductsApi
+from mauve_client.api.products_api import ProductsApi
