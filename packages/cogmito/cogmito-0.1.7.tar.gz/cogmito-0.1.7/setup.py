db6aba63a14642850306709c99dfad8689a05685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:23:51 +0000

    Add package cogmito/0.1.7/cogmito-0.1.7.tar.gz

---
+++ packages/cogmito/cogmito-0.1.7.tar.gz/cogmito-0.1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cogmito",
+    version="0.1.7",
+    author="Aytunç",
+    description="AI destekli yayın ve otomasyon botu",
+    packages=find_packages(),
+    install_requires=[
+        "certifi",
+        "soundcard",
+        "soundfile",
+        "mtranslate",
+        "ollama",
+        "selenium",
+        "undetected-chromedriver",
+        "SpeechRecognition",
+        "python-telegram-bot",
+    ],
+    python_requires=">=3.9,<3.10",
+    entry_points={
+        "console_scripts": [
+            "cogmito=cogmito.main:baslat"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+)
