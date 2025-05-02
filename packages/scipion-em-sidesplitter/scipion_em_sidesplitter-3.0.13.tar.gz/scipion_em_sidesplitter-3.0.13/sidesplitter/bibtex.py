0544ff8f1bd9c29593330787c35a59c6b05c1ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:23:53 +0000

    Add package scipion-em-sidesplitter/3.0.13/scipion_em_sidesplitter-3.0.13.tar.gz

---
+++ packages/scipion-em-sidesplitter/scipion_em_sidesplitter-3.0.13.tar.gz/scipion_em_sidesplitter-3.0.13/sidesplitter/bibtex.py
@@ -0,0 +1,42 @@
+# coding: latin-1
+# **************************************************************************
+# *
+# * Authors:     Grigory Sharov (gsharov@mrc-lmb.cam.ac.uk)
+# *
+# * MRC Laboratory of Molecular Biology (MRC-LMB)
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+@article{Ramlaul2020,
+  title = {Mitigating local over-fitting during single particle reconstruction with SIDESPLITTER},
+  volume = {211},
+  ISSN = {1047-8477},
+  DOI = {http://dx.doi.org/10.1016/j.jsb.2020.107545},
+  number = {2},
+  journal = {Journal of Structural Biology},
+  publisher = {Elsevier BV},
+  author = {Ramlaul,  Kailash and Palmer,  Colin M. and Nakane,  Takanori and Aylett,  Christopher H.S.},
+  year = {2020},
+  month = aug,
+  pages = {107545}
+}
+
+"""
