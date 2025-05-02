d08e5283945dda71251b1dc717fed28801df03b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:05:48 +0000

    Add package otteroad/0.1.0/otteroad-0.1.0-py3-none-any.whl

---
+++ packages/otteroad/otteroad-0.1.0-py3-none-any.whl/otteroad/utils/__init__.py
@@ -0,0 +1,22 @@
+"""
+This package provides shared utilities and abstractions used across the Kafka client library:
+
+- LoggerProtocol: Runtime-checkable protocol for injecting custom loggers.
+- (Future utilities can be added here, e.g., metrics, error classes, common helpers.)
+
+Usage example:
+
+    from otteroad.utils import LoggerProtocol
+
+    def my_function(logger: LoggerProtocol):
+        logger.info("This is a log message")
+
+Exports:
+    - LoggerProtocol: Protocol for a standard logging interface, compatible with Python's logging.Logger.
+"""
+
+from .logger import LoggerProtocol
+
+__all__ = [
+    "LoggerProtocol",
+]
