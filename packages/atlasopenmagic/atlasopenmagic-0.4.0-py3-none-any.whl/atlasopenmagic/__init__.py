c98a8e164b1b44f0420bb9de350048f9f40d44d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:05:14 +0000

    Add package atlasopenmagic/0.4.0/atlasopenmagic-0.4.0-py3-none-any.whl

---
+++ packages/atlasopenmagic/atlasopenmagic-0.4.0-py3-none-any.whl/atlasopenmagic/__init__.py
@@ -0,0 +1,25 @@
+from .metadata import (
+    get_metadata,
+    set_release,
+    get_urls,
+    available_releases,
+    get_current_release,
+    get_urls_data,
+    available_data
+)
+
+from .utils import (
+    install_from_environment
+)
+
+# List of public functions available when importing the package
+__all__ = [
+    "get_urls",
+    "get_metadata",
+    "set_release",
+    "available_releases",
+    "get_current_release",
+    "get_urls_data",
+    "available_data",
+    "install_from_environment"
+]
