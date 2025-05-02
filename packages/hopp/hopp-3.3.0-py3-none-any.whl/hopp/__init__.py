24ba5654580ea99e8a25c13105ae008be21b107d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:05:20 +0000

    Add package hopp/3.3.0/hopp-3.3.0-py3-none-any.whl

---
+++ packages/hopp/hopp-3.3.0-py3-none-any.whl/hopp/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+__version__ = "3.3.0"
+
+ROOT_DIR = Path(__file__).resolve().parent
+
+# environment variable used to specify a testing environment
+TEST_ENV_VAR="TEST"
