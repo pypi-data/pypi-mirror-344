06424661f7000de921dbbc9f7193e599826b5cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:24:00 +0000

    Add package mynotification/1.0.3/mynotification-1.0.3.tar.gz

---
+++ packages/mynotification/mynotification-1.0.3.tar.gz/mynotification-1.0.3/src/mynotification/notification_android.py
@@ -0,0 +1,36 @@
+from jnius import autoclass
+import traceback
+import os
+
+
+def send_notification(title: str, message: str):
+    try:
+        activity_host_class = os.getenv("MAIN_ACTIVITY_HOST_CLASS_NAME")
+        assert activity_host_class
+        activity_host = autoclass(activity_host_class)
+        activity = activity_host.mActivity
+        Context = autoclass("android.context.Context")
+        NotificationManager = autoclass("android.app.NotificationManager")
+        NotificationChannel = autoclass("android.app.NotificationChannel")
+        Notification = autoclass("android.app.Notification")
+        NotificationBuilder = autoclass("android.app.NotificationBuilder")
+        notification_service = activity.getSystemService(Context.NOTIFICATION_SERVICE)
+        channel_id = "my_notification_channel"
+        channel_name = "Flet Notification Channel"
+        importance = NotificationManager.IMPORTANCE_DEFAULT
+        channel = NotificationChannel(channel_id, channel_name, importance)
+        notification_service.createNotificationChannel(channel)
+        builder = NotificationBuilder(activity, channel_id)
+        builder.setContentTitle(title)
+        builder.setContentText(message)
+        builder.setSmallIcon(activity.getApplicationInfo().icon)
+        builder.setAutoCancel(True)
+        notification_id = 1
+        notification = builder.build()
+        notification_service.notify(notification_id, notification)
+        status_text.value = "Notification sent succesfully."
+        status_text.collor = "green"
+
+    except:
+        status_text.value = f"Failed to seld notification: {traceback.format_exc()}"
+        status_text.color = "red"
