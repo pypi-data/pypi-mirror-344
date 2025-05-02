4578b013f6418a222eac1f18e7704e1e987e2999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:24:00 +0000

    Add package inspect-ai/0.3.93/inspect_ai-0.3.93.tar.gz

---
+++ packages/inspect-ai/inspect_ai-0.3.93.tar.gz/inspect_ai-0.3.93/src/inspect_ai/util/_anyio.py
@@ -0,0 +1,38 @@
+import itertools
+import sys
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import ExceptionGroup
+
+
+def inner_exception(exc: Exception) -> Exception:
+    return _flatten_exception(exc)[0]
+
+
+def _flatten_exception(exc: Exception) -> list[Exception]:
+    """Recursively flatten an exception to get all related (__context__) and contained (ExceptionGroup) exceptions."""
+    context_to_follow = (
+        [exc.__context__]
+        # conceptually, if __cause__ is present, it means that this exception
+        # wraps the cause - rather than cause being a separate error. We'll
+        # follow __context__ only if __cause__ is None
+        if exc.__cause__ is None and isinstance(exc.__context__, Exception)
+        else []
+    )
+
+    (maybe_this_exception, children_to_follow) = (
+        ([], exc.exceptions)
+        # if it's a group, follow the children discarding the group
+        if isinstance(exc, ExceptionGroup)
+        else ([exc], [])
+    )
+
+    # We have to use a set since the same exception is likely to be included in
+    # both __context__ and .exceptions
+    other_exceptions = [
+        flattened_e
+        for e in set(itertools.chain(context_to_follow, children_to_follow))
+        for flattened_e in _flatten_exception(e)
+    ]
+
+    return maybe_this_exception + other_exceptions
