53b3cb918d6bea7f3023159cd0ddb5e17f9b837d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:24:38 +0000

    Add package eic-aichat-connect/1.0.3/eic_aichat_connect-1.0.3.tar.gz

---
+++ packages/eic-aichat-connect/eic_aichat_connect-1.0.3.tar.gz/eic_aichat_connect-1.0.3/eic_aichat_connect/data/version1/ModelRequestV1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class ModelRequestV1:
+    """
+    ModelRequestV1 data object.
+    """
+
+    def __init__(self, value: str = None):
+        """
+        Initializes a new instance of ModelRequestV1.
+
+        :param value: Request value.
+        """
+        self.value = value
